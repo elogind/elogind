322e94abd97bbd3b8b0d37719449c0be5b9d99d8
Thomas Hindoe Paaboel Andersen <phomes@gmail.com>
2015-04-09 23:57:35 +0200

    util: fix a typo in comment

--- src/shared/util.c
+++ src/shared/util.c
@@ -1353,7 +1353,7 @@ char *cescape(const char *s) {
 
         assert(s);
 
-        /* Does C style string escaping. May be be reversed with
+        /* Does C style string escaping. May be reversed with
          * cunescape(). */
 
         r = new(char, strlen(s)*4 + 1);
