ecfb1aecd320fef4b1264bf68ad540de1147a71b
Torstein Husebø <torstein@huseboe.net>
2015-07-24 11:14:01 +0200

    treewide: fix typos

--- src/shared/apparmor-util.h
+++ src/shared/apparmor-util.h
@@ -27,7 +27,7 @@
 
 int verify_units(char **filenames, ManagerRunningAs running_as, bool check_man);
 
-/* XXX: find an authorotative list of all pseudo types? */
+/* XXX: find an authoritative list of all pseudo types? */
 bool dns_type_is_pseudo(int n) {
         return IN_SET(n, DNS_TYPE_ANY, DNS_TYPE_AXFR, DNS_TYPE_IXFR, DNS_TYPE_OPT);
 }
