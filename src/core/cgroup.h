638dd5096b5549b7838c821784041af86b87a1f3
WaLyong Cho <walyong.cho@samsung.com>
2016-07-04 07:03:54 +0000

    core: introduce MemorySwapMax=
    Similar to MemoryMax=, MemorySwapMax= limits swap usage. This controls
    controls "memory.swap.max" attribute in unified cgroup.

--- src/core/cgroup.h
+++ src/core/cgroup.h
@@ -98,6 +98,7 @@ struct CGroupContext {
         uint64_t memory_low;
         uint64_t memory_high;
         uint64_t memory_max;
+        uint64_t memory_swap_max;
 
         /* For legacy hierarchies */
         uint64_t cpu_shares;
