c1748bb202ab6f6aa4e5c17409ef83cf3ffdaa37
Lennart Poettering <lennart@poettering.net>
2016-12-01 23:19:31 +0100

    util-lib: add easy helpers for temporary directories that rmdir()ed via _cleanup_
    This adds mkdtemp_malloc() that is a combination of mkdtemp() plus strdup(). It
    initializes its return paremeter only if the temporary directory could be
    created successfully, so that the parameter is exactly non-NULL when the
    directory exists.
    
    rmdir_and_free() and rmdir_and_freep() are also added, and the latter may be
    used inside of _cleanup_ for such a directory string variable, to automatically
    rmdir() the directory if it is non-NULL when the scope exits.
    
    rmdir_and_free() is similar to the existing rm_rf_and_free() however, is only
    removes a single directory and does not operate recursively.

--- src/basic/fileio.h
+++ src/basic/fileio.h
@@ -49,8 +49,6 @@ int parse_env_file(const char *fname, const char *separator, ...) _sentinel_;
 int load_env_file(FILE *f, const char *fname, const char *separator, char ***l);
 int load_env_file_pairs(FILE *f, const char *fname, const char *separator, char ***l);
 
-int merge_env_file(char ***env, FILE *f, const char *fname);
-
 int write_env_file(const char *fname, char **l);
 
 int executable_is_script(const char *path, char **interpreter);
@@ -96,3 +94,5 @@ int link_tmpfile(int fd, const char *path, const char *target);
 #endif // 0
 
 int read_nul_string(FILE *f, char **ret);
+
+int mkdtemp_malloc(const char *template, char **ret);
