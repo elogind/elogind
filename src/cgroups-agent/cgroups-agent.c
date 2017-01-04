fc7f6d6e5c6a88d78805503f2933fcf2f7a0f754
Sven Eden <yamakuzure@gmx.net>
2017-01-04 06:40:46 +0100

    Prep v225: Applying various fixes and changes to src/cgroups-agent that got lost during git am transfer.

--- src/cgroups-agent/cgroups-agent.c
+++ src/cgroups-agent/cgroups-agent.c
@@ -38,13 +38,17 @@ int main(int argc, char *argv[]) {
         log_parse_environment();
         log_open();
 
-        /* Unlike in systemd where this has to use a private socket,
-           since logind doesn't associate control groups with services
-           and doesn't manage the dbus service, we can just use the
-           system bus.  */
-        r = sd_bus_open_system(&bus);
+        /* We send this event to the private D-Bus socket and then the
+         * system instance will forward this to the system bus. We do
+         * this to avoid an activation loop when we start dbus when we
+         * are called when the dbus service is shut down. */
+
+        r = bus_open_system_systemd(&bus);
         if (r < 0) {
-                log_debug_errno(r, "Failed to open system bus: %m");
+                /* If we couldn't connect we assume this was triggered
+                 * while systemd got restarted/transitioned from
+                 * initrd to the system, so let's ignore this */
+                log_debug_errno(r, "Failed to get D-Bus connection: %m");
                 return EXIT_FAILURE;
         }
 
@@ -54,7 +58,7 @@ int main(int argc, char *argv[]) {
                                "Released",
                                "s", argv[1]);
         if (r < 0) {
-                log_debug_errno(r, "Failed to send signal message: %m");
+                log_debug_errno(r, "Failed to send signal message on private connection: %m");
                 return EXIT_FAILURE;
         }
 
