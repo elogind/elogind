75a322055981a2d54b50ddb1d7c2649068ca1dc9
Sven Eden <yamakuzure@gmx.net>
2017-01-16 07:50:35 +0100

    Remove ELFUTILS, GCRYPT, GNUTLS, IMA, LIBAUDIT, LIBCRYPTSETUP, LZ4 and XZ features from build.h

--- src/basic/build.h
+++ src/basic/build.h
@@ -27,12 +27,6 @@
 #define _PAM_FEATURE_ "-PAM"
 #endif
 
-#ifdef HAVE_AUDIT
-#define _AUDIT_FEATURE_ "+AUDIT"
-#else
-#define _AUDIT_FEATURE_ "-AUDIT"
-#endif
-
 #ifdef HAVE_SELINUX
 #define _SELINUX_FEATURE_ "+SELINUX"
 #else
@@ -45,52 +39,16 @@
 #define _APPARMOR_FEATURE_ "-APPARMOR"
 #endif
 
-#ifdef HAVE_IMA
-#define _IMA_FEATURE_ "+IMA"
-#else
-#define _IMA_FEATURE_ "-IMA"
-#endif
-
 #ifdef HAVE_SMACK
 #define _SMACK_FEATURE_ "+SMACK"
 #else
 #define _SMACK_FEATURE_ "-SMACK"
 #endif
 
-#ifdef HAVE_LIBCRYPTSETUP
-#define _LIBCRYPTSETUP_FEATURE_ "+LIBCRYPTSETUP"
-#else
-#define _LIBCRYPTSETUP_FEATURE_ "-LIBCRYPTSETUP"
-#endif
-
-#ifdef HAVE_GCRYPT
-#define _GCRYPT_FEATURE_ "+GCRYPT"
-#else
-#define _GCRYPT_FEATURE_ "-GCRYPT"
-#endif
-
-#ifdef HAVE_GNUTLS
-#define _GNUTLS_FEATURE_ "+GNUTLS"
-#else
-#define _GNUTLS_FEATURE_ "-GNUTLS"
-#endif
-
 #ifdef HAVE_ACL
 #define _ACL_FEATURE_ "+ACL"
 #else
 #define _ACL_FEATURE_ "-ACL"
-#endif
-
-#ifdef HAVE_XZ
-#define _XZ_FEATURE_ "+XZ"
-#else
-#define _XZ_FEATURE_ "-XZ"
-#endif
-
-#ifdef HAVE_LZ4
-#define _LZ4_FEATURE_ "+LZ4"
-#else
-#define _LZ4_FEATURE_ "-LZ4"
 #endif
 
 #ifdef HAVE_SECCOMP
@@ -105,12 +63,6 @@
 #define _BLKID_FEATURE_ "-BLKID"
 #endif
 
-#ifdef HAVE_ELFUTILS
-#define _ELFUTILS_FEATURE_ "+ELFUTILS"
-#else
-#define _ELFUTILS_FEATURE_ "-ELFUTILS"
-#endif
-
 #ifdef HAVE_LIBIDN
 #define _IDN_FEATURE_ "+IDN"
 #else
@@ -119,18 +71,10 @@
 
 #define SYSTEMD_FEATURES                                                \
         _PAM_FEATURE_ " "                                               \
-        _AUDIT_FEATURE_ " "                                             \
         _SELINUX_FEATURE_ " "                                           \
-        _IMA_FEATURE_ " "                                               \
         _APPARMOR_FEATURE_ " "                                          \
         _SMACK_FEATURE_ " "                                             \
-        _LIBCRYPTSETUP_FEATURE_ " "                                     \
-        _GCRYPT_FEATURE_ " "                                            \
-        _GNUTLS_FEATURE_ " "                                            \
         _ACL_FEATURE_ " "                                               \
-        _XZ_FEATURE_ " "                                                \
-        _LZ4_FEATURE_ " "                                               \
         _SECCOMP_FEATURE_ " "                                           \
         _BLKID_FEATURE_ " "                                             \
-        _ELFUTILS_FEATURE_ " "                                          \
         _IDN_FEATURE_
