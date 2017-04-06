94c5f7fa9e34517edad75cd63d71dd459ec4f133
Sven Eden <yamakuzure@gmx.net>
2017-04-06 15:07:20 +0200

    Prep v228: Apply more cosmetic changes found in upstream.

--- src/shared/acl-util.c
+++ src/shared/acl-util.c
@@ -23,8 +23,11 @@
 #include <stdbool.h>
 
 #include "acl-util.h"
-#include "util.h"
+#include "alloc-util.h"
+#include "string-util.h"
 #include "strv.h"
+#include "user-util.h"
+#include "util.h"
 
 int acl_find_uid(acl_t acl, uid_t uid, acl_entry_t *entry) {
         acl_entry_t i;
