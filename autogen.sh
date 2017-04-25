8e5e788db91c2e8b619f4bcbbf2ad08eebeae6d4
Sven Eden <yamakuzure@gmx.net>
2017-04-25 13:03:09 +0200

    Prep v229: Update core build files to upstream

--- autogen.sh
+++ autogen.sh
@@ -1,33 +1,25 @@
 #!/bin/sh
 
-#  This file is part of systemd.
+#  This file is part of elogind
 #
-#  systemd is free software; you can redistribute it and/or modify it
+#  elogind is free software; you can redistribute it and/or modify it
 #  under the terms of the GNU Lesser General Public License as published by
 #  the Free Software Foundation; either version 2.1 of the License, or
 #  (at your option) any later version.
 #
-#  systemd is distributed in the hope that it will be useful, but
+#  elogind is distributed in the hope that it will be useful, but
 #  WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 #  Lesser General Public License for more details.
 #
 #  You should have received a copy of the GNU Lesser General Public License
-#  along with systemd; If not, see <http://www.gnu.org/licenses/>.
+#  along with elogind; If not, see <http://www.gnu.org/licenses/>.
 
 set -e
 
 oldpwd=$(pwd)
 topdir=$(dirname $0)
 cd $topdir
-
-# We do not need this, we are not systemd upstream!
-#if [ -f .git/hooks/pre-commit.sample ] && [ ! -f .git/hooks/pre-commit ]; then
-#        # This part is allowed to fail
-#        cp -p .git/hooks/pre-commit.sample .git/hooks/pre-commit && \
-#        chmod +x .git/hooks/pre-commit && \
-#        echo "Activated pre-commit hook." || :
-#fi
 
 intltoolize --force --automake
 autoreconf --force --install --symlink
