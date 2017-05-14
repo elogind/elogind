a14c67250f7c3ddaf732d36b1dea457c0b6a6227
Ronny Chevalier <chevalier.ronny@gmail.com>
2017-05-14 16:30:40 +0200

    env-util: fix memory leak (#5962)
    If cunescape succeeds, but the assignment is not valid, uce is not freed.
    (cherry picked from commit 16eefcafedeecf0e282add6c1eadeebcb3ad0609)

--- src/basic/env-util.c
+++ src/basic/env-util.c
@@ -800,8 +800,10 @@ int deserialize_environment(char ***environment, const char *line) {
         if (r < 0)
                 return r;
 
-        if (!env_assignment_is_valid(uce))
+        if (!env_assignment_is_valid(uce)) {
+                free(uce);
                 return -EINVAL;
+        }
 
         return strv_env_replace(environment, uce);
 }
