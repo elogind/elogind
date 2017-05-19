cadef8dcd75df1197e4d3834ac6b3260f93b7f68
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2017-05-18 22:32:19 -0400

    sd-login,test-login: return -ENODATA from sd_pid_get_unit too
    After all, we might be running on a non-elogind system.

--- src/libelogind/sd-login/sd-login.c
+++ src/libelogind/sd-login/sd-login.c
@@ -66,12 +66,16 @@ _public_ int sd_pid_get_session(pid_t pid, char **session) {
 }
 
 _public_ int sd_pid_get_unit(pid_t pid, char **unit) {
+#if 0 /// UNNEEDED by elogind
+        int r;
+#endif // 0
 
         assert_return(pid >= 0, -EINVAL);
         assert_return(unit, -EINVAL);
 
 #if 0 /// elogind does not support systemd units
-        return cg_pid_get_unit(pid, unit);
+        r = cg_pid_get_unit(pid, unit);
+        return r == -ENXIO ? -ENODATA : r;
 #else
         return -ESRCH;
 #endif // 0
