1ea8dbd9cd30a46d8f410a7f651bd3e730d2378a
Waldemar Brodkorb <wbx@openadk.org>
2017-06-15 17:44:59 +0200

    Make IDN support conditional
    [zj: rename HAVE_IDN to ENABLE_IDN]

--- src/basic/socket-util.c
+++ src/basic/socket-util.c
@@ -44,9 +44,16 @@
 #include "string-util.h"
 #include "strv.h"
 #include "user-util.h"
+//#include "utf8.h"
 #include "util.h"
 
 #if 0 /// UNNEEDED by elogind
+#ifdef ENABLE_IDN
+#  define IDN_FLAGS (NI_IDN|NI_IDN_USE_STD3_ASCII_RULES)
+#else
+#  define IDN_FLAGS 0
+#endif
+
 int socket_address_parse(SocketAddress *a, const char *s) {
         char *e, *n;
         unsigned u;
@@ -722,8 +729,7 @@ int socknameinfo_pretty(union sockaddr_union *sa, socklen_t salen, char **_ret) 
 
         assert(_ret);
 
-        r = getnameinfo(&sa->sa, salen, host, sizeof(host), NULL, 0,
-                        NI_IDN|NI_IDN_USE_STD3_ASCII_RULES);
+        r = getnameinfo(&sa->sa, salen, host, sizeof(host), NULL, 0, IDN_FLAGS);
         if (r != 0) {
                 int saved_errno = errno;
 
