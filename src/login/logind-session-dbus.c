d422cfe2a7a8253eb28e9b2c7e6192307f96383e
Sven Eden <yamakuzure@gmx.net>
2017-07-03 14:48:37 +0200

    Re-add the saving of the user state file when creating a new session.

--- src/login/logind-session-dbus.c
+++ src/login/logind-session-dbus.c
@@ -772,6 +772,10 @@ int session_send_create_reply(Session *s, sd_bus_error *error) {
          * about the result. */
         session_save(s);
 
+#if 1 /// Additionally elogind saves the user state file
+        user_save(s->user);
+#endif // 1
+
         p = session_bus_path(s);
         if (!p)
                 return -ENOMEM;
