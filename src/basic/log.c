f832565b19a316cde5afa526f0e4fc3155e01d7f
Topi Miettinen <topimiettinen@users.noreply.github.com>
2017-08-31 08:37:32 +0000

    log: reopen log for failed assertions (#6703)
    Reopen log so that failed and aborting assertions can be written to log.
    
    Closes: #6658
    (cherry picked from commit ea89a119cda917a17bd186b3c13197acfd655b12)

--- src/basic/log.c
+++ src/basic/log.c
@@ -820,6 +820,7 @@ noreturn void log_assert_failed_realm(
                 const char *file,
                 int line,
                 const char *func) {
+        log_open();
         log_assert(LOG_REALM_PLUS_LEVEL(realm, LOG_CRIT), text, file, line, func,
                    "Assertion '%s' failed at %s:%u, function %s(). Aborting.");
         abort();
@@ -831,6 +832,7 @@ noreturn void log_assert_failed_unreachable_realm(
                 const char *file,
                 int line,
                 const char *func) {
+        log_open();
         log_assert(LOG_REALM_PLUS_LEVEL(realm, LOG_CRIT), text, file, line, func,
                    "Code should not be reached '%s' at %s:%u, function %s(). Aborting.");
         abort();
