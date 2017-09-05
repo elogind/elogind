77c50dcc00126c2751bab24b1568ee1f77f175f5
shwsh <thatweirdbear@tuta.io>
2017-09-05 22:23:33 +0700

    Update test-login.c

--- src/libelogind/sd-login/test-login.c
+++ src/libelogind/sd-login/test-login.c
@@ -61,6 +61,7 @@ static void test_login(void) {
         uid_t u, u2;
         char *t, **seats, **sessions;
 
+#if 0 // elogind does not support systemd slices
         r = sd_pid_get_unit(0, &unit);
         assert_se(r >= 0 || r == -ENODATA);
         log_info("sd_pid_get_unit(0, …) → \"%s\"", strna(unit));
@@ -72,6 +73,7 @@ static void test_login(void) {
         r = sd_pid_get_slice(0, &slice);
         assert_se(r >= 0 || r == -ENODATA);
         log_info("sd_pid_get_slice(0, …) → \"%s\"", strna(slice));
+#endif
 
         r = sd_pid_get_session(0, &session);
         if (r < 0) {
@@ -304,3 +306,4 @@ int main(int argc, char* argv[]) {
 
         return 0;
 }
+
