7d729de3a70bc7a7fef69f5cea03fcbf835e118a
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2017-10-03 12:05:24 +0100

    Revert "tree-wide: use pid_is_valid() at more places"
    This reverts commit ee043777be58251e7441b4f04594e9e3792d7fb2.
    
    It broke almost everywhere it touched.  The places that
    handn't been converted, were mostly followed by special
    handling for the invalid PID `0`.  That explains why they
    tested for `pid < 0` instead of `pid <= 0`.
    
    I think that one was the first commit I reviewed, heh.

--- src/login/logind-dbus.c
+++ src/login/logind-dbus.c
@@ -348,7 +348,7 @@ static int method_get_session_by_pid(sd_bus_message *message, void *userdata, sd
         r = sd_bus_message_read(message, "u", &pid);
         if (r < 0)
                 return r;
-        if (!pid_is_valid((pid_t) pid))
+        if (pid < 0)
                 return -EINVAL;
 
         if (pid == 0) {
@@ -411,7 +411,7 @@ static int method_get_user_by_pid(sd_bus_message *message, void *userdata, sd_bu
         r = sd_bus_message_read(message, "u", &pid);
         if (r < 0)
                 return r;
-        if (!pid_is_valid((pid_t) pid))
+        if (pid < 0)
                 return -EINVAL;
 
         if (pid == 0) {
