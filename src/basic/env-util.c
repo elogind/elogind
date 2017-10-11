34cd516c6ef5bcea996fdbc089c3106ef4990d65
Lubomir Rintel <lkundrak@v3.sk>
2017-10-11 09:29:30 +0200

    basic/env-util: drop the validation when deserializing environment
    The environment variables we've serialized can quite possibly contain
    characters outside the set allowed by env_assignment_is_valid(). In
    fact, my environment seems to contain a couple of these:
    
      * TERMCAP set by screen contains a '\x7f' character
      * BASH_FUNC_module%% variable has a '%' character in name
    
    Strict check of environment variables name and value certainly makes sense for
    unit files, but not so much for deserialization of values we already had
    in our environment.
    
    (cherry picked from commit ea43bdd1d7c14e3695a4cc081e4ef4f964160dc1)

--- src/basic/env-util.c
+++ src/basic/env-util.c
@@ -810,11 +810,6 @@ int deserialize_environment(char ***environment, const char *line) {
         if (r < 0)
                 return r;
 
-        if (!env_assignment_is_valid(uce)) {
-                free(uce);
-                return -EINVAL;
-        }
-
         return strv_env_replace(environment, uce);
 }
 #endif // 0
