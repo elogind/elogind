19c4e17bdc255030e9a2ca67f1349eefdca414d2
Yu Watanabe <watanabe.yu+github@gmail.com>
2017-12-01 02:19:44 +0900

    fs-util: chase_symlinks(): remove unnecessary slash at the head
    Before this, chase_symlinks("/../../foo/bar",...) returns //foo/bar.
    This removes the unnecessary slash at the head.

--- src/basic/fs-util.c
+++ src/basic/fs-util.c
@@ -728,6 +728,10 @@ int chase_symlinks(const char *path, const char *original_root, unsigned flags, 
                                  * what we got so far. But don't allow this if the remaining path contains "../ or "./"
                                  * or something else weird. */
 
+                                /* If done is "/", as first also contains slash at the head, then remove this redundant slash. */
+                                if (streq_ptr(done, "/"))
+                                        *done = '\0';
+
                                 if (!strextend(&done, first, todo, NULL))
                                         return -ENOMEM;
 
@@ -800,6 +804,10 @@ int chase_symlinks(const char *path, const char *original_root, unsigned flags, 
                         done = first;
                         first = NULL;
                 } else {
+                        /* If done is "/", as first also contains slash at the head, then remove this redundant slash. */
+                        if (streq(done, "/"))
+                                *done = '\0';
+
                         if (!strextend(&done, first, NULL))
                                 return -ENOMEM;
                 }
