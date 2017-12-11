38a3d01be63ab0f156d62a5ad4cebe7c93bda987
Sven Eden <yamakuzure@gmx.net>
2017-12-11 17:12:50 +0100

    basic/exec-utils:do_execute() : Remove "maybe unitialized" warning

--- src/basic/exec-util.c
+++ src/basic/exec-util.c
@@ -130,7 +130,11 @@ static int do_execute(
         STRV_FOREACH(path, paths) {
                 _cleanup_free_ char *t = NULL;
                 _cleanup_close_ int fd = -1;
+#if 0 /// No "maybe uninitialized" warning in elogind
                 pid_t pid;
+#else
+                pid_t pid = 0;
+#endif // 0
 
                 t = strdup(*path);
                 if (!t)
