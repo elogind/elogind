6c33853a2de7ada8d55d411f6a407ff1b69fa526
Yu Watanabe <watanabe.yu+github@gmail.com>
2017-12-29 17:03:54 +0900

    basic: introduce *_to_string_with_check() functions
    They are used in later commits.

--- src/basic/process-util.h
+++ src/basic/process-util.h
@@ -149,6 +149,13 @@ static inline bool pid_is_valid(pid_t p) {
         return p > 0;
 }
 
+static inline int sched_policy_to_string_alloc_with_check(int n, char **s) {
+        if (!sched_policy_is_valid(n))
+                return -EINVAL;
+
+        return sched_policy_to_string_alloc(n, s);
+}
+
 #if 0 /// UNNEEDED by elogind
 int ioprio_parse_priority(const char *s, int *ret);
 #endif // 0
