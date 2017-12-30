1f0774088c4d371a570f7ca2fcb4722c46255e6d
Lennart Poettering <lennart@poettering.net>
2017-12-30 15:19:53 +0100

    sd-bus: let's use mfree() where we can

--- src/libelogind/sd-bus/sd-bus.c
+++ src/libelogind/sd-bus/sd-bus.c
@@ -3510,8 +3510,7 @@ _public_ int sd_bus_path_decode_many(const char *path, const char *path_template
         }
         va_end(list);
 
-        free(labels);
-        labels = NULL;
+        labels = mfree(labels);
         return 1;
 }
 
