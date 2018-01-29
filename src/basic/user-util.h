fa454203ef5f60bca221ab6372d4e4f2318c7150
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2018-01-29 14:23:31 +0100

    sysusers: emit a bit more info at debug level when locking fails
    This is the first error message when running unprivileged, and the message is
    unspecific, so let's at least add some logging at debug level to make this less
    confusing.

--- src/basic/user-util.h
+++ src/basic/user-util.h
@@ -71,6 +71,8 @@ int take_etc_passwd_lock(const char *root);
 #define UID_NOBODY ((uid_t) 65534U)
 #define GID_NOBODY ((gid_t) 65534U)
 
+#define ETC_PASSWD_LOCK_PATH "/etc/.pwd.lock"
+
 #if 0 /// UNNEEDED by elogind
 static inline bool uid_is_dynamic(uid_t uid) {
         return DYNAMIC_UID_MIN <= uid && uid <= DYNAMIC_UID_MAX;
