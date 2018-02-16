e614bab976056dd3b6e402a71a0a6de5b0b7572b
Lennart Poettering <lennart@poettering.net>
2018-02-16 14:59:26 +0100

    bpf: mount bpffs by default on boot
    We make heavy use of BPF functionality these days, hence expose the BPF
    file system too by default now. (Note however, that we don't actually
    make use bpf file systems object yet, but we might later on too.)

--- src/core/mount-setup.c
+++ src/core/mount-setup.c
@@ -124,6 +124,8 @@ static const MountPoint mount_table[] = {
         { "efivarfs",    "/sys/firmware/efi/efivars", "efivarfs",   NULL,                      MS_NOSUID|MS_NOEXEC|MS_NODEV,
           is_efi_boot,   MNT_NONE                   },
 #endif
+        { "bpf",         "/sys/fs/bpf",               "bpf",        NULL,                      MS_NOSUID|MS_NOEXEC|MS_NODEV,
+          NULL,          MNT_NONE,                  },
 #else
         { "cgroup",      "/sys/fs/cgroup/elogind",    "cgroup",     "none,name=elogind,release_agent="SYSTEMD_CGROUP_AGENT_PATH",xattr", MS_NOSUID|MS_NOEXEC|MS_NODEV,
           cg_is_legacy_wanted, MNT_IN_CONTAINER  },
