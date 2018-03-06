c477f8d4b5a31f89ee5bbd75218a24f276c33175
Sven Eden <yamakuzure@gmx.net>
2018-03-06 08:34:00 +0100

    tools/check-includes.pl : Prepared for check-tree.pl handling.

--- tools/check-includes.pl
+++ tools/check-includes.pl
@@ -3,6 +3,26 @@
 # checkincludes: Find files included more than once in (other) files.
 # Copyright abandoned, 2000, Niels Kristian Bech Jensen <nkbj@image.dk>.
 
+#if 0 // 5 errors and 2 warnings are inaccaptable for elogind - See PerlCritic.
+# foreach $file (@ARGV) {
+# 	open(FILE, $file) or die "Cannot open $file: $!.\n";
+# 
+# 	my %includedfiles = ();
+# 
+# 	while (<FILE>) {
+# 		if (m/^\s*#\s*include\s*[<"](\S*)[>"]/o) {
+# 			++$includedfiles{$1};
+# 		}
+# 	}
+# 	foreach $filename (keys %includedfiles) {
+# 		if ($includedfiles{$filename} > 1) {
+# 			print "$file: $filename is included more than once.\n";
+# 		}
+# 	}
+# 
+# 	close(FILE);
+# }
+#else
 use strict;
 use warnings;
 
@@ -23,3 +43,4 @@ foreach my $file (@ARGV) {
 		}
 	}
 }
+#endif // 0
