40905e905209868d09fe8c6987fee9733dd0db4f
Lennart Poettering <lennart@poettering.net>
2018-03-26 13:25:51 +0200

    fd-util: introduce fd_reopen() helper for reopening an fd
    We have the same code for this in place at various locations, let's
    unify that. Also, let's repurpose test-fs-util.c as a test for this new
    helper cal..

--- src/basic/fd-util.h
+++ src/basic/fd-util.h
@@ -102,10 +102,6 @@ int acquire_data_fd(const void *data, size_t size, unsigned flags);
 #define ERRNO_IS_DISCONNECT(r) \
         IN_SET(r, ENOTCONN, ECONNRESET, ECONNREFUSED, ECONNABORTED, EPIPE, ENETUNREACH)
 
-/* Resource exhaustion, could be our fault or general system trouble */
-#define ERRNO_IS_RESOURCE(r) \
-        IN_SET(r, ENOMEM, EMFILE, ENFILE)
-
 int fd_move_above_stdio(int fd);
 
 int rearrange_stdio(int original_input_fd, int original_output_fd, int original_error_fd);
@@ -121,3 +117,5 @@ static inline int make_null_stdio(void) {
                 (fd) = -1;                      \
                 _fd_;                           \
         })
+
+int fd_reopen(int fd, int flags);
