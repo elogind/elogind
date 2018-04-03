05301c0619add83c1426523f08336bc30e1020e2
Evgeny Vereshchagin <evvers@ya.ru>
2018-04-03 16:04:22 +0300

    core: skip the removal of cgroups in the TEST_RUN_MINIMAL mode (#8622)
    When `systemd` is run in the TEST_RUN_MINIMAL mode, it doesn't really
    set up cgroups, so it shouldn't try to remove anything.
    
    Closes https://github.com/systemd/systemd/issues/8474.
    
    (cherry picked from commit f6c63f6fc90040f0017a7cc37f3a05d5b86226d7)

--- src/core/cgroup.c
+++ src/core/cgroup.c
@@ -2340,7 +2340,7 @@ void manager_shutdown_cgroup(Manager *m, bool delete) {
 
         /* We can't really delete the group, since we are in it. But
          * let's trim it. */
-        if (delete && m->cgroup_root)
+        if (delete && m->cgroup_root && m->test_run_flags != MANAGER_TEST_RUN_MINIMAL)
                 (void) cg_trim(SYSTEMD_CGROUP_CONTROLLER, m->cgroup_root, false);
 
 #if 0 /// elogind is not init
