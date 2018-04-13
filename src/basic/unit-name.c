118491a83690985bf91d4883aa08e13c131eb084
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2018-04-13 12:38:11 +0200

    basic/unit-name: remove duplicate check
    The check that was right below already covers this case.

--- src/basic/unit-name.c
+++ src/basic/unit-name.c
@@ -251,8 +251,6 @@ int unit_name_build(const char *prefix, const char *instance, const char *suffix
         assert(suffix);
         assert(ret);
 
-        if (isempty(suffix))
-                return -EINVAL;
         if (suffix[0] != '.')
                 return -EINVAL;
 
