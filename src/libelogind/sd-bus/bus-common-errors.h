1fa1ee057d4c0cdd6ee3b0af99ed9c2bbddc1613
Lennart Poettering <lennart@poettering.net>
2018-04-16 21:41:40 +0200

    add new portable service framework
    This adds a small service "systemd-portabled" and a matching client
    "portablectl", which implement the "portable service" concept.
    
    The daemon implements the actual operations, is PolicyKit-enabled and is
    activated on demand with exit-on-idle.
    
    Both the daemon and the client are an optional build artifact, enabled
    by default rhough.

--- src/libelogind/sd-bus/bus-common-errors.h
+++ src/libelogind/sd-bus/bus-common-errors.h
@@ -45,6 +45,8 @@
 #define BUS_ERROR_NO_SUCH_GROUP_MAPPING "org.freedesktop.machine1.NoSuchGroupMapping"
 #endif // 0
 
+#define BUS_ERROR_NO_SUCH_PORTABLE_IMAGE "org.freedesktop.portable1.NoSuchImage"
+
 #define BUS_ERROR_NO_SUCH_SESSION "org.freedesktop.login1.NoSuchSession"
 #define BUS_ERROR_NO_SESSION_FOR_PID "org.freedesktop.login1.NoSessionForPID"
 #define BUS_ERROR_NO_SUCH_USER "org.freedesktop.login1.NoSuchUser"
