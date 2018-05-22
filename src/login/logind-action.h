4f0a75e5e5e87b7c955ebf98be6f7149e47ce8a7
Lennart Poettering <lennart@poettering.net>
2018-05-22 13:10:17 +0200

    tree-wide: port over all code to the new CONFIG_PARSER_PROTOTYPE() macro
    This makes most header files easier to look at. Also Emacs gets really
    slow when browsing through large sections of overly long prototypes,
    which is much improved by this macro.
    
    We should probably not do something similar with too many other cases,
    as macros like this might help readability for some, but make it worse
    for others. But I think given the complexity of this specific prototype
    and how often we use it, it's worth doing.

--- src/login/logind-action.h
+++ src/login/logind-action.h
@@ -5,20 +5,9 @@
   This file is part of systemd.
 
   Copyright 2012 Lennart Poettering
-
-  elogind is free software; you can redistribute it and/or modify it
-  under the terms of the GNU Lesser General Public License as published by
-  the Free Software Foundation; either version 2.1 of the License, or
-  (at your option) any later version.
-
-  elogind is distributed in the hope that it will be useful, but
-  WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public License
-  along with elogind; If not, see <http://www.gnu.org/licenses/>.
 ***/
+
+//#include "conf-parser.h"
 
 typedef enum HandleAction {
         HANDLE_IGNORE,
@@ -49,4 +38,5 @@ const char* handle_action_to_string(HandleAction h) _const_;
 HandleAction handle_action_from_string(const char *s) _pure_;
 
 const char* manager_target_for_action(HandleAction handle);
-int config_parse_handle_action(const char *unit, const char *filename, unsigned line, const char *section, unsigned section_line, const char *lvalue, int ltype, const char *rvalue, void *data, void *userdata);
+
+CONFIG_PARSER_PROTOTYPE(config_parse_handle_action);
