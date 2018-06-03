71dad92df98c9b9a5aeb3238dc15dd7d868378d7
Yu Watanabe <watanabe.yu+github@gmail.com>
2018-06-04 00:30:18 +0900

    conf-parser: reject utf8-invalid lines

--- src/shared/conf-parser.c
+++ src/shared/conf-parser.c
@@ -43,6 +43,7 @@
 //#include "rlimit-util.h"
 //#include "rlimit-util.h"
 //#include "rlimit-util.h"
+//#include "rlimit-util.h"
 
 int config_item_table_lookup(
                 const void *table,
@@ -219,6 +220,9 @@ static int parse_line(
 
                 return config_parse(unit, fn, NULL, sections, lookup, table, flags, userdata);
         }
+
+        if (!utf8_is_valid(l))
+                return log_syntax_invalid_utf8(unit, LOG_WARNING, filename, line, l);
 
         if (*l == '[') {
                 size_t k;
