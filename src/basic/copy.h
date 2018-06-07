45753e646137e6acceebdbaf0750fd6d5e834d9f
Lennart Poettering <lennart@poettering.net>
2018-06-07 16:03:43 +0200

    tree-wide: unify how we define bit mak enums
    Let's always write "1 << 0", "1 << 1" and so on, except where we need
    more than 31 flag bits, where we write "UINT64(1) << 0", and so on to force
    64bit values.

--- src/basic/copy.h
+++ src/basic/copy.h
@@ -13,10 +13,10 @@
 #include <sys/types.h>
 
 typedef enum CopyFlags {
-        COPY_REFLINK    = 1U << 0, /* Try to reflink */
-        COPY_MERGE      = 1U << 1, /* Merge existing trees with our new one to copy */
-        COPY_REPLACE    = 1U << 2, /* Replace an existing file if there's one */
-        COPY_SAME_MOUNT = 1U << 3, /* Don't descend recursively into other file systems, across mount point boundaries */
+        COPY_REFLINK    = 1 << 0, /* Try to reflink */
+        COPY_MERGE      = 1 << 1, /* Merge existing trees with our new one to copy */
+        COPY_REPLACE    = 1 << 2, /* Replace an existing file if there's one */
+        COPY_SAME_MOUNT = 1 << 3, /* Don't descend recursively into other file systems, across mount point boundaries */
 } CopyFlags;
 
 #if 0 /// UNNEEDED by elogind
