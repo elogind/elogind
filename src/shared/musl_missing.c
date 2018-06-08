1c9629692145891f10a36227749470d87979dd0b
Sven Eden <yamakuzure@gmx.net>
2018-06-08 16:45:25 +0200

    Prep v238: Fix elogind_daemonize(), it failed due to some misunderstandings on my side.

--- src/shared/musl_missing.c
+++ src/shared/musl_missing.c
@@ -28,29 +28,33 @@ char *program_invocation_name       = NULL;
 char *program_invocation_short_name = NULL;
 #endif // __GLIBC__
 
+const char *program_arg_name = NULL;
+
 #include "musl_missing.h"
 
 static void elogind_free_program_name(void) {
-        if (program_invocation_name)
+
+        if (program_invocation_name && (program_invocation_name != program_arg_name) && strlen(program_invocation_name))
                 program_invocation_name       = mfree(program_invocation_name);
-        if (program_invocation_short_name)
+        if (program_invocation_short_name && (program_invocation_short_name != program_arg_name) && strlen(program_invocation_short_name))
                 program_invocation_short_name = mfree(program_invocation_short_name);
 }
 
 void elogind_set_program_name(const char* pcall) {
         assert(pcall && pcall[0]);
 
+        program_arg_name = pcall;
+
         if ( ( program_invocation_name
-            && strcmp(program_invocation_name, pcall))
+            && strcmp(program_invocation_name, program_arg_name))
           || ( program_invocation_short_name
-            && strcmp(program_invocation_short_name, basename(pcall)) ) )
+            && strcmp(program_invocation_short_name, basename(program_arg_name)) ) )
                 elogind_free_program_name();
 
         if (NULL == program_invocation_name)
-                program_invocation_name       = strdup(pcall);
+                program_invocation_name       = strdup(program_arg_name);
         if (NULL == program_invocation_short_name)
-                program_invocation_short_name = strdup(basename(pcall));
-
+                program_invocation_short_name = strdup(basename(program_arg_name));
 #ifndef __GLIBC__
         atexit(elogind_free_program_name);
 #endif // __GLIBC__
