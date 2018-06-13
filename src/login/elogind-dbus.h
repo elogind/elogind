b5802a1d75408cb8fc3e97f9194c5c31c915cfd8
Sven Eden <yamakuzure@gmx.net>
2018-06-13 17:51:18 +0200

    Move method_do_shutdown_or_sleep() and its callers back to logind-sbus.c

--- src/login/elogind-dbus.h
+++ src/login/elogind-dbus.h
@@ -27,24 +27,12 @@ int execute_shutdown_or_sleep(Manager *m, InhibitWhat w,
                               HandleAction action, sd_bus_error *error);
 int manager_scheduled_shutdown_handler(sd_event_source *s, uint64_t usec,
                                        void *userdata);
-int method_halt        (sd_bus_message *message, void *userdata, sd_bus_error *error);
-int method_hibernate   (sd_bus_message *message, void *userdata, sd_bus_error *error);
-int method_hybrid_sleep(sd_bus_message *message, void *userdata, sd_bus_error *error);
-int method_poweroff    (sd_bus_message *message, void *userdata, sd_bus_error *error);
-int method_reboot      (sd_bus_message *message, void *userdata, sd_bus_error *error);
-int method_suspend     (sd_bus_message *message, void *userdata, sd_bus_error *error);
-
 
 /* prototypes for former static functions in logind-dbus.c */
 int delay_shutdown_or_sleep(Manager *m, InhibitWhat w, HandleAction action);
 int  manager_inhibit_timeout_handler(sd_event_source *s, uint64_t usec, void *userdata);
 void reset_scheduled_shutdown(Manager *m);
 int  send_prepare_for(Manager *m, InhibitWhat w, bool _active);
-int  verify_shutdown_creds(Manager *m, sd_bus_message *message, InhibitWhat w,
-                           bool interactive, const char *action,
-                           const char *action_multiple_sessions,
-                           const char *action_ignore_inhibit,
-                           sd_bus_error *error);
 
 
 #endif // ELOGIND_SRC_LOGIN_ELOGIND_DBUS_H_INCLUDED
