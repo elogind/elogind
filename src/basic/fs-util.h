ec8dd50c8d36cd807a41d549e25a24490d08eb28
Yu Watanabe <watanabe.yu+github@gmail.com>
2018-06-14 11:26:29 +0900

    fs-util: introduce fchmod_and_chown()
    The new function fchmod_and_chown() is almost same as chmod_and_chown()
    except it takes file descriptor instead of file path.

--- src/basic/fs-util.h
+++ src/basic/fs-util.h
@@ -37,6 +37,7 @@ int readlink_and_make_absolute(const char *p, char **r);
 #endif // 0
 
 int chmod_and_chown(const char *path, mode_t mode, uid_t uid, gid_t gid);
+int fchmod_and_chown(int fd, mode_t mode, uid_t uid, gid_t gid);
 
 int fchmod_umask(int fd, mode_t mode);
 int fchmod_opath(int fd, mode_t m);
