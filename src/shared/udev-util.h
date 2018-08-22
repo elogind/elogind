9dfadac9c5a5cc87aa5ab12bdaa11f583e7b0950
Yu Watanabe <watanabe.yu+github@gmail.com>
2018-08-22 16:25:17 +0900

    udev: move udev cleanup functions from udev-util.h to udev.h

--- src/shared/udev-util.h
+++ src/shared/udev-util.h
@@ -3,18 +3,11 @@
 
 
 #if 0 /// elogind needs the systems udev header
-#include "udev.h"
 #else
 #include <libudev.h>
 #endif // 0
-#include "util.h"
 
 #if 0 /// UNNEEDED by elogind
-DEFINE_TRIVIAL_CLEANUP_FUNC(struct udev_event*, udev_event_unref);
-DEFINE_TRIVIAL_CLEANUP_FUNC(struct udev_rules*, udev_rules_unref);
-DEFINE_TRIVIAL_CLEANUP_FUNC(struct udev_ctrl*, udev_ctrl_unref);
-DEFINE_TRIVIAL_CLEANUP_FUNC(struct udev_ctrl_connection*, udev_ctrl_connection_unref);
-DEFINE_TRIVIAL_CLEANUP_FUNC(struct udev_ctrl_msg*, udev_ctrl_msg_unref);
 #endif // 0
 
 #if 0 /// UNNEEDED by elogind
