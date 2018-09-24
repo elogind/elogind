f340b006aa5d833c677805077602911ad768d067
Christoph Willing <chris.willing@linux.com>
2018-09-25 08:39:53 +1000

    Deal with glibc-2.28
    Closes: https://github.com/elogind/elogind/issues/74
    Signed-off-by: Christoph Willing <chris.willing@linux.com>
    Signed-off-by: Sven Eden <sven.eden@prydeworx.com>

--- src/basic/missing.h
+++ src/basic/missing.h
@@ -1371,7 +1371,7 @@ struct fib_rule_uid_range {
 #define PF_KTHREAD 0x00200000
 #endif
 
-#if ! HAVE_STRUCT_STATX
+#if !HAVE_STRUCT_STATX_IN_SYS_STAT_H && !HAVE_STRUCT_STATX_IN_LINUX_STAT_H
 struct statx_timestamp {
         int64_t tv_sec;
         uint32_t tv_nsec;
