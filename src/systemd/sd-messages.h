507789306868304cf6df76d72393146899e53c44
Lennart Poettering <lennart@poettering.net>
2018-10-02 16:43:54 +0200

    tree-wide: drop all references to "journalctl --new-id128"
    Let's advertise "systemd-id128 new" instead.

--- src/systemd/sd-messages.h
+++ src/systemd/sd-messages.h
@@ -26,6 +26,7 @@ _SD_BEGIN_DECLARATIONS;
 /* Hey! If you add a new message here, you *must* also update the message catalog with an appropriate explanation */
 
 /* And if you add a new ID here, make sure to generate a random one with "elogind-id128 new". Do not use any other IDs,
+/* And if you add a new ID here, make sure to generate a random one with "elogind-id128 new". Do not use any other IDs,
  * and do not count them up manually. */
 
 #define SD_MESSAGE_JOURNAL_START          SD_ID128_MAKE(f7,73,79,a8,49,0b,40,8b,be,5f,69,40,50,5a,77,7b)
