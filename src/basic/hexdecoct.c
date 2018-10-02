51242175a6d0bfb7d524d05d8a78365b370d947a
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2018-10-02 12:15:22 +0200

    basic/hexdecoct: check for overflow
    LGTM was complaining:
    > Multiplication result may overflow 'int' before it is converted to 'long'.
    Fix this by changing all types to ssize_t and add a check for overflow
    while at it.
    
    (cherry picked from commit 3d6c1844744f631995af72867d5f293430d8015b)
    (cherry picked from commit a445c7af70b30fee7a7172568778881289fb6052)

--- src/basic/hexdecoct.c
+++ src/basic/hexdecoct.c
@@ -576,8 +576,7 @@ static int base64_append_width(
 
         _cleanup_free_ char *x = NULL;
         char *t, *s;
-        ssize_t slen, len, avail;
-        int line, lines;
+        ssize_t len, slen, avail, line, lines;
 
         len = base64mem(p, l, &x);
         if (len <= 0)
@@ -586,6 +585,9 @@ static int base64_append_width(
         lines = (len + width - 1) / width;
 
         slen = strlen_ptr(sep);
+        if (lines > (SSIZE_MAX - plen - 1 - slen) / (indent + width + 1))
+                return -ENOMEM;
+
         t = realloc(*prefix, plen + 1 + slen + (indent + width + 1) * lines);
         if (!t)
                 return -ENOMEM;
