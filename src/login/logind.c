0554def508b3328b70aaa6464886ad51219b9379
Stefan Stefanović <stefanx2ovic@gmail.com>
2018-11-04 18:39:03 +0100

    Register any of the signals, report all, return first error (#90 #94)
    Because SIGTERM got a NULL handler in manager_new(), an error message
    like "Failed to register SIGINT handler: Device or resource busy"
    started to show up in the logs, for example dmesg. (#90)
    
    The proposed fix did not work, and the issue had another consequence.
    If /run is not on tmpfs, and elogind gets killed, the PID file is not
    removed and elogind won't start again. (#94)
    
    The original installation of NULL handlers is now blocked, the
    setting of the signla mask has been completed, and the installation
    of the handler is now gone through without breaking. If anything goes
    wrong, all errors will be reported, and the first is then returned.
    
    Bug: #90
    Bug: #94
    Closes: #90
    Closes: #94
    Signed-off-by: Sven Eden <sven.eden@prydeworx.com>

--- src/login/logind.c
+++ src/login/logind.c
@@ -82,7 +82,7 @@ static int manager_new(Manager **ret) {
         if (r < 0)
                 return r;
 
-#if 0 /// elogind uses its own signal handler, installed at elogind_manager_new()
+#if 0 /// elogind uses its own signal handler, installed at elogind_manager_startup()
         r = sd_event_add_signal(m->event, NULL, SIGINT, NULL, NULL);
         if (r < 0)
                 return r;
@@ -1139,6 +1139,12 @@ static int manager_startup(Manager *m) {
         if (r < 0)
                 return log_error_errno(r, "Failed to register SIGHUP handler: %m");
 
+#if 1 /// install elogind specific signal handlers
+        r = elogind_manager_startup(m);
+        if (r < 0)
+                return log_error_errno(r, "Failed to register elogind signal handlers: %m");
+#endif // 1
+
         /* Connect to console */
         r = manager_connect_console(m);
         if (r < 0)
@@ -1306,7 +1312,11 @@ int main(int argc, char *argv[]) {
                 return log_error_errno(r, "Failed to create /run/systemd/machines : %m");
 #endif // 0
 
+#if 0 /// elogind also blocks SIGQUIT, and installs a signal handler for it
         assert_se(sigprocmask_many(SIG_BLOCK, NULL, SIGHUP, SIGTERM, SIGINT, -1) >= 0);
+#else
+        assert_se(sigprocmask_many(SIG_BLOCK, NULL, SIGHUP, SIGTERM, SIGINT, SIGQUIT, -1) >= 0);
+#endif // 0
 
         r = manager_new(&m);
         if (r < 0) {
