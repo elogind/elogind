3e4101d14940a7a60672bc2fed78d189f2cd0741
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2018-11-13 11:10:13 +0100

    udevd: allow more parameters to be set through udev.conf
    Rebooting to set change the kernel command line to set some udev parameters is
    inconvenient. Let's allow setting more stuff in the config file.
    
    Also drop quotes from around "info" in udev.conf. We need to accept them for
    compatibility, but there is no reason to use them.

--- src/shared/udev-util.h
+++ src/shared/udev-util.h
@@ -1,8 +1,13 @@
 /* SPDX-License-Identifier: LGPL-2.1+ */
 #pragma once
 
+//#include "time-util.h"
 
 #if 0 /// elogind needs the systems udev header
+int udev_parse_config_full(
+                unsigned *ret_children_max,
+                usec_t *ret_exec_delay_usec,
+                usec_t *ret_event_timeout_usec);
 #else
 #include <libudev.h>
 #endif // 0
@@ -11,5 +16,7 @@
 #endif // 0
 
 #if 0 /// UNNEEDED by elogind
-int udev_parse_config(void);
 #endif // 0
+static inline int udev_parse_config(void) {
+        return udev_parse_config_full(NULL, NULL, NULL);
+}
