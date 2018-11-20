3ee0844b7d087bdd8a0cacbd1fa212f1ada02af8
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2018-11-20 14:03:46 +0100

    basic/main-func: also close the pager automatically
    We generally want to close the pager last. This patch closes the pager last,
    after the static destuctor calls. This means that they can do logging and such
    like during normal program runtime.

--- src/basic/main-func.h
+++ src/basic/main-func.h
@@ -3,21 +3,25 @@
 
 //#include <stdlib.h>
 
-/* Negative return values from impl are mapped to EXIT_FAILURE, and
- * everything else means success! */
-#define DEFINE_MAIN_FUNCTION(impl)                                      \
+//#include "pager.h"
+//#include "static-destruct.h"
+
+#define _DEFINE_MAIN_FUNCTION(impl, ret)                                \
         int main(int argc, char *argv[]) {                              \
                 int r;                                                  \
                 r = impl(argc, argv);                                   \
-                return r < 0 ? EXIT_FAILURE : EXIT_SUCCESS;             \
+                static_destruct();                                      \
+                pager_close();                                          \
+                return ret;                                             \
         }
+
+/* Negative return values from impl are mapped to EXIT_FAILURE, and
+ * everything else means success! */
+#define DEFINE_MAIN_FUNCTION(impl)                                      \
+        _DEFINE_MAIN_FUNCTION(impl, r < 0 ? EXIT_FAILURE : EXIT_SUCCESS)
 
 /* Zero is mapped to EXIT_SUCCESS, and both negative and positive values
  * are mapped to EXIT_FAILURE.
  * Note: this means "true" maps to EXIT_FAILURE. */
 #define DEFINE_MAIN_FUNCTION_WITH_POSITIVE_FAILURE(impl)                \
-        int main(int argc, char *argv[]) {                              \
-                int r;                                                  \
-                r = impl(argc, argv);                                   \
-                return r != 0 ? EXIT_FAILURE : EXIT_SUCCESS;            \
-        }
+        _DEFINE_MAIN_FUNCTION(impl, r != 0 ? EXIT_FAILURE : EXIT_SUCCESS)
