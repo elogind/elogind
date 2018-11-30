1aa29095539fd462aa2264dea18813d7db11cff6
Lennart Poettering <lennart@poettering.net>
2018-11-30 21:06:16 +0100

    fileio: remove unnecessary initialization

--- src/basic/fileio.c
+++ src/basic/fileio.c
@@ -1486,7 +1486,7 @@ int open_tmpfile_linkable(const char *target, int flags, char **ret_path) {
 #endif // 0
 
 int open_serialization_fd(const char *ident) {
-        int fd = -1;
+        int fd;
 
         fd = memfd_create(ident, MFD_CLOEXEC);
         if (fd < 0) {
