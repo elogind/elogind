a7bf3a1846d569166f7ea640ed2a71c05af0e4a7
Yu Watanabe <watanabe.yu+github@gmail.com>
2018-12-06 06:49:18 +0100

    missing: move definition of secure_getenv() to missing_stdlib.h

--- src/basic/missing.h
+++ src/basic/missing.h
@@ -6,7 +6,6 @@
 #include <errno.h>
 #include <inttypes.h>
 //#include <linux/falloc.h>
-#include <stdlib.h>
 #include <sys/syscall.h>
 
 #else
@@ -72,14 +71,6 @@
 
 #ifndef DM_DEFERRED_REMOVE
 #define DM_DEFERRED_REMOVE (1 << 17)
-#endif
-
-#if ! HAVE_SECURE_GETENV
-#  if HAVE___SECURE_GETENV
-#    define secure_getenv __secure_getenv
-#  else
-#    error "neither secure_getenv nor __secure_getenv are available"
-#  endif
 #endif
 
 #ifndef CIFS_MAGIC_NUMBER
@@ -150,6 +141,7 @@
 //#include "missing_random.h"
 //#include "missing_resource.h"
 //#include "missing_socket.h"
+//#include "missing_stdlib.h"
 //#include "missing_timerfd.h"
 //#include "missing_type.h"
 
