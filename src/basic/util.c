04f7b3cc09a6a6d4d87140705c125c1695512cf1
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2018-12-19 09:29:32 +0100

    Remove use of PACKAGE_STRING
    PACKAGE_VERSION is more explicit, and also, we don't pretend that changing the
    project name in meson.build has any real effect. "systemd" is embedded in a
    thousand different places, so let's just use the hardcoded string consistently.
    This is mostly in preparation for future changes.

--- src/basic/util.c
+++ src/basic/util.c
@@ -564,7 +564,7 @@ uint64_t system_tasks_max_scale(uint64_t v, uint64_t max) {
 }
 
 int version(void) {
-        puts(PACKAGE_STRING "\n"
+        puts("elogind " PACKAGE_VERSION "\n"
              SYSTEMD_FEATURES);
         return 0;
 }
