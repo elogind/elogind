systemd/systemd

docs: add a simple, auto-generated index.md
This is useful for the github pages feature

tools/make-index-md.sh
@@ -1,0 +1,15 @@
+#!/bin/sh
+
+set -eu
+
+cd "$@"/docs/
+(
+        echo "# systemd Documentation"
+
+        for f in *.md ; do
+                if [ "x$f" != "xindex.md" ] ; then
+                        t=`grep "^# " "$f" | head -n 1 | sed -e 's/^#\s*//'`
+                        echo -e "\n* [$t]($f)"
+                fi
+        done
+) > index.md

elogind/elogind

tools/make-index-md.sh
@@ -1,0 +1,15 @@
+#!/bin/sh
+
+set -eu
+
+cd "$@"/docs/
+(
+        echo "# elogind Documentation"
+
+        for f in *.md ; do
+                if [ "x$f" != "xindex.md" ] ; then
+                        t=`grep "^# " "$f" | head -n 1 | sed -e 's/^#\s*//'`
+                        echo -e "\n* [$t]($f)"
+                fi
+        done
+) > index.md