3bc3087346efca936e2b6ae6bbb9c0bb7558548e
Sven Eden <sven.eden@prydeworx.com>
2019-02-08 18:55:01 +0100

    Prep 240: Fix migration flaws and missing updates in tools/ [13/15]

--- tools/make-index-md.sh
+++ tools/make-index-md.sh
@@ -4,12 +4,33 @@ set -eu
 
 cd "$@"/docs/
 (
-        echo "# elogind Documentation"
+        echo -e "# elogind Documentation\n"
 
         for f in *.md ; do
                 if [ "x$f" != "xindex.md" ] ; then
                         t=`grep "^# " "$f" | head -n 1 | sed -e 's/^#\s*//'`
-                        echo -e "\n* [$t]($f)"
+
+                        if [ "x$f" = "xCODE_OF_CONDUCT.md" -o "x$f" = "xCONTRIBUTING.md" ] ; then
+                                # For some reason GitHub refuses to generate
+                                # HTML versions of these two documents,
+                                # probably because they are in some way special
+                                # in GitHub behaviour (as they are shown as
+                                # links in the issue submission form). Let's
+                                # work around this limitation by linking to
+                                # their repository browser version
+                                # instead. This might not even be such a bad
+                                # thing, given that the issue submission form
+                                # and our index file thus link to the same
+                                # version.
+                                u="https://github.com/systemd/systemd/blob/master/docs/$f"
+                        else
+#if 0 /// We point to the elogind repository instead
+#                                 u="https://systemd.io/"`echo "$f" | sed -e 's/.md$//'`
+#else
+                                u="https://github.com/elogind/elogind/blob/master/docs/"`echo "$f" | sed -e 's/.md$//'`
+#endif // 0
+                        fi
+                        echo "* [$t]($u)"
                 fi
         done
 ) > index.md
