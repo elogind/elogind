systemd/systemd

man: move all config file options to one section
We had "SYSTEM MANAGER DIRECTIVES" which was a misnomer already, because
it also listed user manager stuff. Let's make this a more general section
and move the items for other services there too (from "MISCELANENOUS").

tools/make-directive-index.py
@@ -98,12 +98,12 @@ TEMPLATE = '''\
         </refsect1>
 
         <refsect1>
-                <title>System manager directives</title>
+                <title>Program configuration options</title>
 
                 <para>Directives for configuring the behaviour of the
-                systemd process.</para>
+                systemd process and other tools through configuration files.</para>
 
-                <variablelist id='systemd-directives' />
+                <variablelist id='config-directives' />
         </refsect1>
 
         <refsect1>

elogind/elogind

tools/make-directive-index.py
@@ -98,12 +98,12 @@ TEMPLATE = '''\
         </refsect1>
 
         <refsect1>
-                <title>System manager directives</title>
+                <title>Program configuration options</title>
 
                 <para>Directives for configuring the behaviour of the
-                elogind process.</para>
+                elogind process and other tools through configuration files.</para>
 
-                <variablelist id='elogind-directives' />
+                <variablelist id='config-directives' />
         </refsect1>
 
         <refsect1>