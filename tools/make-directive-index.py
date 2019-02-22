systemd/systemd

man: add a new directives section for .nspawn
They is quite a bit of those directives and they were in "MISCELLANEOUS" because
they don't quite fit anywhere. When the OCI-compat stuff is merged, there'll
be even more, so let's make a separate section for them.

tools/make-directive-index.py
@@ -98,6 +98,15 @@ TEMPLATE = '''\
         </refsect1>
 
         <refsect1>
+                <title><citerefentry><refentrytitle>systemd.nspawn</refentrytitle><manvolnum>5</manvolnum></citerefentry>
+                directives</title>
+
+                <para>Directives for configuring systemd-nspawn containers.</para>
+
+                <variablelist id='nspawn-directives' />
+        </refsect1>
+
+        <refsect1>
                 <title>Program configuration options</title>
 
                 <para>Directives for configuring the behaviour of the
@@ -107,7 +116,7 @@ TEMPLATE = '''\
         </refsect1>
 
         <refsect1>
-                <title>command line options</title>
+                <title>Command line options</title>
 
                 <para>Command-line options accepted by programs in the
                 systemd suite.</para>

elogind/elogind

tools/make-directive-index.py
@@ -98,6 +98,15 @@ TEMPLATE = '''\
         </refsect1>
 
         <refsect1>
+                <title><citerefentry><refentrytitle>elogind.nspawn</refentrytitle><manvolnum>5</manvolnum></citerefentry>
+                directives</title>
+
+                <para>Directives for configuring elogind-nspawn containers.</para>
+
+                <variablelist id='nspawn-directives' />
+        </refsect1>
+
+        <refsect1>
                 <title>Program configuration options</title>
 
                 <para>Directives for configuring the behaviour of the
@@ -107,7 +116,7 @@ TEMPLATE = '''\
         </refsect1>
 
         <refsect1>
-                <title>command line options</title>
+                <title>Command line options</title>
 
                 <para>Command-line options accepted by programs in the
                 elogind suite.</para>