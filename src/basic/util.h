2c4b028a21bb923064c49a9c459708fed187de05
Lennart Poettering <lennart@poettering.net>
2019-03-13 11:35:47 +0100

    util: split out some stuff into a new file limits-util.[ch]

--- src/basic/util.h
+++ src/basic/util.h
@@ -245,12 +245,6 @@ static inline unsigned log2u_round_up(unsigned x) {
 
 int container_get_leader(const char *machine, pid_t *pid);
 
-uint64_t physical_memory(void);
-uint64_t physical_memory_scale(uint64_t v, uint64_t max);
-
-uint64_t system_tasks_max(void);
-uint64_t system_tasks_max_scale(uint64_t v, uint64_t max);
-
 int version(void);
 
 #if 0 /// UNNEEDED by elogind
