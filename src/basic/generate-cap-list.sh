b33293869340ccdcb4cbb911c1f04faac9c69323
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2019-04-04 14:10:42 +0200

    scripts: use 4 space indentation
    We had all kinds of indentation: 2 sp, 3 sp, 4 sp, 8 sp, and mixed.
    4 sp was the most common, in particular the majority of scripts under test/
    used that. Let's standarize on 4 sp, because many commandlines are long and
    there's a lot of nesting, and with 8sp indentation less stuff fits. 4 sp
    also seems to be the default indentation, so this will make it less likely
    that people will mess up if they don't load the editor config. (I think people
    often use vi, and vi has no support to load project-wide configuration
    automatically. We distribute a .vimrc file, but it is not loaded by default,
    and even the instructions in it seem to discourage its use for security
    reasons.)
    
    Also remove the few vim config lines that were left. We should either have them
    on all files, or none.
    
    Also remove some strange stuff like '#!/bin/env bash', yikes.

--- src/basic/generate-cap-list.sh
+++ src/basic/generate-cap-list.sh
@@ -3,8 +3,8 @@ set -eu
 
 #if 0 /// elogind needs musl_missing.sh, which is in shared.
 # $1 -dM -include linux/capability.h -include "$2" -include "$3" - </dev/null | \
-#         awk '/^#define[ \t]+CAP_[A-Z_]+[ \t]+/ { print $2; }' | \
-#         grep -v CAP_LAST_CAP
+#     awk '/^#define[ \t]+CAP_[A-Z_]+[ \t]+/ { print $2; }' | \
+#     grep -v CAP_LAST_CAP
 #else
 $1 -dM -include linux/capability.h -I"$2"/src/shared -include "$3" -include "$4" - </dev/null | \
         awk '/^#define[ \t]+CAP_[A-Z_]+[ \t]+/ { print $2; }' | \
