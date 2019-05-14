1df40f29cec53ab1936dff6fe8b1c6d0f540be19
Zach Smith <z@zxmth.us>
2019-05-13 20:01:36 -0700

    fix typo in wakealarm message

--- src/shared/sleep-config.c
+++ src/shared/sleep-config.c
@@ -404,7 +404,7 @@ static bool can_s2h(Manager *m) {
         r = access("/sys/class/rtc/rtc0/wakealarm", W_OK);
         if (r < 0) {
                 log_full(errno == ENOENT ? LOG_DEBUG : LOG_WARNING,
-                         "/sys/class/rct/rct0/wakealarm is not writable %m");
+                         "/sys/class/rtc/rtc0/wakealarm is not writable %m");
                 return false;
         }
 
