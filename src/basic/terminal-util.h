0267a910b7c803b9b888acbbef69d5f8358bd9b5
Franck Bui <fbui@suse.com>
2019-06-07 10:17:11 +0200

    terminal-util: introduce openpt_allocate()
    Allocating a pty is done in a couple of places so let's introduce a new helper
    which does the job.
    
    Also the new function, as well as openpt_in_namespace(), returns both pty
    master and slave so the callers don't need to know about the pty slave
    allocation details.
    
    For the same reasons machine_openpt() prototype has also been changed to return
    both pty master and slave so callers don't need to allocate a pty slave which
    might be in a different namespace.
    
    Finally openpt_in_namespace() has been renamed into
    openpt_allocate_in_namespace().

--- src/basic/terminal-util.h
+++ src/basic/terminal-util.h
@@ -168,9 +168,9 @@ int getttyname_harder(int fd, char **r);
 
 #if 0 /// UNNEEDED by elogind
 int ptsname_malloc(int fd, char **ret);
-int ptsname_namespace(int pty, char **ret);
 
-int openpt_in_namespace(pid_t pid, int flags);
+int openpt_allocate(int flags, char **ret_slave);
+int openpt_allocate_in_namespace(pid_t pid, int flags, char **ret_slave);
 int open_terminal_in_namespace(pid_t pid, const char *name, int mode);
 #endif // 0
 
