c53ce0296d2d540c363c66ebebc8ce9f39c1dcf7
Yu Watanabe <watanabe.yu+github@gmail.com>
2019-07-03 23:27:27 +0900

    sd-event: replace snprintf() with strpcpyf()

--- src/test/test-strxcpyx.c
+++ src/test/test-strxcpyx.c
@@ -1,5 +1,6 @@
 /* SPDX-License-Identifier: LGPL-2.1+ */
 
+//#include <stdio.h>
 #include <string.h>
 
 #include "string-util.h"
@@ -80,6 +81,24 @@ static void test_strscpyl(void) {
         assert_se(space_left == 10);
 }
 
+static void test_sd_event_code_migration(void) {
+        char b[100 * DECIMAL_STR_MAX(unsigned) + 1];
+        char c[100 * DECIMAL_STR_MAX(unsigned) + 1], *p;
+        unsigned i;
+        size_t l;
+        int o;
+
+        for (i = o = 0; i < 100; i++)
+                o += snprintf(&b[o], sizeof(b) - o, "%u ", i);
+
+        p = c;
+        l = sizeof(c);
+        for (i = 0; i < 100; i++)
+                l = strpcpyf(&p, l, "%u ", i);
+
+        assert_se(streq(b, c));
+}
+
 int main(int argc, char *argv[]) {
         test_strpcpy();
 #if 0 /// UNNEEDED by elogind
@@ -88,6 +107,8 @@ int main(int argc, char *argv[]) {
 #endif // 0
         test_strscpy();
         test_strscpyl();
+
+        test_sd_event_code_migration();
 
         return 0;
 }
