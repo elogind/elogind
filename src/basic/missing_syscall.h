9161101b4bb0cff7ee38613e996fd54cee56b32e
Dan Streetman <ddstreet@canonical.com>
2019-08-14 21:08:36 -0400

    src/basic/missing_syscall: add comment lines for PR 13319 changes
    Add a comment line explaining that the syscall defines might be
    defined to invalid negative numbers, as libseccomp redefines them
    to negative numbers if not defined by the kernel headers, which is
    not obvious just from reading the code checking for defined && > 0

--- src/basic/missing_syscall.h
+++ src/basic/missing_syscall.h
@@ -34,6 +34,7 @@ static inline int missing_pivot_root(const char *new_root, const char *put_old) 
 /* ======================================================================= */
 
 #if !HAVE_MEMFD_CREATE
+/* may be (invalid) negative number due to libseccomp, see PR 13319 */
 #  if ! (defined __NR_memfd_create && __NR_memfd_create > 0)
 #    if defined __NR_memfd_create
 #      undef __NR_memfd_create
@@ -80,6 +81,7 @@ static inline int missing_memfd_create(const char *name, unsigned int flags) {
 /* ======================================================================= */
 
 #if !HAVE_GETRANDOM
+/* may be (invalid) negative number due to libseccomp, see PR 13319 */
 #  if ! (defined __NR_getrandom && __NR_getrandom > 0)
 #    if defined __NR_getrandom
 #      undef __NR_getrandom
@@ -142,6 +144,7 @@ static inline pid_t missing_gettid(void) {
 /* ======================================================================= */
 
 #if !HAVE_NAME_TO_HANDLE_AT
+/* may be (invalid) negative number due to libseccomp, see PR 13319 */
 #  if ! (defined __NR_name_to_handle_at && __NR_name_to_handle_at > 0)
 #    if defined __NR_name_to_handle_at
 #      undef __NR_name_to_handle_at
@@ -182,6 +185,7 @@ static inline int missing_name_to_handle_at(int fd, const char *name, struct fil
 /* ======================================================================= */
 
 #if !HAVE_SETNS
+/* may be (invalid) negative number due to libseccomp, see PR 13319 */
 #  if ! (defined __NR_setns && __NR_setns > 0)
 #    if defined __NR_setns
 #      undef __NR_setns
@@ -222,6 +226,7 @@ static inline pid_t raw_getpid(void) {
 /* ======================================================================= */
 
 #if !HAVE_RENAMEAT2
+/* may be (invalid) negative number due to libseccomp, see PR 13319 */
 #  if ! (defined __NR_renameat2 && __NR_renameat2 > 0)
 #    if defined __NR_renameat2
 #      undef __NR_renameat2
@@ -322,6 +327,7 @@ static inline key_serial_t missing_request_key(const char *type, const char *des
 /* ======================================================================= */
 
 #if !HAVE_COPY_FILE_RANGE
+/* may be (invalid) negative number due to libseccomp, see PR 13319 */
 #  if ! (defined __NR_copy_file_range && __NR_copy_file_range > 0)
 #    if defined __NR_copy_file_range
 #      undef __NR_copy_file_range
@@ -364,6 +370,7 @@ static inline ssize_t missing_copy_file_range(int fd_in, loff_t *off_in,
 
 #if 0 /// elogind does not control/manage Berkeley packet filters
 #if !HAVE_BPF
+/* may be (invalid) negative number due to libseccomp, see PR 13319 */
 #  if ! (defined __NR_bpf && __NR_bpf > 0)
 #    if defined __NR_bpf
 #      undef __NR_bpf
@@ -405,6 +412,7 @@ static inline int missing_bpf(int cmd, union bpf_attr *attr, size_t size) {
 /* ======================================================================= */
 
 #ifndef __IGNORE_pkey_mprotect
+/* may be (invalid) negative number due to libseccomp, see PR 13319 */
 #  if ! (defined __NR_pkey_mprotect && __NR_pkey_mprotect > 0)
 #    if defined __NR_pkey_mprotect
 #      undef __NR_pkey_mprotect
@@ -440,6 +448,7 @@ static inline int missing_bpf(int cmd, union bpf_attr *attr, size_t size) {
 /* ======================================================================= */
 
 #if !HAVE_STATX
+/* may be (invalid) negative number due to libseccomp, see PR 13319 */
 #  if ! (defined __NR_statx && __NR_statx > 0)
 #    if defined __NR_statx
 #      undef __NR_statx
