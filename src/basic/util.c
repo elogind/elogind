e08b4f6f2b84493680c412507474eae6ca0a1b62
Franck Bui <fbui@suse.com>
2019-10-01 14:31:14 +0200

    pid1: restore the original environment passed by the kernel when switching to a new system manager
    PID1 may modified the environment passed by the kernel when it starts
    running. Commit 9d48671c62de133a2b9fe7c31e70c0ff8e68f2db unset $HOME for
    example.
    
    In case PID1 is going to switch to a new root and execute a new system manager
    which is not systemd, we should restore the original environment as the new
    manager might expect some variables to be set by default (more specifically
    $HOME).

--- src/basic/util.c
+++ src/basic/util.c
@@ -39,6 +39,7 @@
 #include "set.h"
 #include "signal-util.h"
 #include "stat-util.h"
+#include "static-destruct.h"
 #include "string-util.h"
 #include "strv.h"
 #include "time-util.h"
@@ -50,10 +51,13 @@
 int saved_argc = 0;
 char **saved_argv = NULL;
 #if 0 /// UNNEEDED by elogind
+char **saved_env = NULL;
 static int saved_in_initrd = -1;
 #endif // 0
 
 #if 0 /// UNNEEDED by elogind
+STATIC_DESTRUCTOR_REGISTER(saved_env, strv_freep);
+
 bool kexec_loaded(void) {
        _cleanup_free_ char *s = NULL;
 
@@ -304,3 +308,7 @@ void disable_coredumps(void) {
                 log_debug_errno(r, "Failed to turn off coredumps, ignoring: %m");
 }
 #endif // 0
+
+void save_env(void) {
+        saved_env = strv_copy(environ);
+}
--- src/basic/util.h
+++ src/basic/util.h
@@ -32,6 +32,9 @@ static inline void save_argc_argv(int argc, char **argv) {
 }
 
 #if 0 /// UNNEEDED by elogind
+extern char **saved_env;
+void save_env(void);
+
 bool kexec_loaded(void);
 
 int prot_from_flags(int flags) _const_;
