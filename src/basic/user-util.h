08b2115b5ac7589f7fa44f63300d01227d9f7e46
Lennart Poettering <lennart@poettering.net>
2019-11-13 23:13:42 +0100

    shared: split out crypt() specific helpers into its own .c/.h in src/shared/
    This way we can use libxcrypt specific functionality such as
    crypt_gensalt() and thus take benefit of the newer algorithms libxcrypt
    implements. (Also adds support for a new env var $SYSTEMD_CRYPT_PREFIX
    which may be used to select the hash algorithm to use for libxcrypt.)
    
    Also, let's move the weird crypt.h inclusion into libcrypt.h so that
    there's a single place for it.

--- src/basic/user-util.h
+++ src/basic/user-util.h
@@ -156,7 +156,6 @@ int fgetsgent_sane(FILE *stream, struct sgrp **sg);
 int putsgent_sane(const struct sgrp *sg, FILE *stream);
 #endif
 
-int make_salt(char **ret);
 #endif // 0
 
 bool is_nologin_shell(const char *shell);
