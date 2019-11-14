d38fca36f58347d2b4470009a065541844b613d9
Lennart Poettering <lennart@poettering.net>
2019-11-14 14:51:04 +0100

    xattr-util: add flistxattr_malloc() that returns a NULSTR

--- src/basic/xattr-util.h
+++ src/basic/xattr-util.h
@@ -25,3 +25,5 @@ int fd_getcrtime(int fd, usec_t *usec);
 int path_getcrtime(const char *p, usec_t *usec);
 int fd_getcrtime_at(int dirfd, const char *name, usec_t *usec, int flags);
 #endif // 0
+
+int flistxattr_malloc(int fd, char **ret);
