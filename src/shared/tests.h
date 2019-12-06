1de607b1c0f06cdffc4b6bb87bd39f1ac2f23dcc
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2019-12-06 21:51:59 +0100

    tests: move unit files to units/ subdirectory
    We have a bazillion of those unit files, and keeping them all directly in tests/
    has become rather unwieldy.

--- src/shared/tests.h
+++ src/shared/tests.h
@@ -21,7 +21,7 @@ static inline bool manager_errno_skip_test(int r) {
 
 char* setup_fake_runtime_dir(void);
 int enter_cgroup_subroot(char **ret_cgroup);
-const char* get_testdata_dir(void);
+int get_testdata_dir(const char *suffix, char **ret);
 const char* get_catalog_dir(void);
 #endif // 0
 bool slow_tests_enabled(void);
