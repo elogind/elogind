systemd/systemd

tests: get rid of test-helper.[ch] completely
I don't think there's any particular reason to keep those functions in a separate
file.

src/shared/tests.h
@@ -3,6 +3,8 @@
 
 #include <stdbool.h>
 
+#include "sd-daemon.h"
+
 #include "macro.h"
 
 static inline bool manager_errno_skip_test(int r) {
@@ -17,6 +19,7 @@ static inline bool manager_errno_skip_test(int r) {
 }
 
 char* setup_fake_runtime_dir(void);
+int enter_cgroup_subroot(void);
 const char* get_testdata_dir(void);
 const char* get_catalog_dir(void);
 bool slow_tests_enabled(void);
@@ -29,3 +32,10 @@ bool have_namespaces(void);
 /* We use the small but non-trivial limit here */
 #define CAN_MEMLOCK_SIZE (512 * 1024U)
 bool can_memlock(void);
+
+#define TEST_REQ_RUNNING_SYSTEMD(x)                                 \
+        if (sd_booted() > 0) {                                      \
+                x;                                                  \
+        } else {                                                    \
+                printf("systemd not booted skipping '%s'\n", #x);   \
+        }

elogind/elogind

src/shared/tests.h
@@ -4,6 +4,8 @@
 #include <stdbool.h>
 
 #if 0 /// UNNEEDED by elogind
+#include "sd-daemon.h"
+
 #include "macro.h"
 
 static inline bool manager_errno_skip_test(int r) {
@@ -18,6 +20,7 @@ static inline bool manager_errno_skip_test(int r) {
 }
 
 char* setup_fake_runtime_dir(void);
+int enter_cgroup_subroot(void);
 const char* get_testdata_dir(void);
 const char* get_catalog_dir(void);
 #endif // 0
@@ -31,3 +34,10 @@ bool have_namespaces(void);
 /* We use the small but non-trivial limit here */
 #define CAN_MEMLOCK_SIZE (512 * 1024U)
 bool can_memlock(void);
+
+#define TEST_REQ_RUNNING_SYSTEMD(x)                                 \
+        if (sd_booted() > 0) {                                      \
+                x;                                                  \
+        } else {                                                    \
+                printf("elogind not booted skipping '%s'\n", #x);   \
+        }