poettering/systemd

logind: port to UserRecord object
This changes the user tracking of logind to use the new-style UserRecord
object.

In a later commit this enables us to do per-user resource management.

src/login/logind-core.c
@@ -28,6 +28,7 @@
 #include "terminal-util.h"
 #include "udev-util.h"
 #include "user-util.h"
+#include "userdb.h"
 
 void manager_reset_config(Manager *m) {
         assert(m);
@@ -139,21 +140,18 @@ int manager_add_session(Manager *m, const char *id, Session **_session) {
 
 int manager_add_user(
                 Manager *m,
-                uid_t uid,
-                gid_t gid,
-                const char *name,
-                const char *home,
+                UserRecord *ur,
                 User **_user) {
 
         User *u;
         int r;
 
         assert(m);
-        assert(name);
+        assert(ur);
 
-        u = hashmap_get(m->users, UID_TO_PTR(uid));
+        u = hashmap_get(m->users, UID_TO_PTR(ur->uid));
         if (!u) {
-                r = user_new(&u, m, uid, gid, name, home);
+                r = user_new(&u, m, ur);
                 if (r < 0)
                         return r;
         }
@@ -169,32 +167,35 @@ int manager_add_user_by_name(
                 const char *name,
                 User **_user) {
 
-        const char *home = NULL;
-        uid_t uid;
-        gid_t gid;
+        _cleanup_(user_record_unrefp) UserRecord *ur = NULL;
         int r;
 
         assert(m);
         assert(name);
 
-        r = get_user_creds(&name, &uid, &gid, &home, NULL, 0);
+        r = userdb_by_name(name, 0, &ur);
         if (r < 0)
                 return r;
 
-        return manager_add_user(m, uid, gid, name, home, _user);
+        return manager_add_user(m, ur, _user);
 }
 
-int manager_add_user_by_uid(Manager *m, uid_t uid, User **_user) {
-        struct passwd *p;
+int manager_add_user_by_uid(
+                Manager *m,
+                uid_t uid,
+                User **_user) {
+
+        _cleanup_(user_record_unrefp) UserRecord *ur = NULL;
+        int r;
 
         assert(m);
+        assert(uid_is_valid(uid));
 
-        errno = 0;
-        p = getpwuid(uid);
-        if (!p)
-                return errno_or_else(ENOENT);
+        r = userdb_by_uid(uid, 0, &ur);
+        if (r < 0)
+                return r;
 
-        return manager_add_user(m, uid, p->pw_gid, p->pw_name, p->pw_dir, _user);
+        return manager_add_user(m, ur, _user);
 }
 
 int manager_add_inhibitor(Manager *m, const char* id, Inhibitor **ret) {

src/login/logind.h
@@ -12,6 +12,7 @@
 #include "list.h"
 #include "set.h"
 #include "time-util.h"
+#include "user-record.h"
 
 typedef struct Manager Manager;
 
@@ -28,7 +29,7 @@ struct Manager {
         Hashmap *seats;
         Hashmap *sessions;
         Hashmap *sessions_by_leader;
-        Hashmap *users;
+        Hashmap *users;  /* indexed by UID */
         Hashmap *inhibitors;
         Hashmap *buttons;
         Hashmap *brightness_writers;
@@ -131,7 +132,7 @@ int manager_add_device(Manager *m, const char *sysfs, bool master, Device **_dev
 int manager_add_button(Manager *m, const char *name, Button **_button);
 int manager_add_seat(Manager *m, const char *id, Seat **_seat);
 int manager_add_session(Manager *m, const char *id, Session **_session);
-int manager_add_user(Manager *m, uid_t uid, gid_t gid, const char *name, const char *home, User **_user);
+int manager_add_user(Manager *m, UserRecord *ur, User **_user);
 int manager_add_user_by_name(Manager *m, const char *name, User **_user);
 int manager_add_user_by_uid(Manager *m, uid_t uid, User **_user);
 int manager_add_inhibitor(Manager *m, const char* id, Inhibitor **_inhibitor);

elogind/elogind

src/login/logind-core.c
@@ -29,6 +29,7 @@
 #include "user-util.h"
 /// Additional includes needed by elogind
 #include "elogind.h"
+#include "userdb.h"
 
 void manager_reset_config(Manager *m) {
         assert(m);
@@ -207,21 +208,18 @@ int manager_add_session(Manager *m, const char *id, Session **ret_session) {
 
 int manager_add_user(
                 Manager *m,
-                uid_t uid,
-                gid_t gid,
-                const char *name,
-                const char *home,
+                UserRecord *ur,
                 User **ret_user) {
 
         User *u;
         int r;
 
         assert(m);
-        assert(name);
+        assert(ur);
 
-        u = hashmap_get(m->users, UID_TO_PTR(uid));
+        u = hashmap_get(m->users, UID_TO_PTR(ur->uid));
         if (!u) {
-                r = user_new(&u, m, uid, gid, name, home);
+                r = user_new(&u, m, ur);
                 if (r < 0)
                         return r;
         }
@@ -237,32 +235,35 @@ int manager_add_user_by_name(
                 const char *name,
                 User **ret_user) {
 
-        const char *home = NULL;
-        uid_t uid;
-        gid_t gid;
+        _cleanup_(user_record_unrefp) UserRecord *ur = NULL;
         int r;
 
         assert(m);
         assert(name);
 
-        r = get_user_creds(&name, &uid, &gid, &home, NULL, 0);
+        r = userdb_by_name(name, 0, &ur);
         if (r < 0)
                 return r;
 
-        return manager_add_user(m, uid, gid, name, home, ret_user);
+        return manager_add_user(m, ur, ret_user);
 }
 
-int manager_add_user_by_uid(Manager *m, uid_t uid, User **ret_user) {
-        struct passwd *p;
+int manager_add_user_by_uid(
+                Manager *m,
+                uid_t uid,
+                User **ret_user) {
+
+        _cleanup_(user_record_unrefp) UserRecord *ur = NULL;
+        int r;
 
         assert(m);
+        assert(uid_is_valid(uid));
 
-        errno = 0;
-        p = getpwuid(uid);
-        if (!p)
-                return errno_or_else(ENOENT);
+        r = userdb_by_uid(uid, 0, &ur);
+        if (r < 0)
+                return r;
 
-        return manager_add_user(m, uid, p->pw_gid, p->pw_name, p->pw_dir, ret_user);
+        return manager_add_user(m, ur, ret_user);
 }
 
 int manager_add_inhibitor(Manager *m, const char* id, Inhibitor **ret) {

src/login/logind.h
@@ -12,6 +12,7 @@
 #include "list.h"
 #include "set.h"
 #include "time-util.h"
+#include "user-record.h"
 
 typedef struct Manager Manager;
 
@@ -37,7 +38,7 @@ struct Manager {
         Hashmap *seats;
         Hashmap *sessions;
         Hashmap *sessions_by_leader;
-        Hashmap *users;
+        Hashmap *users;  /* indexed by UID */
         Hashmap *inhibitors;
         Hashmap *buttons;
         Hashmap *brightness_writers;
@@ -181,7 +182,7 @@ int manager_add_device(Manager *m, const char *sysfs, bool master, Device **ret_
 int manager_add_button(Manager *m, const char *name, Button **ret_button);
 int manager_add_seat(Manager *m, const char *id, Seat **ret_seat);
 int manager_add_session(Manager *m, const char *id, Session **ret_session);
-int manager_add_user(Manager *m, uid_t uid, gid_t gid, const char *name, const char *home, User **ret_user);
+int manager_add_user(Manager *m, UserRecord *ur, User **ret_user);
 int manager_add_user_by_name(Manager *m, const char *name, User **ret_user);
 int manager_add_user_by_uid(Manager *m, uid_t uid, User **ret_user);
 int manager_add_inhibitor(Manager *m, const char* id, Inhibitor **ret_inhibitor);