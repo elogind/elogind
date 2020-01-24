67435b73c33851347399944600bed400a099bd14
Sven Eden <sven.eden@prydeworx.com>
2020-01-24 16:39:23 +0100

    Removed src/shared/{group,user}-record-show.[hc] ; They are nowhere needed.
    Signed-off-by: Sven Eden <sven.eden@prydeworx.com>

--- src/shared/user-record-show.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: LGPL-2.1+ */
-#pragma once
-
-#include "user-record.h"
-
-const char *user_record_state_color(const char *state);
-
-void user_record_show(UserRecord *hr, bool show_full_group_info);
