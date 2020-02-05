eebb6d1d8f74519f34acd27c49f9ef9301a7fda2
Sven Eden <sven.eden@prydeworx.com>
2020-02-05 07:48:08 +0100

    Fix migration issues in src/login [4/7]
    Signed-off-by: Sven Eden <sven.eden@prydeworx.com>

--- src/login/logind-dbus.c
+++ src/login/logind-dbus.c
@@ -13,7 +13,7 @@
 #include "bus-common-errors.h"
 #include "bus-error.h"
 #include "bus-polkit.h"
-#include "bus-unit-util.h"
+//#include "bus-unit-util.h"
 #include "bus-util.h"
 #include "cgroup-util.h"
 #include "device-util.h"
