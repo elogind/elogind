462f2368f2394f3f0a58a1a1a8502f0ddfc9ff71
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2020-05-28 17:27:11 +0200

    strv: propagate location info from the call site too

--- src/basic/strv.h
+++ src/basic/strv.h
@@ -242,7 +242,9 @@ int fputstrv(FILE *f, char * const *l, const char *separator, bool *space);
         })
 
 extern const struct hash_ops string_strv_hash_ops;
-int string_strv_hashmap_put(Hashmap **h, const char *key, const char *value);
 #if 0 /// UNNEEDED by elogind
-int string_strv_ordered_hashmap_put(OrderedHashmap **h, const char *key, const char *value);
 #endif // 0
+int _string_strv_hashmap_put(Hashmap **h, const char *key, const char *value  HASHMAP_DEBUG_PARAMS);
+int _string_strv_ordered_hashmap_put(OrderedHashmap **h, const char *key, const char *value  HASHMAP_DEBUG_PARAMS);
+#define string_strv_hashmap_put(h, k, v) _string_strv_hashmap_put(h, k, v  HASHMAP_DEBUG_SRC_ARGS)
+#define string_strv_ordered_hashmap_put(h, k, v) _string_strv_ordered_hashmap_put(h, k, v  HASHMAP_DEBUG_SRC_ARGS)
