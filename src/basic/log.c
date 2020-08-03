11e5177d70f787ec7116e567ec976811a2970724
Franck Bui <fbui@suse.com>
2020-08-03 17:50:11 +0200

    log: don't explicitly re-open log for failed assertions
    This was needed before commit 16e4fd87c5be06d2b7a3b368205c8c5bab9df32a added a
    mode that opens the log fds for every single log message. This mode is used in
    execute.c since then making the explicit call to log_open unnecessary.
    
    This basically reverts ea89a119cda917a17bd186b3c13197acfd655b12.

--- src/basic/log.c
+++ src/basic/log.c
@@ -871,7 +871,6 @@ _noreturn_ void log_assert_failed_realm(
                 const char *file,
                 int line,
                 const char *func) {
-        (void) log_open();
         log_assert(LOG_REALM_PLUS_LEVEL(realm, LOG_CRIT), text, file, line, func,
                    "Assertion '%s' failed at %s:%u, function %s(). Aborting.");
         abort();
@@ -883,7 +882,6 @@ _noreturn_ void log_assert_failed_unreachable_realm(
                 const char *file,
                 int line,
                 const char *func) {
-        (void) log_open();
         log_assert(LOG_REALM_PLUS_LEVEL(realm, LOG_CRIT), text, file, line, func,
                    "Code should not be reached '%s' at %s:%u, function %s(). Aborting.");
         abort();
