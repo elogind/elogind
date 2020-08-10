49f595c664ddcd3a41c34f7e9b07742d6295b2f5
Luca Boccassi <luca.boccassi@microsoft.com>
2020-08-10 11:24:51 +0100

    udev-util: add device_wait_for_devlink
    Allows to wait for an event by matching on the devlink that gets
    created.

--- src/shared/udev-util.h
+++ src/shared/udev-util.h
@@ -30,6 +30,7 @@ static inline int udev_parse_config(void) {
 }
 
 int device_wait_for_initialization(sd_device *device, const char *subsystem, usec_t timeout, sd_device **ret);
+int device_wait_for_devlink(const char *path, const char *subsystem, usec_t timeout, sd_device **ret);
 int device_is_renaming(sd_device *dev);
 #endif // 0
 bool device_for_action(sd_device *dev, DeviceAction action);
