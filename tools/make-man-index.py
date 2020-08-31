systemd/systemd

man: drop obsolete HAVE_PYTHON conditional
It stopped making sense when automake support was dropped and python started
being required to perform a build.
Follow-up for 72cdb3e783174dcf9223a49f03e3b0e2ca95ddb8.

tools/make-man-index.py
@@ -9,7 +9,7 @@ from xml_helper import xml_parse, xml_print, tree
 MDASH = ' — ' if sys.version_info.major >= 3 else ' -- '
 
 TEMPLATE = '''\
-<refentry id="systemd.index" conditional="HAVE_PYTHON">
+<refentry id="systemd.index">
 
   <refentryinfo>
     <title>systemd.index</title>

elogind/elogind

tools/make-man-index.py
@@ -9,7 +9,7 @@ from xml_helper import xml_parse, xml_print, tree
 MDASH = ' — ' if sys.version_info.major >= 3 else ' -- '
 
 TEMPLATE = '''\
-<refentry id="elogind.index" conditional="HAVE_PYTHON">
+<refentry id="elogind.index">
 
   <refentryinfo>
       <!-- 1 /// Must add elogind authors for the additions and changes -->