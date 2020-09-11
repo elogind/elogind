05addd3674088023c99d8a0ba869134d1f8fd731
Sven Eden <sven.eden@prydeworx.com>
2020-09-11 11:51:53 +0200

    Remove some more orphaned headers
    Signed-off-by: Sven Eden <sven.eden@prydeworx.com>

--- src/core/generator-setup.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: LGPL-2.1+ */
-#pragma once
-
-#include "path-lookup.h"
-
-int lookup_paths_mkdir_generator(LookupPaths *p);
-void lookup_paths_trim_generator(LookupPaths *p);
-void lookup_paths_flush_generator(LookupPaths *p);
