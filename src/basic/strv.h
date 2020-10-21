7ccc1008472f9a08563372af0b1c242a4be5b900
williamvds <william@williamvds.me>
2020-10-21 17:14:37 +0100

    Add strv_prepend
    Inserts a copy of the value at the head of the list.

--- src/basic/strv.h
+++ src/basic/strv.h
@@ -36,6 +36,7 @@ int strv_extend_strv(char ***a, char * const *b, bool filter_duplicates);
 #if 0 /// UNNEEDED by elogind
 int strv_extend_strv_concat(char ***a, char * const *b, const char *suffix);
 #endif // 0
+int strv_prepend(char ***l, const char *value);
 int strv_extend(char ***l, const char *value);
 #if 0 /// UNNEEDED by elogind
 int strv_extendf(char ***l, const char *format, ...) _printf_(2,0);
