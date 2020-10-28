54381a9299e4e71d9218658e8d1aed30fb2f49e9
Yu Watanabe <watanabe.yu+github@gmail.com>
2020-10-29 00:01:02 +0900

    libsystemd-network: move config_parse_hwaddr() and config_parse_hwaddrs()

--- src/shared/conf-parser.h
+++ src/shared/conf-parser.h
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/* SPDX-License-Identifier: LGPL-2.1+ */
 #pragma once
 
 #include <errno.h>
@@ -35,7 +35,7 @@ typedef enum ConfigParseFlags {
 /* Prototype for a parser for a specific configuration setting */
 typedef int (*ConfigParserCallback)(CONFIG_PARSER_ARGUMENTS);
 
-/* A macro declaring a function prototype, following the typedef above, simply because it's so cumbersomely long
+/* A macro declaring the a function prototype, following the typedef above, simply because it's so cumbersomely long
  * otherwise. (And current emacs gets irritatingly slow when editing files that contain lots of very long function
  * prototypes on the same screen…) */
 #define CONFIG_PARSER_PROTOTYPE(name) int name(CONFIG_PARSER_ARGUMENTS)
@@ -155,6 +155,8 @@ CONFIG_PARSER_PROTOTYPE(config_parse_mtu);
 CONFIG_PARSER_PROTOTYPE(config_parse_rlimit);
 CONFIG_PARSER_PROTOTYPE(config_parse_vlanprotocol);
 #endif // 0
+CONFIG_PARSER_PROTOTYPE(config_parse_hwaddr);
+CONFIG_PARSER_PROTOTYPE(config_parse_hwaddrs);
 CONFIG_PARSER_PROTOTYPE(config_parse_percent);
 
 typedef enum Disabled {
