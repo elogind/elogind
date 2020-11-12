5a5fba6dc2e7d0e2d818b7a0fc7e40bf93b8ce51
Julia Kartseva <hex@fb.com>
2020-11-12 14:05:15 -0800

    core: add socket-bind cgroup mask harness
    Standard cgroup harness for bpf feature.

--- src/basic/cgroup-util.c
+++ src/basic/cgroup-util.c
@@ -2318,6 +2318,7 @@ static const char *const cgroup_controller_table[_CGROUP_CONTROLLER_MAX] = {
         [CGROUP_CONTROLLER_BPF_DEVICES] = "bpf-devices",
 #endif // 0
         [CGROUP_CONTROLLER_BPF_FOREIGN] = "bpf-foreign",
+        [CGROUP_CONTROLLER_BPF_SOCKET_BIND] = "bpf-socket-bind",
 };
 
 DEFINE_STRING_TABLE_LOOKUP(cgroup_controller, CGroupController);
