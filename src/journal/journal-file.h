27020aa111e0e942dc5ec4bfc5af845796ac6ee1
Yu Watanabe <watanabe.yu+github@gmail.com>
2020-12-16 04:36:14 +0900

    journal: move journal_field_valid() to journal_file.c
    (cherry picked from commit adce225a104d0b7503aa7322db15d1c6dd8b8093)
    (cherry picked from commit b7f69284f1eb21c51cb659a96685cffc6c472ffd)

--- src/journal/journal-file.h
+++ src/journal/journal-file.h
@@ -274,3 +274,5 @@ static inline bool JOURNAL_FILE_COMPRESS(JournalFile *f) {
 #endif // 0
 
 uint64_t journal_file_hash_data(JournalFile *f, const void *data, size_t sz);
+
+bool journal_field_valid(const char *p, size_t l, bool allow_protected);
