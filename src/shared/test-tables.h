a7a5f08b91e152d45c275c00650fac4cfd1526de
Sven Eden <sven.eden@prydeworx.com>
2020-12-25 14:43:18 +0100

    _test_table(): Pacify gcc-10 -ftrace by calling strnull(val) (#189)
    Closes: #189
    Signed-off-by: Sven Eden <sven.eden@prydeworx.com>
    (cherry picked from commit 8b21e351cf952f283ad23011b70281769ac9bc4f)

--- src/shared/test-tables.h
+++ src/shared/test-tables.h
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 //#include <string.h>
+/// Additional includes needed by elogind
+#include "string-util.h"
 
 typedef const char* (*lookup_t)(int);
 typedef int (*reverse_t)(const char*);
@@ -27,7 +29,11 @@ static inline void _test_table(const char *name,
                 }
 
                 if (boring < 1 || i == size)
+#if 0 /// To pacify gcc-10 ftrace, call strnull() on `val` in elogind (#189)
                         printf("%s: %d → %s → %d\n", name, i, val, rev);
+#else // 0
+                        printf("%s: %d → %s → %d\n", name, i, strnull(val), rev);
+#endif // 0
                 else if (boring == 1)
                         printf("%*s  ...\n", (int) strlen(name), "");
 
