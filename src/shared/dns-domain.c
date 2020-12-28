9e2d253c596180642b489f4332623fca1289bc37
Luca Boccassi <bluca@debian.org>
2020-12-28 00:23:14 +0000

    shared/dns: fix dlopen_idn return code check
    Fixes https://github.com/systemd/systemd/issues/18078

--- src/shared/dns-domain.c
+++ src/shared/dns-domain.c
@@ -1287,7 +1287,7 @@ int dns_name_apply_idna(const char *name, char **ret) {
         int r;
 
         r = dlopen_idn();
-        if (r == EOPNOTSUPP) {
+        if (r == -EOPNOTSUPP) {
                 *ret = NULL;
                 return 0;
         }
