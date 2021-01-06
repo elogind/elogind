a47c47c01aec744e204878faebab8a63684c0c65
Lennart Poettering <lennart@poettering.net>
2021-01-06 15:51:05 +0100

    format-table: add cell type for "mode_t" values

--- src/shared/format-table.c
+++ src/shared/format-table.c
@@ -11,6 +11,7 @@
 #include "fileio.h"
 #include "format-table.h"
 #include "format-util.h"
+#include "fs-util.h"
 #include "gunicode.h"
 #include "id128-util.h"
 //#include "in-addr-util.h"
@@ -108,6 +109,7 @@ typedef struct TableData {
                 uid_t uid;
                 gid_t gid;
                 pid_t pid;
+                mode_t mode;
                 /* … add more here as we start supporting more cell data types … */
         };
 } TableData;
@@ -312,6 +314,9 @@ static size_t table_data_size(TableDataType type, const void *data) {
                 return sizeof(gid_t);
         case TABLE_PID:
                 return sizeof(pid_t);
+
+        case TABLE_MODE:
+                return sizeof(mode_t);
 
         default:
                 assert_not_reached("Uh? Unexpected cell type");
@@ -829,6 +834,7 @@ int table_add_many_internal(Table *t, TableDataType first_type, ...) {
                         uid_t uid;
                         gid_t gid;
                         pid_t pid;
+                        mode_t mode;
                 } buffer;
 
                 switch (type) {
@@ -974,6 +980,11 @@ int table_add_many_internal(Table *t, TableDataType first_type, ...) {
                 case TABLE_PID:
                         buffer.pid = va_arg(ap, pid_t);
                         data = &buffer.pid;
+                        break;
+
+                case TABLE_MODE:
+                        buffer.mode = va_arg(ap, mode_t);
+                        data = &buffer.mode;
                         break;
 
                 case TABLE_SET_MINIMUM_WIDTH: {
@@ -1290,6 +1301,9 @@ static int cell_data_compare(TableData *a, size_t index_a, TableData *b, size_t 
 
                 case TABLE_PID:
                         return CMP(a->pid, b->pid);
+
+                case TABLE_MODE:
+                        return CMP(a->mode, b->mode);
 
                 default:
                         ;
@@ -1734,6 +1748,21 @@ static const char *table_data_format(Table *t, TableData *d, bool avoid_uppercas
                 if (!p)
                         return NULL;
 
+                d->formatted = TAKE_PTR(p);
+                break;
+        }
+
+        case TABLE_MODE: {
+                _cleanup_free_ char *p;
+
+                if (d->mode == MODE_INVALID)
+                        return "n/a";
+
+                p = new(char, 4 + 1);
+                if (!p)
+                        return NULL;
+
+                sprintf(p, "%04o", d->mode & 07777);
                 d->formatted = TAKE_PTR(p);
                 break;
         }
@@ -2550,6 +2579,12 @@ static int table_data_to_json(TableData *d, JsonVariant **ret) {
                         return json_variant_new_null(ret);
 
                 return json_variant_new_integer(ret, d->int_val);
+
+        case TABLE_MODE:
+                if (d->mode == MODE_INVALID)
+                        return json_variant_new_null(ret);
+
+                return json_variant_new_unsigned(ret, d->mode);
 
         default:
                 return -EINVAL;
