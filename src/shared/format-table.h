1f952bf2baa9c8a4a6222a556872ad529c02f705
Lennart Poettering <lennart@poettering.net>
2021-02-09 16:36:07 +0100

    format-table: make UID/GID/PID fields first class citizens + add signal cell type
    This way we can display invalid UIDs/GIDs/PIDs as n/a while still
    storing them as is.
    
    Also, let's add a new cell type for unix signal, that is stored as
    integer, but displayed as signal name string.

--- src/shared/format-table.h
+++ src/shared/format-table.h
@@ -41,6 +41,10 @@ typedef enum TableDataType {
 #endif // 0
         TABLE_ID128,
         TABLE_UUID,
+        TABLE_UID,
+        TABLE_GID,
+        TABLE_PID,
+        TABLE_SIGNAL,
         _TABLE_DATA_TYPE_MAX,
 
         /* The following are not really data types, but commands for table_add_cell_many() to make changes to
@@ -58,16 +62,6 @@ typedef enum TableDataType {
 
         _TABLE_DATA_TYPE_INVALID = -1,
 } TableDataType;
-
-/* PIDs are just 32bit signed integers on Linux */
-#define TABLE_PID TABLE_INT32
-assert_cc(sizeof(pid_t) == sizeof(int32_t));
-
-/* UIDs/GIDs are just 32bit unsigned integers on Linux */
-#define TABLE_UID TABLE_UINT32
-#define TABLE_GID TABLE_UINT32
-assert_cc(sizeof(uid_t) == sizeof(uint32_t));
-assert_cc(sizeof(gid_t) == sizeof(uint32_t));
 
 typedef struct Table Table;
 typedef struct TableCell TableCell;
