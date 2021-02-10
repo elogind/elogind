4c04012103bfb5200233727e8875a61c6d768c9a
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2021-02-10 07:49:17 +0100

    test-tables: fix warning about NULL used in printf
    With the simplified asserts, newer gcc is able to (correctly) figure
    out that NULL was being passed.

--- src/shared/test-tables.h
+++ src/shared/test-tables.h
@@ -30,7 +30,7 @@ static inline void _test_table(const char *name,
 
                 if (boring < 1 || i == size)
 #if 0 /// To pacify gcc-10 ftrace, call strnull() on `val` in elogind (#189)
-                        printf("%s: %d → %s → %d\n", name, i, val, rev);
+                        printf("%s: %d → %s → %d\n", name, i, strnull(val), rev);
 #else // 0
                         printf("%s: %d → %s → %d\n", name, i, strnull(val), rev);
 #endif // 0
