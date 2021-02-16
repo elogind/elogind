f525df82b31acaa78a5287988342d4a2e72ef532
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2021-02-16 16:04:41 +0100

    Refactor strv_env_replace() into strv_env_replace_consume()
    All callers of strv_env_replace() would free the argument on error.
    So let's follow the same pattern as with strv_consume (and similar
    naming) and unconditionally "use up" the argument.

--- src/basic/env-util.h
+++ src/basic/env-util.h
@@ -48,7 +48,7 @@ char **strv_env_unset(char **l, const char *p); /* In place ... */
 #if 0 /// UNNEEDED by elogind
 char **strv_env_unset_many(char **l, ...) _sentinel_;
 #endif // 0
-int strv_env_replace(char ***l, char *p); /* In place ... */
+int strv_env_replace_consume(char ***l, char *p); /* In place ... */
 int strv_env_replace_strdup(char ***l, const char *assignment);
 int strv_env_assign(char ***l, const char *key, const char *value);
 
