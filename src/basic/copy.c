13160e998edfcc0a9ba5293e45d26bfb09e10699
Lennart Poettering <lennart@poettering.net>
2021-03-04 18:52:10 +0100

    copy: move sync_rights() to copy.c and rename copy_rights()
    It's so similar to copy_access(), hence let's move it over and rename it
    in similar style to the rest of the functions.
    
    No change in behaviour, just moving things over.

--- src/basic/copy.c
+++ src/basic/copy.c
@@ -1236,6 +1236,8 @@ int copy_access(int fdf, int fdt) {
         assert(fdf >= 0);
         assert(fdt >= 0);
 
+        /* Copies just the access mode (and not the ownership) from fdf to fdt */
+
         if (fstat(fdf, &st) < 0)
                 return -errno;
 
@@ -1243,6 +1245,20 @@ int copy_access(int fdf, int fdt) {
                 return -errno;
 
         return 0;
+}
+
+int copy_rights(int fdf, int fdt) {
+        struct stat st;
+
+        assert(fdf >= 0);
+        assert(fdt >= 0);
+
+        /* Copies both access mode and ownership from fdf to fdt */
+
+        if (fstat(fdf, &st) < 0)
+                return -errno;
+
+        return fchmod_and_chown(fdt, st.st_mode & 07777, st.st_uid, st.st_gid);
 }
 
 int copy_xattr(int fdf, int fdt) {
