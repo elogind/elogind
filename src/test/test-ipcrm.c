63da270a35aee4a77b72b8933f57c869854a089a
Sven Eden <sven.eden@prydeworx.com>
2021-03-09 10:05:07 +0100

    test/test-ipcrm.c: There is no reason anymore to hard-code 'nobody'
    Signed-off-by: Sven Eden <sven.eden@prydeworx.com>

--- src/test/test-ipcrm.c
+++ src/test/test-ipcrm.c
@@ -8,13 +8,9 @@
 int main(int argc, char *argv[]) {
         uid_t uid;
         int r;
-#if 0 /// not configurable in elogind
         const char* name = argv[1] ?: NOBODY_USER_NAME;
 
         test_setup_logging(LOG_INFO);
-#else // 0
-        const char* name = argv[1] ?: "nobody";
-#endif // 0
 
         r = get_user_creds(&name, &uid, NULL, NULL, NULL, 0);
         if (r == -ESRCH)
