9519ca213a0e0d1f1ba33714ce11820096280053
Ondrej Kozina <okozina@redhat.com>
2021-05-20 15:37:08 +0200

    Add support for systemd-pkcs11 libcryptsetup plugin.
    Add support for systemd-pkcs11 based LUKS2 device activation
    via libcryptsetup plugin. This make the feature (pkcs11 sealed
    LUKS2 keyslot passphrase) usable from both systemd utilities
    and cryptsetup cli.
    
    The feature is configured via -Dlibcryptsetup-plugins combo
    with default value set to 'auto'. It get's enabled automatically
    when cryptsetup 2.4.0 or later is installed in build system.

--- src/shared/pkcs11-util.h
+++ src/shared/pkcs11-util.h
@@ -78,5 +78,11 @@ int pkcs11_crypt_device_callback(
 #endif
 #endif // 0
 
+typedef struct {
+        const char *friendly_name;
+        usec_t until;
+        bool headless;
+} elogind_pkcs11_plugin_params;
+
 int pkcs11_list_tokens(void);
 int pkcs11_find_token_auto(char **ret);
