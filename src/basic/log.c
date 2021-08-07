systemd/systemd

log: optionally display thread ID in log output to tty
This is very useful when playing around with threaded tests, but should
be useful in other cases too.

src/basic/log.c
@@ -22,6 +22,7 @@
 #include "io-util.h"
 #include "log.h"
 #include "macro.h"
+#include "missing_syscall.h"
 #include "parse-util.h"
 #include "proc-cmdline.h"
 #include "process-util.h"
@@ -53,6 +54,7 @@ static bool syslog_is_stream = false;
 static bool show_color = false;
 static bool show_location = false;
 static bool show_time = false;
+static bool show_tid = false;
 
 static bool upgrade_syslog_to_journal = false;
 static bool always_reopen_console = false;
@@ -360,8 +362,9 @@ static int write_to_console(
 
         char location[256],
              header_time[FORMAT_TIMESTAMP_MAX],
-             prefix[1 + DECIMAL_STR_MAX(int) + 2];
-        struct iovec iovec[8] = {};
+             prefix[1 + DECIMAL_STR_MAX(int) + 2],
+             tid_string[3 + DECIMAL_STR_MAX(pid_t) + 1];
+        struct iovec iovec[9];
         const char *on = NULL, *off = NULL;
         size_t n = 0;
 
@@ -378,6 +381,11 @@ static int write_to_console(
                         iovec[n++] = IOVEC_MAKE_STRING(header_time);
                         iovec[n++] = IOVEC_MAKE_STRING(" ");
                 }
+        }
+
+        if (show_tid) {
+                xsprintf(tid_string, "(" PID_FMT ") ", gettid());
+                iovec[n++] = IOVEC_MAKE_STRING(tid_string);
         }
 
         if (show_color)
@@ -1133,6 +1141,11 @@ static int parse_proc_cmdline_item(const char *key, const char *value, void *dat
                 if (log_show_location_from_string(value ?: "1") < 0)
                         log_warning("Failed to parse log location setting '%s'. Ignoring.", value);
 
+        } else if (proc_cmdline_key_streq(key, "systemd.log_tid")) {
+
+                if (log_show_tid_from_string(value ?: "1") < 0)
+                        log_warning("Failed to parse log tid setting '%s'. Ignoring.", value);
+
         } else if (proc_cmdline_key_streq(key, "systemd.log_time")) {
 
                 if (log_show_time_from_string(value ?: "1") < 0)
@@ -1177,6 +1190,10 @@ void log_parse_environment_cli_realm(LogRealm realm) {
         e = getenv("SYSTEMD_LOG_TIME");
         if (e && log_show_time_from_string(e) < 0)
                 log_warning("Failed to parse log time '%s'. Ignoring.", e);
+
+        e = getenv("SYSTEMD_LOG_TID");
+        if (e && log_show_tid_from_string(e) < 0)
+                log_warning("Failed to parse log tid '%s'. Ignoring.", e);
 }
 
 LogTarget log_get_target(void) {
@@ -1211,6 +1228,14 @@ bool log_get_show_time(void) {
         return show_time;
 }
 
+void log_show_tid(bool b) {
+        show_tid = b;
+}
+
+bool log_get_show_tid(void) {
+        return show_tid;
+}
+
 int log_show_color_from_string(const char *e) {
         int t;
 
@@ -1241,6 +1266,17 @@ int log_show_time_from_string(const char *e) {
                 return t;
 
         log_show_time(t);
+        return 0;
+}
+
+int log_show_tid_from_string(const char *e) {
+        int t;
+
+        t = parse_boolean(e);
+        if (t < 0)
+                return t;
+
+        log_show_tid(t);
         return 0;
 }
 

elogind/elogind

src/basic/log.c
@@ -22,6 +22,7 @@
 #include "io-util.h"
 #include "log.h"
 #include "macro.h"
+#include "missing_syscall.h"
 #include "parse-util.h"
 #include "proc-cmdline.h"
 #include "process-util.h"
@@ -53,6 +54,7 @@ static bool syslog_is_stream = false;
 static bool show_color = false;
 static bool show_location = false;
 static bool show_time = false;
+static bool show_tid = false;
 
 #if 0 /// UNNEEDED by elogind
 static bool upgrade_syslog_to_journal = false;
@@ -385,8 +387,9 @@ static int write_to_console(
 
         char location[256],
              header_time[FORMAT_TIMESTAMP_MAX],
-             prefix[1 + DECIMAL_STR_MAX(int) + 2];
-        struct iovec iovec[8] = {};
+             prefix[1 + DECIMAL_STR_MAX(int) + 2],
+             tid_string[3 + DECIMAL_STR_MAX(pid_t) + 1];
+        struct iovec iovec[9];
         const char *on = NULL, *off = NULL;
         size_t n = 0;
 
@@ -403,6 +406,11 @@ static int write_to_console(
                         iovec[n++] = IOVEC_MAKE_STRING(header_time);
                         iovec[n++] = IOVEC_MAKE_STRING(" ");
                 }
+        }
+
+        if (show_tid) {
+                xsprintf(tid_string, "(" PID_FMT ") ", gettid());
+                iovec[n++] = IOVEC_MAKE_STRING(tid_string);
         }
 
         if (show_color)
@@ -1170,6 +1178,11 @@ static int parse_proc_cmdline_item(const char *key, const char *value, void *dat
                 if (log_show_location_from_string(value ?: "1") < 0)
                         log_warning("Failed to parse log location setting '%s'. Ignoring.", value);
 
+        } else if (proc_cmdline_key_streq(key, "elogind.log_tid")) {
+
+                if (log_show_tid_from_string(value ?: "1") < 0)
+                        log_warning("Failed to parse log tid setting '%s'. Ignoring.", value);
+
         } else if (proc_cmdline_key_streq(key, "elogind.log_time")) {
 
                 if (log_show_time_from_string(value ?: "1") < 0)
@@ -1214,6 +1227,10 @@ void log_parse_environment_cli_realm(LogRealm realm) {
         e = getenv("SYSTEMD_LOG_TIME");
         if (e && log_show_time_from_string(e) < 0)
                 log_warning("Failed to parse log time '%s'. Ignoring.", e);
+
+        e = getenv("SYSTEMD_LOG_TID");
+        if (e && log_show_tid_from_string(e) < 0)
+                log_warning("Failed to parse log tid '%s'. Ignoring.", e);
 }
 
 LogTarget log_get_target(void) {
@@ -1248,6 +1265,14 @@ bool log_get_show_time(void) {
         return show_time;
 }
 
+void log_show_tid(bool b) {
+        show_tid = b;
+}
+
+bool log_get_show_tid(void) {
+        return show_tid;
+}
+
 int log_show_color_from_string(const char *e) {
         int t;
 
@@ -1278,6 +1303,17 @@ int log_show_time_from_string(const char *e) {
                 return t;
 
         log_show_time(t);
+        return 0;
+}
+
+int log_show_tid_from_string(const char *e) {
+        int t;
+
+        t = parse_boolean(e);
+        if (t < 0)
+                return t;
+
+        log_show_tid(t);
         return 0;
 }
 