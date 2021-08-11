systemd/systemd

tree-wide: use free_and_strdup_warn()

src/shared/conf-parser.c
@@ -731,10 +731,7 @@ int config_parse_string(
         assert(rvalue);
         assert(data);
 
-        if (free_and_strdup(s, empty_to_null(rvalue)) < 0)
-                return log_oom();
-
-        return 0;
+        return free_and_strdup(s, empty_to_null(rvalue));
 }
 
 int config_parse_path(

elogind/elogind

src/shared/conf-parser.c
@@ -743,10 +743,7 @@ int config_parse_string(
         assert(rvalue);
         assert(data);
 
-        if (free_and_strdup(s, empty_to_null(rvalue)) < 0)
-                return log_oom();
-
-        return 0;
+        return free_and_strdup_warn(s, empty_to_null(rvalue));
 }
 
 #if 0 /// UNNEEDED by elogind