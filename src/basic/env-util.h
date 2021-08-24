dc91a31d341ac631c2d4a30f53109200bc7eff85
Sven Eden <sven.eden@prydeworx.com>
2021-08-24 08:41:55 +0200

    Remove duplicated setenv_elogind_exec_pid() declaration
    Signed-off-by: Sven Eden <sven.eden@prydeworx.com>

--- src/basic/env-util.h
+++ src/basic/env-util.h
@@ -64,7 +64,6 @@ int getenv_bool_secure(const char *p);
 /* Like setenv, but calls unsetenv if value == NULL. */
 int set_unset_env(const char *name, const char *value, bool overwrite);
 
-int setenv_elogind_exec_pid(bool update_only);
 /* Like putenv, but duplicates the memory like setenv. */
 int putenv_dup(const char *assignment, bool override);
 
