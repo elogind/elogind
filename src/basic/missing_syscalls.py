systemd/systemd

basic/missing-syscall: add numbers for riscv (64-bit)

src/basic/missing_syscalls.py
@@ -65,6 +65,8 @@ DEF_TEMPLATE = '''
 #    endif
 #  elif defined(__powerpc__)
 #    define systemd_NR_{syscall} {nr_powerpc}
+#  elif defined(__riscv) && defined(__LP64__)
+#    define systemd_NR_{syscall} {nr_riscv64}
 #  elif defined(__s390__)
 #    define systemd_NR_{syscall} {nr_s390}
 #  elif defined(__sparc__)

elogind/elogind

src/basic/missing_syscalls.py
@@ -69,6 +69,8 @@ DEF_TEMPLATE_B = '''\
 #    endif
 #  elif defined(__powerpc__)
 #    define systemd_NR_{syscall} {nr_powerpc}
+#  elif defined(__riscv) && defined(__LP64__)
+#    define elogind_NR_{syscall} {nr_riscv64}
 #  elif defined(__s390__)
 #    define systemd_NR_{syscall} {nr_s390}
 #  elif defined(__sparc__)