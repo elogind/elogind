ab8be542c9227a3adc04a432aa6861f8ad04fa75
Sven Eden <sven.eden@prydeworx.com>
2021-08-25 12:20:05 +0200

    Add new shared/parse-argument.h
    Signed-off-by: Sven Eden <sven.eden@prydeworx.com>

---
+++ src/shared/parse-argument.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include "json.h"
+
+int parse_boolean_argument(const char *optname, const char *s, bool *ret);
+int parse_json_argument(const char *s, JsonFormatFlags *ret);
+#if 0 /// UNNEEDED by elogind
+int parse_path_argument(const char *path, bool suppress_root, char **arg);
+#endif // 0
+int parse_signal_argument(const char *s, int *ret);
