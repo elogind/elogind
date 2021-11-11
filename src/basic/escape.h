6e0ddea7aefef4dcf632cc81bfcd216a1c6217e4
Lennart Poettering <lennart@poettering.net>
2021-11-11 09:32:32 +0100

    escape: add flags argument to quote_command_line()
    That way, we can reuse the call at one more place (see later patch).

--- src/basic/escape.h
+++ src/basic/escape.h
@@ -71,3 +71,4 @@ char* escape_non_printable_full(const char *str, size_t console_width, XEscapeFl
 /// elogind empty mask removed (UNNEEDED by elogind)
 char* shell_escape(const char *s, const char *bad);
 char* shell_maybe_quote(const char *s, ShellEscapeFlags flags);
+char* quote_command_line(char **argv, ShellEscapeFlags flags);
