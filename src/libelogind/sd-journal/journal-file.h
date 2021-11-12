82b52e00a1ee4f65e0ad3aef65d735a4fd0e0dad
Daan De Meyer <daan.j.demeyer@gmail.com>
2021-11-12 11:17:01 +0000

    journal: Add journal_file_object_to_string()

--- src/libelogind/sd-journal/journal-file.h
+++ src/libelogind/sd-journal/journal-file.h
@@ -274,3 +274,5 @@ uint64_t journal_file_hash_data(JournalFile *f, const void *data, size_t sz);
 
 bool journal_field_valid(const char *p, size_t l, bool allow_protected);
 #endif // 0
+
+const char* journal_object_type_to_string(ObjectType type) _const_;
