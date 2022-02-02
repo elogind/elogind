a0783bd0e187a92a8407a9b7d0589c02e189f7c7
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-02-02 12:29:54 +0900

    sd-device: move device_read_uevent_file() to device-private.h
    Then, it can be used by our tools or daemons.

--- src/libelogind/sd-device/device-private.h
+++ src/libelogind/sd-device/device-private.h
@@ -79,6 +79,8 @@ static inline int device_read_db(sd_device *device) {
         return device_read_db_internal(device, false);
 }
 
+int device_read_uevent_file(sd_device *device);
+
 sd_device_action_t device_action_from_string(const char *s) _pure_;
 const char *device_action_to_string(sd_device_action_t a) _const_;
 #if 0 /// UNNEEDED by elogind
