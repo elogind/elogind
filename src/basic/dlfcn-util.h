d81587e9223117fc34bf565210655cc8c382fd20
Sven Eden <sven.eden@prydeworx.com>
2022-02-02 15:17:29 +0100

    Add libdl link dependency (now required)
    Signed-off-by: Sven Eden <sven.eden@prydeworx.com>

--- src/basic/dlfcn-util.h
+++ src/basic/dlfcn-util.h
@@ -7,7 +7,9 @@
 
 DEFINE_TRIVIAL_CLEANUP_FUNC_FULL(void*, dlclose, NULL);
 
+#if 0 /// UNNEEDED by elogind
 int dlsym_many_and_warn(void *dl, int level, ...);
+#endif // 0
 
 /* Macro useful for putting together variable/symbol name pairs when calling dlsym_many_and_warn(). Assumes
  * that each library symbol to resolve will be placed in a variable with the "sym_" prefix, i.e. a symbol
