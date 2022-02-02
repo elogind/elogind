systemd/systemd

sd-device: expose device_cache_sysattr_value() and device_get_cached_sysattr_value()

src/libsystemd/sd-device/device-private.h
@@ -18,12 +18,14 @@ static inline int device_new_from_watch_handle(sd_device **ret, int wd) {
 }
 
 int device_get_device_id(sd_device *device, const char **ret);
-
 int device_get_devlink_priority(sd_device *device, int *priority);
 int device_get_watch_handle(sd_device *device);
 int device_get_devnode_mode(sd_device *device, mode_t *mode);
 int device_get_devnode_uid(sd_device *device, uid_t *uid);
 int device_get_devnode_gid(sd_device *device, gid_t *gid);
+
+int device_cache_sysattr_value(sd_device *device, const char *key, char *value);
+int device_get_cached_sysattr_value(sd_device *device, const char *in_key, const char **ret_value);
 
 void device_seal(sd_device *device);
 void device_set_is_initialized(sd_device *device);

src/libsystemd/sd-device/sd-device.c
@@ -1938,7 +1938,7 @@ _public_ int sd_device_get_trigger_uuid(sd_device *device, sd_id128_t *ret) {
         return 0;
 }
 
-static int device_cache_sysattr_value(sd_device *device, const char *key, char *value) {
+int device_cache_sysattr_value(sd_device *device, const char *key, char *value) {
         _cleanup_free_ char *new_key = NULL, *old_value = NULL;
         int r;
 
@@ -1965,18 +1965,18 @@ static int device_cache_sysattr_value(sd_device *device, const char *key, char *
         return 0;
 }
 
-static int device_get_cached_sysattr_value(sd_device *device, const char *_key, const char **_value) {
+int device_get_cached_sysattr_value(sd_device *device, const char *in_key, const char **ret_value) {
         const char *key = NULL, *value;
 
         assert(device);
-        assert(_key);
+        assert(in_key);
 
-        value = hashmap_get2(device->sysattr_values, _key, (void **) &key);
+        value = hashmap_get2(device->sysattr_values, in_key, (void **) &key);
         if (!key)
                 return -ENOENT;
 
-        if (_value)
-                *_value = value;
+        if (ret_value)
+                *ret_value = value;
         return 0;
 }
 

elogind/elogind

src/libelogind/sd-device/device-private.h
@@ -30,6 +30,9 @@ int device_get_devnode_gid(sd_device *device, gid_t *gid);
 #endif // 0
 /// elogind empty mask removed (UNNEEDED by elogind)
 
+int device_cache_sysattr_value(sd_device *device, const char *key, char *value);
+int device_get_cached_sysattr_value(sd_device *device, const char *key, const char **ret_value);
+
 void device_seal(sd_device *device);
 void device_set_is_initialized(sd_device *device);
 #if 0 /// UNNEEDED by elogind

src/libelogind/sd-device/sd-device.c
@@ -1945,7 +1945,7 @@ _public_ int sd_device_get_trigger_uuid(sd_device *device, sd_id128_t *ret) {
         return 0;
 }
 
-static int device_cache_sysattr_value(sd_device *device, const char *key, char *value) {
+int device_cache_sysattr_value(sd_device *device, const char *key, char *value) {
         _unused_ _cleanup_free_ char *old_value = NULL;
         _cleanup_free_ char *new_key = NULL;
         int r;
@@ -1973,7 +1973,7 @@ static int device_cache_sysattr_value(sd_device *device, const char *key, char *
         return 0;
 }
 
-static int device_get_cached_sysattr_value(sd_device *device, const char *key, const char **ret_value) {
+int device_get_cached_sysattr_value(sd_device *device, const char *key, const char **ret_value) {
         const char *k = NULL, *value;
 
         assert(device);