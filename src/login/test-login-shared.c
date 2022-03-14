9a33bc6f38c0f415b4963e0465920a20d1104472
Jan Janssen <medhefgo@web.de>
2022-03-14 10:37:27 +0100

    test: Use TEST macros in more places

--- src/login/test-login-shared.c
+++ src/login/test-login-shared.c
@@ -2,10 +2,11 @@
 
 #include "login-util.h"
 #include "macro.h"
+#include "tests.h"
 
 /// Additional includes needed by elogind
 #include "musl_missing.h"
-static void test_session_id_valid(void) {
+TEST(session_id_valid) {
         assert_se(session_id_valid("c1"));
         assert_se(session_id_valid("1234"));
 
@@ -14,12 +15,5 @@ static void test_session_id_valid(void) {
         assert_se(!session_id_valid("\tid"));
 }
 
-int main(int argc, char* argv[]) {
         elogind_set_program_name(argv[0]);
-        log_parse_environment();
-        log_open();
-
-        test_session_id_valid();
-
-        return 0;
-}
+DEFINE_TEST_MAIN(LOG_INFO);
