74161c5964bd2a46ae7541c07d59b658a6e9d8e0
Lennart Poettering <lennart@poettering.net>
2022-03-23 12:07:06 +0100

    efi-loader: split efi-api.[ch] from efi-loader.[ch]
    Some refactoring: split efi-loader.[ch] in two: isolate the calls that
    implement out boot loader interface spec, and those which implement
    access to upstream UEFI firmware features.
    
    They are quite different in nature and behaviour, and even semantically
    it makes to keep these two separate. At the very least because the
    previous name "efi-loader.[ch]" suggests all was about loader-specific
    APIs, but much of it is generic uefi stuff...
    
    While we are at it, I renamed a bunch of return parameters to follow our
    usual ret_xyz naming. But besides renaming no real code changes.

--- src/shared/efi-loader.h
+++ src/shared/efi-loader.h
@@ -6,23 +6,15 @@
 #include "efivars-fundamental.h"
 #include "efivars.h"
 
+/* Various calls that interface with EFI variables implementing https://elogind.io/BOOT_LOADER_INTERFACE */
+
 #if ENABLE_EFI
 
-int efi_reboot_to_firmware_supported(void);
-int efi_get_reboot_to_firmware(void);
-int efi_set_reboot_to_firmware(bool value);
 
 #if 0 /// UNNEEDED by elogind
-int efi_get_boot_option(uint16_t nr, char **title, sd_id128_t *part_uuid, char **path, bool *active);
-int efi_add_boot_option(uint16_t id, const char *title, uint32_t part, uint64_t pstart, uint64_t psize, sd_id128_t part_uuid, const char *path);
-int efi_remove_boot_option(uint16_t id);
-int efi_get_boot_order(uint16_t **order);
-int efi_set_boot_order(uint16_t *order, size_t n);
-int efi_get_boot_options(uint16_t **options);
-
-int efi_loader_get_device_part_uuid(sd_id128_t *u);
-int efi_loader_get_boot_usec(usec_t *firmware, usec_t *loader);
 #endif // 0
+int efi_loader_get_device_part_uuid(sd_id128_t *ret);
+int efi_loader_get_boot_usec(usec_t *ret_firmware, usec_t *ret_loader);
 
 int efi_loader_get_entries(char ***ret);
 
@@ -32,48 +24,12 @@ int efi_loader_get_config_timeout_one_shot(usec_t *ret);
 int efi_loader_update_entry_one_shot_cache(char **cache, struct stat *cache_stat);
 
 #if 0 /// UNNEEDED by elogind
-bool efi_has_tpm2(void);
 #endif // 0
 
 #else
 
-static inline int efi_reboot_to_firmware_supported(void) {
-        return -EOPNOTSUPP;
-}
-
-static inline int efi_get_reboot_to_firmware(void) {
-        return -EOPNOTSUPP;
-}
-
-static inline int efi_set_reboot_to_firmware(bool value) {
-        return -EOPNOTSUPP;
-}
 
 #if 0 /// UNNEEDED by elogind
-static inline int efi_get_boot_option(uint16_t nr, char **title, sd_id128_t *part_uuid, char **path, bool *active) {
-        return -EOPNOTSUPP;
-}
-
-static inline int efi_add_boot_option(uint16_t id, const char *title, uint32_t part, uint64_t pstart, uint64_t psize, sd_id128_t part_uuid, const char *path) {
-        return -EOPNOTSUPP;
-}
-
-static inline int efi_remove_boot_option(uint16_t id) {
-        return -EOPNOTSUPP;
-}
-
-static inline int efi_get_boot_order(uint16_t **order) {
-        return -EOPNOTSUPP;
-}
-
-static inline int efi_set_boot_order(uint16_t *order, size_t n) {
-        return -EOPNOTSUPP;
-}
-
-static inline int efi_get_boot_options(uint16_t **options) {
-        return -EOPNOTSUPP;
-}
-
 static inline int efi_loader_get_device_part_uuid(sd_id128_t *u) {
         return -EOPNOTSUPP;
 }
@@ -100,9 +56,6 @@ static inline int efi_loader_update_entry_one_shot_cache(char **cache, struct st
 }
 
 #if 0 /// UNNEEDED by elogind
-static inline bool efi_has_tpm2(void) {
-        return false;
-}
 #endif // 0
 
 #endif
@@ -110,5 +63,4 @@ static inline bool efi_has_tpm2(void) {
 bool efi_loader_entry_name_valid(const char *s);
 
 #if 0 /// UNNEEDED in elogind
-char *efi_tilt_backslashes(char *s);
 #endif // 0
