0c0f716d79d740ea02b248a1ea26ed7d0cfb9ab4
Lennart Poettering <lennart@poettering.net>
2022-04-01 14:26:50 +0200

    path-util: use STR_IN_SET() where appropriate

--- src/basic/path-util.c
+++ src/basic/path-util.c
@@ -1251,9 +1251,10 @@ bool hidden_or_backup_file(const char *filename) {
         assert(filename);
 
         if (filename[0] == '.' ||
-            streq(filename, "lost+found") ||
-            streq(filename, "aquota.user") ||
-            streq(filename, "aquota.group") ||
+            STR_IN_SET(filename,
+                       "lost+found",
+                       "aquota.user",
+                       "aquota.group") ||
             endswith(filename, "~"))
                 return true;
 
