5c62dc2e7c7681c68a30b81d0a97c4decf4de2b4
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-04-11 07:01:59 +0900

    path-util: introduce path_make_relative_parent()

--- src/basic/path-util.h
+++ src/basic/path-util.h
@@ -65,6 +65,7 @@ int path_make_absolute_cwd(const char *p, char **ret);
 #if 0 /// UNNEEDED by elogind
 int path_make_relative(const char *from, const char *to, char **ret);
 #endif // 0
+int path_make_relative_parent(const char *from_child, const char *to, char **ret);
 char *path_startswith_full(const char *path, const char *prefix, bool accept_dot_dot) _pure_;
 static inline char* path_startswith(const char *path, const char *prefix) {
         return path_startswith_full(path, prefix, true);
