578cb8094ef62bbfca8053e22db895b4e3fb0abb
Lennart Poettering <lennart@poettering.net>
2022-04-20 16:06:14 +0200

    compress: make Compression a regular non-sparse enum
    Given we have two different types for the journal object flags and the
    Compression enum, let's make the latter a regular non-sparse enum, and
    thus remove some surprises. We have to convert anyway between the two,
    and already do via COMPRESSION_FROM_OBJECT().

--- src/libelogind/sd-journal/sd-journal.c
+++ src/libelogind/sd-journal/sd-journal.c
@@ -2411,6 +2411,8 @@ _public_ int sd_journal_get_data(sd_journal *j, const char *field, const void **
                 l = le64toh(d->object.size) - offsetof(Object, data.payload);
 
                 c = COMPRESSION_FROM_OBJECT(d);
+                if (c < 0)
+                        return -EPROTONOSUPPORT;
                 if (c != COMPRESSION_NONE) {
 #if HAVE_COMPRESSION
                         r = decompress_startswith(
@@ -2487,6 +2489,8 @@ static int return_data(
                 return -E2BIG;
 
         c = COMPRESSION_FROM_OBJECT(o);
+        if (c < 0)
+                return -EPROTONOSUPPORT;
         if (c != COMPRESSION_NONE) {
 #if HAVE_COMPRESSION
                 size_t rsize;
