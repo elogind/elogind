c2cb20f2c557afc1fd48c0336a2b455b22e68258
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2022-05-10 10:51:43 +0200

    shared/json: fix memleak in sort

--- src/shared/json.c
+++ src/shared/json.c
@@ -4601,7 +4601,7 @@ static int json_cmp_strings(const void *x, const void *y) {
 
 int json_variant_sort(JsonVariant **v) {
         _cleanup_free_ JsonVariant **a = NULL;
-        JsonVariant *n = NULL;
+        _cleanup_(json_variant_unrefp) JsonVariant *n = NULL;
         size_t m;
         int r;
 
@@ -4634,7 +4634,7 @@ int json_variant_sort(JsonVariant **v) {
         if (!n->sorted) /* Check if this worked. This will fail if there are multiple identical keys used. */
                 return -ENOTUNIQ;
 
-        JSON_VARIANT_REPLACE(*v, n);
+        JSON_VARIANT_REPLACE(*v, TAKE_PTR(n));
 
         return 1;
 }
