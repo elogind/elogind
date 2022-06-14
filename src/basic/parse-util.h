0e3992f26bcd6e4d1a055fee3aa172caff67fe02
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-06-14 15:06:27 +0900

    Revert NFTSet feature
    This reverts PR #22587 and its follow-up commit. More specifically,
    2299b1cae32c1fb8911da0ce26efced68032f4f8 (partially),
    e176f855278d5098d3fecc5aa24ba702147d42e0,
    ceb46a31a01b3d3d1d6095d857e29ea214a2776b, and
    51bb9076ab8c050bebb64db5035852385accda35.
    
    The PR was merged without final approval, and has several issues:
    - OSS fuzz reported issues in the conf parser,
    - It calls synchrnous netlink call, it should not be especially in PID1,
    - The importance of NFTSet for CGroup and DynamicUser may be
      questionable, at least, there was no justification PID1 should support
      it.
    - For networkd, it should be implemented with Request object,
    - There is no test for the feature.
    
    Fixes #23711.
    Fixes #23717.
    Fixes #23719.
    Fixes #23720.
    Fixes #23721.
    Fixes #23759.

--- src/basic/parse-util.h
+++ src/basic/parse-util.h
@@ -162,4 +162,3 @@ int store_loadavg_fixed_point(unsigned long i, unsigned long f, loadavg_t *ret);
 int parse_loadavg_fixed_point(const char *s, loadavg_t *ret);
 #endif // 0
 
-bool nft_identifier_bad(const char *id);
