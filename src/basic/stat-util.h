3e0fb5b400b71519aecfbfd59eb7ab20cf571a24
Lennart Poettering <lennart@poettering.net>
2022-07-13 23:43:36 +0200

    stat-util: replace is_dir() + is_dir_fd() by single is_dir_full() call
    This new call can execute both of the old operations, but also do
    generic fstatat() like behaviour.

--- src/basic/stat-util.h
+++ src/basic/stat-util.h
@@ -15,9 +15,14 @@
 #if 0 /// UNNEEDED by elogind
 int is_symlink(const char *path);
 #endif // 0
-int is_dir(const char *path, bool follow);
 #if 0 /// UNNEEDED by elogind
-int is_dir_fd(int fd);
+int is_dir_full(int atfd, const char *fname, bool follow);
+static inline int is_dir(const char *path, bool follow) {
+        return is_dir_full(AT_FDCWD, path, follow);
+}
+static inline int is_dir_fd(int fd) {
+        return is_dir_full(fd, NULL, false);
+}
 int is_device_node(const char *path);
 
 int dir_is_empty_at(int dir_fd, const char *path, bool ignore_hidden_or_backup);
