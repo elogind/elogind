dbe3df4125678600f325397bc62c04874d08cbe4
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-08-12 03:18:49 +0900

    sd-device-enumerator: drop noisy log messages
    These are not for failed tasks, and provide almost no information.

--- src/libelogind/sd-device/device-enumerator.c
+++ src/libelogind/sd-device/device-enumerator.c
@@ -748,8 +748,6 @@ static int enumerator_scan_dir(
         if (!dir)
                 return -errno;
 
-        log_debug("sd-device-enumerator: Scanning %s", path);
-
         FOREACH_DIRENT_ALL(de, dir, return -errno) {
                 int k;
 
@@ -954,8 +952,6 @@ static int enumerator_scan_devices_children(sd_device_enumerator *enumerator) {
 
 static int enumerator_scan_devices_all(sd_device_enumerator *enumerator) {
         int k, r = 0;
-
-        log_debug("sd-device-enumerator: Scan all dirs");
 
         k = enumerator_scan_dir(enumerator, "bus", "devices", NULL);
         if (k < 0)
