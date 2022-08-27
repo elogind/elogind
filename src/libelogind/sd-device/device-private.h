bdc80bcacf05fa7a7136e93177fc8f5d32b877aa
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-08-28 02:45:56 +0900

    sd-device: introduce devpath_from_devnum()

--- src/libelogind/sd-device/device-private.h
+++ src/libelogind/sd-device/device-private.h
@@ -11,6 +11,7 @@
 #include "macro.h"
 
 #if 0 /// UNNEEDED by elogind
+int device_new_from_mode_and_devnum(sd_device **ret, mode_t mode, dev_t devnum);
 int device_new_from_nulstr(sd_device **ret, char *nulstr, size_t len);
 int device_new_from_strv(sd_device **ret, char **strv);
 int device_new_from_watch_handle_at(sd_device **ret, int dirfd, int wd);
