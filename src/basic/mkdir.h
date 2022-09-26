1d2fe0ac2fb7ea009dd4544155d0da96718e3479
Daan De Meyer <daan.j.demeyer@gmail.com>
2022-09-26 11:19:03 +0200

    mkdir: Add mkdirat_parents()

--- src/basic/mkdir.h
+++ src/basic/mkdir.h
@@ -16,8 +16,11 @@ int mkdirat_safe(int dir_fd, const char *path, mode_t mode, uid_t uid, gid_t gid
 static inline int mkdir_safe(const char *path, mode_t mode, uid_t uid, gid_t gid, MkdirFlags flags) {
         return mkdirat_safe(AT_FDCWD, path, mode, uid, gid, flags);
 }
-int mkdir_parents(const char *path, mode_t mode);
 #if 0 /// UNNEEDED by elogind
+int mkdirat_parents(int dir_fd, const char *path, mode_t mode);
+static inline int mkdir_parents(const char *path, mode_t mode) {
+        return mkdirat_parents(AT_FDCWD, path, mode);
+}
 int mkdir_parents_safe(const char *prefix, const char *path, mode_t mode, uid_t uid, gid_t gid, MkdirFlags flags);
 #endif // 0
 int mkdir_p(const char *path, mode_t mode);
@@ -32,5 +35,6 @@ int mkdirat_safe_internal(int dir_fd, const char *path, mode_t mode, uid_t uid, 
 static inline int mkdir_safe_internal(const char *path, mode_t mode, uid_t uid, gid_t gid, MkdirFlags flags, mkdirat_func_t _mkdir) {
         return mkdirat_safe_internal(AT_FDCWD, path, mode, uid, gid, flags, _mkdir);
 }
+int mkdirat_parents_internal(int dir_fd, const char *path, mode_t mode, uid_t uid, gid_t gid, MkdirFlags flags, mkdirat_func_t _mkdirat);
 int mkdir_parents_internal(const char *prefix, const char *path, mode_t mode, uid_t uid, gid_t gid, MkdirFlags flags, mkdirat_func_t _mkdir);
 int mkdir_p_internal(const char *prefix, const char *path, mode_t mode, uid_t uid, gid_t gid, MkdirFlags flags, mkdirat_func_t _mkdir);
