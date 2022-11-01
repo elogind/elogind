d6b332c31297067f08114ab556425c137ffa29fa
Lennart Poettering <lennart@poettering.net>
2022-11-01 14:56:44 +0100

    json: add helper for adding variant to array suppressing duplicates

--- src/shared/json.h
+++ src/shared/json.h
@@ -217,8 +217,11 @@ int json_variant_set_field_unsigned(JsonVariant **v, const char *field, uint64_t
 int json_variant_set_field_boolean(JsonVariant **v, const char *field, bool b);
 int json_variant_set_field_strv(JsonVariant **v, const char *field, char **l);
 
+JsonVariant *json_variant_find(JsonVariant *haystack, JsonVariant *needle);
+
 int json_variant_append_array(JsonVariant **v, JsonVariant *element);
 #endif // 0
+int json_variant_append_array_nodup(JsonVariant **v, JsonVariant *element);
 
 int json_variant_merge(JsonVariant **v, JsonVariant *m);
 
