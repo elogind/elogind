213337737f39aaacd216570cdc4e551b25fc3981
Frantisek Sumsal <frantisek@sumsal.cz>
2022-11-07 15:43:54 +0100

    Revert "mountpoint-util: tmpfs supports uid=/gid= mount options"
    This reverts commit 7d4f00c88c65532bf66d20b3ec498b5bfaa621d2.
    
    fstype_can_uid_gid() is about fixating all files to the specified
    uid/gid. tmpfs does not qualify. The uid/gid parameter there is simply
    about the default uid/gid for the root inode of the tmpfs, it allows
    setting uids/gid arbirarily for all inodes after that.
    
    This distinction matters: for file systems this function returns true
    for we can use this in place of uidmapped mounts. But for tmpfs this is
    not going to work, given inodes on that fs can end up having arbitrary
    uid/gid.
    
    See: https://github.com/systemd/systemd/pull/25284#issue-1438427144

--- src/basic/mountpoint-util.c
+++ src/basic/mountpoint-util.c
@@ -481,7 +481,6 @@ bool fstype_can_uid_gid(const char *fstype) {
                           "iso9660",
                           "msdos",
                           "ntfs",
-                          "tmpfs",
                           "vfat");
 }
 #endif // 0
