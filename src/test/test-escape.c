c8e20e12c68592b8765492cf1d75b0711a622bb1
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-12-06 12:00:45 +0900

    test: add basic tests for octescape()
    (cherry picked from commit 4f438c638b5335cd5d8f614804743a04311689f8)

--- src/test/test-escape.c
+++ src/test/test-escape.c
@@ -223,6 +223,21 @@ TEST(quote_command_line) {
                                     "true \"\\$dollar\"");
 }
 
-#if 0 /// UNNEEDED by elogind
-#endif // 0
+/// elogind empty mask removed (UNNEEDED by elogind)
+static void test_octescape_one(const char *s, const char *expected) {
+        _cleanup_free_ char *ret;
+
+        assert_se(ret = octescape(s, strlen_ptr(s)));
+        log_debug("octescape(\"%s\") → \"%s\" (expected: \"%s\")", strnull(s), ret, expected);
+        assert_se(streq(ret, expected));
+}
+
+TEST(octescape) {
+        test_octescape_one(NULL, "");
+        test_octescape_one("", "");
+        test_octescape_one("foo", "foo");
+        test_octescape_one("\"\\\"", "\\042\\134\\042");
+        test_octescape_one("\123\213\222", "\123\\213\\222");
+}
+
 DEFINE_TEST_MAIN(LOG_DEBUG);
