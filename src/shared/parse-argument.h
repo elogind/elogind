7d1bdb02bba04528d19d3f29287812370704bd84
Sven Eden <sven.eden@prydeworx.com>
2022-12-07 09:33:51 +0100

    Fix masks in src/shared/parse-argument.h
    Signed-off-by: Sven Eden <sven.eden@prydeworx.com>

--- src/shared/parse-argument.h
+++ src/shared/parse-argument.h
@@ -7,5 +7,5 @@ int parse_boolean_argument(const char *optname, const char *s, bool *ret);
 int parse_json_argument(const char *s, JsonFormatFlags *ret);
 #if 0 /// UNNEEDED by elogind
 int parse_path_argument(const char *path, bool suppress_root, char **arg);
-int parse_signal_argument(const char *s, int *ret);
 #endif // 0
+int parse_signal_argument(const char *s, int *ret);
