systemd/systemd

shared/bus-util: rename variables to follow newer style

src/shared/bus-util.h
@@ -33,8 +33,8 @@ bool bus_error_is_unknown_service(const sd_bus_error *error);
 
 int bus_check_peercred(sd_bus *c);
 
-int bus_connect_system_systemd(sd_bus **_bus);
-int bus_connect_user_systemd(sd_bus **_bus);
+int bus_connect_system_systemd(sd_bus **ret_bus);
+int bus_connect_user_systemd(sd_bus **ret_bus);
 
 int bus_connect_transport(BusTransport transport, const char *host, bool user, sd_bus **bus);
 int bus_connect_transport_systemd(BusTransport transport, const char *host, bool user, sd_bus **bus);

elogind/elogind

src/shared/bus-util.h
@@ -39,9 +39,9 @@ bool bus_error_is_unknown_service(const sd_bus_error *error);
 #if 0 /// UNNEEDED by elogind
 int bus_check_peercred(sd_bus *c);
 
-int bus_connect_system_systemd(sd_bus **_bus);
-int bus_connect_user_systemd(sd_bus **_bus);
 #endif // 0
+int bus_connect_system_elogind(sd_bus **ret_bus);
+int bus_connect_user_elogind(sd_bus **ret_bus);
 
 int bus_connect_transport(BusTransport transport, const char *host, bool user, sd_bus **bus);
 #if 0 /// UNNEEDED by elogind