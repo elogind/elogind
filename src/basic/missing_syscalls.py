systemd/systemd

basic/missing-syscalls: add PARISC (HPPA support)
Bug: https://github.com/systemd/systemd/issues/23180
Signed-off-by: Sam James <sam@gentoo.org>

src/basic/missing_syscalls.py
@@ -74,6 +74,8 @@ DEF_TEMPLATE_B = '''\
 #    else
 #      error "Unknown MIPS ABI"
 #    endif
+#  elif defined(__hppa__)
+#    define systemd_NR_{syscall} {nr_parisc}
 #  elif defined(__powerpc__)
 #    define systemd_NR_{syscall} {nr_powerpc}
 #  elif defined(__riscv)

elogind/elogind

src/basic/missing_syscalls.py
@@ -74,6 +74,8 @@ DEF_TEMPLATE_B = '''\
 #    else
 #      error "Unknown MIPS ABI"
 #    endif
+#  elif defined(__hppa__)
+#    define elogind_NR_{syscall} {nr_parisc}
 #  elif defined(__powerpc__)
 #    define systemd_NR_{syscall} {nr_powerpc}
 #  elif defined(__riscv)