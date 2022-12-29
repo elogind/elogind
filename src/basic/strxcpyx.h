2cc9215e7ce59750e4a2929a87d1d62024bc1313
Sven Eden <sven.eden@prydeworx.com>
2022-12-29 12:05:28 +0100

    Mask unused functions in src/basic/strxcpyx.*
    Signed-off-by: Sven Eden <sven.eden@prydeworx.com>

--- src/basic/strxcpyx.h
+++ src/basic/strxcpyx.h
@@ -6,8 +6,6 @@
 
 #include "macro.h"
 
-#if 0 /// UNNEEDED by elogind
-#endif // 0
 size_t strnpcpy_full(char **dest, size_t size, const char *src, size_t len, bool *ret_truncated);
 static inline size_t strnpcpy(char **dest, size_t size, const char *src, size_t len) {
         return strnpcpy_full(dest, size, src, len, NULL);
@@ -19,9 +17,11 @@ static inline size_t strpcpy(char **dest, size_t size, const char *src) {
 size_t strpcpyf_full(char **dest, size_t size, bool *ret_truncated, const char *src, ...) _printf_(4, 5);
 #define strpcpyf(dest, size, src, ...) \
         strpcpyf_full((dest), (size), NULL, (src), ##__VA_ARGS__)
+#if 0 /// UNNEEDED by elogind
 size_t strpcpyl_full(char **dest, size_t size, bool *ret_truncated, const char *src, ...) _sentinel_;
 #define strpcpyl(dest, size, src, ...) \
         strpcpyl_full((dest), (size), NULL, (src), ##__VA_ARGS__)
+#endif // 0
 size_t strnscpy_full(char *dest, size_t size, const char *src, size_t len, bool *ret_truncated);
 static inline size_t strnscpy(char *dest, size_t size, const char *src, size_t len) {
         return strnscpy_full(dest, size, src, len, NULL);
