d0590f53bf7b8ace563ef04a9d047be4abaa00be
Cristian Rodríguez <crodriguez@owncloud.com>
2023-01-01 22:40:26 +0000

    basic: fix hosed return value in skip_session()
    ../src/basic/cgroup-util.c: In function ‘skip_session’:
    ../src/basic/cgroup-util.c:1241:32: error: incompatible types when returning type ‘_Bool’ but ‘const char *’ was expected
     1241 |                         return false;

--- src/basic/cgroup-util.c
+++ src/basic/cgroup-util.c
@@ -1286,7 +1286,7 @@ static const char *skip_session(const char *p) {
                  * here. */
 
                 if (!session_id_valid(buf))
-                        return false;
+                        return NULL;
 
                 p += n;
                 p += strspn(p, "/");
