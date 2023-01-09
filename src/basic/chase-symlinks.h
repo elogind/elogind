0aea1d5a94b43cab21ef93715ab25e2d13c9678e
Ludwig Nussel <ludwig.nussel@suse.de>
2023-01-09 17:58:57 +0100

    chase-symlinks: new chase_symlinks_and_unlink()

--- src/basic/chase-symlinks.h
+++ src/basic/chase-symlinks.h
@@ -36,5 +36,6 @@ int chase_symlinks_and_stat(const char *path, const char *root, ChaseSymlinksFla
 int chase_symlinks_and_access(const char *path, const char *root, ChaseSymlinksFlags chase_flags, int access_mode, char **ret_path, int *ret_fd);
 #endif // 0
 int chase_symlinks_and_fopen_unlocked(const char *path, const char *root, ChaseSymlinksFlags chase_flags, const char *open_flags, char **ret_path, FILE **ret_file);
+int chase_symlinks_and_unlink(const char *path, const char *root, ChaseSymlinksFlags chase_flags, int unlink_flags, char **ret_path);
 
 int chase_symlinks_at(int dir_fd, const char *path, ChaseSymlinksFlags flags, char **ret_path, int *ret_fd);
