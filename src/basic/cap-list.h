ac2bd1fb3bd2223ad561c0904c21d134d528d659
Lennart Poettering <lennart@poettering.net>
2023-02-20 12:25:44 +0100

    cap-list: modernize capability_set_from_string() a bit
    Make return parameter optional. And return whether there were any caps
    we didn't recognize via 0/1 return value.

--- src/basic/cap-list.h
+++ src/basic/cap-list.h
@@ -11,5 +11,5 @@ int capability_list_length(void);
 
 #if 0 /// UNNEEDED by elogind
 int capability_set_to_string(uint64_t set, char **ret);
-int capability_set_from_string(const char *s, uint64_t *set);
 #endif // 0
+int capability_set_from_string(const char *s, uint64_t *ret);
