62a74a960ed7bc9da4d07b9cda36d40cabb41865
David Tardon <dtardon@redhat.com>
2023-02-21 10:41:47 +0100

    logind-session: make stopping of idle session visible to admins

--- src/login/logind-session.c
+++ src/login/logind-session.c
@@ -38,7 +38,6 @@
 #include "tmpfile-util.h"
 #include "uid-alloc-range.h"
 #include "user-util.h"
-//#include "util.h"
 /// Additional includes needed by elogind
 #include "cgroup-setup.h"
 #include "extract-word.h"
@@ -65,8 +64,8 @@ int session_new(Session **ret, Manager *m, const char *id) {
 
         *s = (Session) {
                 .manager = m,
-                .fifo_fd = -1,
-                .vtfd = -1,
+                .fifo_fd = -EBADF,
+                .vtfd = -EBADF,
                 .audit_id = AUDIT_SESSION_INVALID,
                 .tty_validity = _TTY_VALIDITY_INVALID,
         };
@@ -764,7 +763,7 @@ static int session_dispatch_stop_on_idle(sd_event_source *source, uint64_t t, vo
 
         idle = session_get_idle_hint(s, &ts);
         if (idle) {
-                log_debug("Session \"%s\" of user \"%s\" is idle, stopping.", s->id, s->user->user_record->user_name);
+                log_info("Session \"%s\" of user \"%s\" is idle, stopping.", s->id, s->user->user_record->user_name);
 
                 return session_stop(s, /* force */ true);
         }
