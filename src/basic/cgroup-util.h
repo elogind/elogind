d9c3a086c31d3507b9abcce3b39602778a37c0e6
Lennart Poettering <lennart@poettering.net>
2023-02-23 18:27:43 +0100

    shared: move cg_set_access() declaration to right header file
    This function was moved from cgroup-util.c to cgroup-setup.c a while
    back, but the prototype in the matching header files wasn't migrated.
    Let's fix that.

--- src/basic/cgroup-util.h
+++ src/basic/cgroup-util.h
@@ -9,7 +9,7 @@
 #include <sys/statfs.h>
 #include <sys/types.h>
 
-#include "def.h"
+#include "constants.h"
 #include "set.h"
 
 #if 0 /// elogind has them set through config.h
@@ -252,7 +252,6 @@ int cg_get_attribute_as_uint64(const char *controller, const char *path, const c
 /* Does a parse_boolean() on the attribute contents and sets ret accordingly */
 int cg_get_attribute_as_bool(const char *controller, const char *path, const char *attribute, bool *ret);
 
-int cg_set_access(const char *controller, const char *path, uid_t uid, gid_t gid);
 int cg_get_owner(const char *controller, const char *path, uid_t *ret_uid);
 
 int cg_set_xattr(const char *controller, const char *path, const char *name, const void *value, size_t size, int flags);
@@ -278,6 +277,7 @@ int cg_path_get_session(const char *path, char **session);
 int cg_path_get_owner_uid(const char *path, uid_t *uid);
 #if 0 /// UNNEEDED by elogind
 int cg_path_get_unit(const char *path, char **unit);
+int cg_path_get_unit_path(const char *path, char **unit);
 int cg_path_get_user_unit(const char *path, char **unit);
 int cg_path_get_machine_name(const char *path, char **machine);
 #endif // 0
