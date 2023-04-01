9dc3034cd615e51e0b6b22e183f3d6892645f68b
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-04-02 00:35:33 +0900

    sd-id128: rename argument and add missing assertion

--- src/libelogind/sd-id128/id128-util.h
+++ src/libelogind/sd-id128/id128-util.h
@@ -22,11 +22,11 @@ typedef enum Id128FormatFlag {
 } Id128FormatFlag;
 
 int id128_read_fd(int fd, Id128FormatFlag f, sd_id128_t *ret);
-int id128_read(const char *p, Id128FormatFlag f, sd_id128_t *ret);
+int id128_read(const char *path, Id128FormatFlag f, sd_id128_t *ret);
 
 /// elogind empty mask removed (UNNEEDED by elogind)
 int id128_write_fd(int fd, Id128FormatFlag f, sd_id128_t id);
-int id128_write(const char *p, Id128FormatFlag f, sd_id128_t id);
+int id128_write(const char *path, Id128FormatFlag f, sd_id128_t id);
 
 void id128_hash_func(const sd_id128_t *p, struct siphash *state);
 int id128_compare_func(const sd_id128_t *a, const sd_id128_t *b) _pure_;
