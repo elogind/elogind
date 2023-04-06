7b699d8ceab459ddc6335c70aa3ae4dc0f374308
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-04-07 04:34:54 +0900

    update-utmp: downgrade log level of ignored failure

--- src/update-utmp/update-utmp.c
+++ src/update-utmp/update-utmp.c
@@ -251,8 +251,8 @@ void update_utmp(int argc, char* argv[]) {
         /* If the kernel lacks netlink or audit support, don't worry about it. */
         c.audit_fd = audit_open();
         if (c.audit_fd < 0)
-                log_full_errno(IN_SET(errno, EAFNOSUPPORT, EPROTONOSUPPORT) ? LOG_DEBUG : LOG_ERR,
-                               errno, "Failed to connect to audit log: %m");
+                log_full_errno(IN_SET(errno, EAFNOSUPPORT, EPROTONOSUPPORT) ? LOG_DEBUG : LOG_WARNING,
+                               errno, "Failed to connect to audit log, ignoring: %m");
 #endif
 #if 0 /// UNNEEDED by elogind
         r = bus_connect_system_systemd(&c.bus);
