afa4f2b1c97b75ecb84b3041415c78019383cf71
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-04-07 06:09:15 +0900

    conf-files: introduce _at() variants of conf_files_list() or friends

--- src/basic/conf-files.h
+++ src/basic/conf-files.h
@@ -12,9 +12,12 @@ enum {
 };
 
 int conf_files_list(char ***ret, const char *suffix, const char *root, unsigned flags, const char *dir);
+int conf_files_list_at(char ***ret, const char *suffix, int rfd, unsigned flags, const char *dir);
 int conf_files_list_strv(char ***ret, const char *suffix, const char *root, unsigned flags, const char* const* dirs);
+int conf_files_list_strv_at(char ***ret, const char *suffix, int rfd, unsigned flags, const char * const *dirs);
 int conf_files_list_nulstr(char ***ret, const char *suffix, const char *root, unsigned flags, const char *dirs);
 #if 0 /// UNNEEDED by elogind
+int conf_files_list_nulstr_at(char ***ret, const char *suffix, int rfd, unsigned flags, const char *dirs);
 int conf_files_insert(char ***strv, const char *root, char **dirs, const char *path);
 int conf_files_list_with_replacement(
                 const char *root,
