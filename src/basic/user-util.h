9658893f037c3482d3cc154964b29626b52d4c8f
Daan De Meyer <daan.j.demeyer@gmail.com>
2023-04-11 15:21:51 +0200

    user-util: Add default_root_shell_at()

--- src/basic/user-util.h
+++ src/basic/user-util.h
@@ -152,6 +152,7 @@ int putsgent_sane(const struct sgrp *sg, FILE *stream);
 #endif // 0
 
 bool is_nologin_shell(const char *shell);
+const char* default_root_shell_at(int rfd);
 const char* default_root_shell(const char *root);
 
 #if 0 /// UNNEEDED by elogind
