2a1f96bda55469d949dd3661d1775b56b901de7f
David Tardon <dtardon@redhat.com>
2023-05-05 08:09:14 +0200

    fd-util: introduce parse_fd()
    It's a simple wrapper for safe_atoi() that returns error if the parsed
    fd is < 0 .

--- src/basic/parse-util.h
+++ src/basic/parse-util.h
@@ -23,9 +23,11 @@ int parse_mtu(int family, const char *s, uint32_t *ret);
 
 int parse_size(const char *t, uint64_t base, uint64_t *size);
 #if 0 /// UNNEEDED by elogind
+int parse_sector_size(const char *t, uint64_t *ret);
 int parse_range(const char *t, unsigned *lower, unsigned *upper);
 int parse_errno(const char *t);
 #endif // 0
+int parse_fd(const char *t);
 
 #define SAFE_ATO_REFUSE_PLUS_MINUS (1U << 30)
 #define SAFE_ATO_REFUSE_LEADING_ZERO (1U << 29)
