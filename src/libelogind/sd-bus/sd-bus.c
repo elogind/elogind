8b2d831cbf6733afd84349db3a58d276ffd73e74
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2023-05-18 17:03:53 +0200

    sd-bus: do not assert if bus description is not set
    The code has an explicit fallback using runtime_scope_to_string(), which
    is also documented in the man page. So -ENXIO should only be returned when
    the fallback doesn't work, i.e. bus->runtime_scope == -EINVAL.
    
    Fixup for f4b2933ee7890e5d414ab266d8586f19027a2bd9.
    
    This should fix the following errors during boot:
    May 18 16:05:37 fedora systemd-update-utmp[263]: Assertion 'bus->description' failed at src/libsystemd/sd-bus/sd-bus.c:4201, function sd_bus_get_description(). Ignoring.
    May 18 16:05:38 fedora systemctl[360]: Assertion 'bus->description' failed at src/libsystemd/sd-bus/sd-bus.c:4201, function sd_bus_get_description(). Ignoring.
    May 18 16:05:38 fedora systemctl[363]: Assertion 'bus->description' failed at src/libsystemd/sd-bus/sd-bus.c:4201, function sd_bus_get_description(). Ignoring.
    May 18 16:05:38 fedora systemd-update-utmp[372]: Assertion 'bus->description' failed at src/libsystemd/sd-bus/sd-bus.c:4201, function sd_bus_get_description(). Ignoring.
    May 18 16:05:38 fedora @ystemctl[387]: Assertion 'bus->description' failed at src/libsystemd/sd-bus/sd-bus.c:4201, function sd_bus_get_description(). Ignoring.
    May 18 16:05:40 fedora systemd-update-utmp[477]: Assertion 'bus->description' failed at src/libsystemd/sd-bus/sd-bus.c:4201, function sd_bus_get_description(). Ignoring.
    May 18 16:05:41 fedora systemd-homed[509]: Assertion 'bus->description' failed at src/libsystemd/sd-bus/sd-bus.c:4201, function sd_bus_get_description(). Ignoring.
    May 18 16:05:41 fedora systemd-logind[510]: Assertion 'bus->description' failed at src/libsystemd/sd-bus/sd-bus.c:4201, function sd_bus_get_description(). Ignoring.
    May 18 16:05:41 fedora systemd-update-utmp[529]: Assertion 'bus->description' failed at src/libsystemd/sd-bus/sd-bus.c:4201, function sd_bus_get_description(). Ignoring.
    May 18 16:05:42 fedora systemd-user-runtime-dir[531]: Assertion 'bus->description' failed at src/libsystemd/sd-bus/sd-bus.c:4201, function sd_bus_get_description(). Ignoring.
    May 18 16:05:42 fedora systemctl[542]: Assertion 'bus->description' failed at src/libsystemd/sd-bus/sd-bus.c:4201, function sd_bus_get_description(). Ignoring.
    May 18 16:05:42 fedora systemd-hostnamed[556]: Assertion 'bus->description' failed at src/libsystemd/sd-bus/sd-bus.c:4201, function sd_bus_get_description(). Ignoring.
    
    (cherry picked from commit aa77ed55e1dc0f7bcb633d637eb604cdc3807957)
    (cherry picked from commit e91557a1e08c4f34b8ce91636e316143a60c4490)

--- src/libelogind/sd-bus/sd-bus.c
+++ src/libelogind/sd-bus/sd-bus.c
@@ -4156,7 +4156,6 @@ _public_ int sd_bus_get_description(sd_bus *bus, const char **description) {
         assert_return(bus, -EINVAL);
         assert_return(bus = bus_resolve(bus), -ENOPKG);
         assert_return(description, -EINVAL);
-        assert_return(bus->description, -ENXIO);
         assert_return(!bus_pid_changed(bus), -ECHILD);
 
         if (bus->description)
@@ -4166,7 +4165,7 @@ _public_ int sd_bus_get_description(sd_bus *bus, const char **description) {
         else if (bus->is_user)
                 *description = "user";
         else
-                *description = NULL;
+                return -ENXIO;
 
         return 0;
 }
