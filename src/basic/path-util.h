318c7ad85abda13a6a46fde8ba246d8dd53e298a
Lennart Poettering <lennart@poettering.net>
2023-05-19 14:47:37 +0200

    stat-util: rename files_same() → inode_same()
    Let's be more accurate about what this function does: it checks whether
    the underlying reported inode is the same. Internally, this already uses
    a better named stat_inode_same() call, hence let's similarly name the
    wrapping function following the same logic.
    
    Similar for files_same_at() and path_equal_or_same_files().
    
    No code changes, just some renaming.

--- src/basic/path-util.h
+++ src/basic/path-util.h
@@ -83,9 +83,8 @@ static inline bool path_equal_filename(const char *a, const char *b) {
         return path_compare_filename(a, b) == 0;
 }
 
-bool path_equal_or_files_same(const char *a, const char *b, int flags);
-#if 0 /// UNNEEDED by elogind
-#endif // 0
+/// elogind empty mask removed (UNNEEDED by elogind)
+bool path_equal_or_inode_same(const char *a, const char *b, int flags);
 
 char* path_extend_internal(char **x, ...);
 #define path_extend(x, ...) path_extend_internal(x, __VA_ARGS__, POINTER_MAX)
