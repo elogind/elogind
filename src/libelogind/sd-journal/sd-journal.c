edc0e7f571816ffa19d55a3ff032e00a09a555d0
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-05-28 14:20:27 +0900

    sd-journal: introduce sd_journal_step_one()
    After the commit 7a4ee861615101ddd2f95056cf30e69e41da86ce,
    sd_journal_next() following sd_journal_seek_tail() takes no-op,
    and we need to call sd_journal_previous(). This may be useful in
    some cases, e.g. to fix the issue explained in the previous commit.

--- src/libelogind/sd-journal/sd-journal.c
+++ src/libelogind/sd-journal/sd-journal.c
@@ -1003,6 +1003,16 @@ _public_ int sd_journal_previous(sd_journal *j) {
 }
 
 #if 0 /// UNNEEDED by elogind
+_public_ int sd_journal_step_one(sd_journal *j, int advanced) {
+        assert_return(j, -EINVAL);
+
+        if (j->current_location.type == LOCATION_HEAD)
+                return sd_journal_next(j);
+        if (j->current_location.type == LOCATION_TAIL)
+                return sd_journal_previous(j);
+        return real_journal_next(j, advanced ? DIRECTION_DOWN : DIRECTION_UP);
+}
+
 static int real_journal_next_skip(sd_journal *j, direction_t direction, uint64_t skip) {
         int c = 0, r;
 
