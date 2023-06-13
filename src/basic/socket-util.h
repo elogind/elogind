2831d39ffd814201a6f21d7b7d7443b7e2cf3ce7
Lennart Poettering <lennart@poettering.net>
2023-06-13 10:15:59 +0200

    socket: bump listen() backlog to INT_MAX everywhere
    This is a rework of #24764 by Cristian Rodríguez
    <crodriguez@owncloud.com>, which stalled.
    
    Instead of assigning -1 we'll use a macro defined to INT_MAX however.
    
    (cherry picked from commit 768fcd779fbb9fd86932da4bef031260b88da210)
    (cherry picked from commit fec335082f59ec9e86cc2dd20a5e4001e6a71d9b)

--- src/basic/socket-util.h
+++ src/basic/socket-util.h
@@ -360,3 +360,10 @@ int socket_get_mtu(int fd, int af, size_t *ret);
 #define UCRED_INVALID { .pid = 0, .uid = UID_INVALID, .gid = GID_INVALID }
 
 int connect_unix_path(int fd, int dir_fd, const char *path);
+
+/* libc's SOMAXCONN is defined to 128 or 4096 (at least on glibc). But actually, the value can be much
+ * larger. In our codebase we want to set it to the max usually, since noawadays socket memory is properly
+ * tracked by memcg, and hence we don't need to enforce extra limits here. Moreover, the kernel caps it to
+ * /proc/sys/net/core/somaxconn anyway, thus by setting this to unbounded we just make that sysctl file
+ * authoritative. */
+#define SOMAXCONN_DELUXE INT_MAX
