fd623d8158d2773cbc0f2398b106db1bb9acca0f
Luca Boccassi <bluca@debian.org>
2023-06-26 16:13:49 +0100

    serialize: add serialize_bool_elide() helper
    In many case we serialize into objects that have been zero-initialized.
    To save some time and resources when there are a lot of booleans, add a
    helper that serializes only when the boolean is true.

--- src/shared/serialize.h
+++ src/shared/serialize.h
@@ -23,6 +23,9 @@ static inline int serialize_bool(FILE *f, const char *key, bool b) {
         return serialize_item(f, key, yes_no(b));
 }
 #endif // 0
+static inline int serialize_bool_elide(FILE *f, const char *key, bool b) {
+        return b ? serialize_item(f, key, yes_no(b)) : 0;
+}
 
 int deserialize_usec(const char *value, usec_t *timestamp);
 #if 0 /// UNNEEDED by elogind
