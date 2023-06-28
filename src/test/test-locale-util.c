16e34cd473878e42179b6047dcc7b3590271ee8d
OMOJOLA JOSHUA <omojolajoshua@gmail.com>
2023-06-28 14:04:42 +0100

    PID1: detect battery level in initrd and if low refuse continuing to boot, print message and shut down.

--- src/test/test-locale-util.c
+++ src/test/test-locale-util.c
@@ -121,6 +121,7 @@ TEST(dump_special_glyphs) {
         dump_glyph(SPECIAL_GLYPH_RECYCLING);
         dump_glyph(SPECIAL_GLYPH_DOWNLOAD);
         dump_glyph(SPECIAL_GLYPH_SPARKLES);
+        dump_glyph(SPECIAL_GLYPH_LOW_BATTERY);
         dump_glyph(SPECIAL_GLYPH_WARNING_SIGN);
 }
 
