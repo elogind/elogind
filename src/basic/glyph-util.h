b60aa510c394b4e0330d9a79c40b2d78ba3faa62
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-06-29 02:43:17 +0900

    battery-check: several follow-ups
    Follow-ups for e3d4148d50909119c4e9327e6ad96d3ca7f4661a.
    
    - add reference to initrd-battery-check.service in man page, and move
      its section from 1 to 8,
    - add link to man page in help message,
    - introduce ERRNO_IS_NO_PLYMOUTH(),
    - propagate error in battery_check_send_plymouth_message(),
    - rename battery_check_send_plymouth_message() -> plymouth_send_message(),
    - return earlier when the first battery level check passed to reduce
      indentation,
    - fix potential use of invalid fd on battery restored,
    - do not use emoji for /dev/console,
    - add simple test (mostly for coverity),
    
    etc, etc...

--- src/basic/glyph-util.h
+++ src/basic/glyph-util.h
@@ -50,17 +50,13 @@ typedef enum SpecialGlyph {
         _SPECIAL_GLYPH_INVALID = -EINVAL,
 } SpecialGlyph;
 
-const char *special_glyph_full(SpecialGlyph code, bool force_utf) _const_;
-
 bool emoji_enabled(void);
 
 #if 0 /// UNNEEDED by elogind
+const char *special_glyph_full(SpecialGlyph code, bool force_utf) _const_;
+
 static inline const char *special_glyph(SpecialGlyph code) {
         return special_glyph_full(code, false);
-}
-
-static inline const char *special_glyph_force_utf(SpecialGlyph code) {
-        return special_glyph_full(code, true);
 }
 
 static inline const char *special_glyph_check_mark(bool b) {
