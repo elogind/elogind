23a05d0f10eeeda93f472d89e6c6f6b5222c7126
Mike Yuan <me@yhndnzj.com>
2023-07-10 20:42:01 +0800

    path-util: make path_equal_or_inode_same static inline
    No functional change, just refactoring.

--- src/basic/path-util.c
+++ src/basic/path-util.c
@@ -492,9 +492,6 @@ int path_compare(const char *a, const char *b) {
         }
 }
 
-bool path_equal_or_inode_same(const char *a, const char *b, int flags) {
-        return path_equal(a, b) || inode_same(a, b, flags) > 0;
-}
 
 #if 0 /// UNNEEDED by elogind
 int path_compare_filename(const char *a, const char *b) {
