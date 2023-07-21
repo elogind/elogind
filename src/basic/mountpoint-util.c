90a9fa6cce78bbecb4314dc0751f5408087313d2
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-07-22 01:15:59 +0900

    mountpoint-util: statx() may return EINVAL if the kernel does not support the syscall
    When statx() is called with AT_STATX_DONT_SYNC, glibc's fallback code
    returns EINVAL. See statx_generic() in glibc.

--- src/basic/mountpoint-util.c
+++ src/basic/mountpoint-util.c
@@ -359,21 +359,9 @@ int path_is_mount_point(const char *t, const char *root, int flags) {
         return fd_is_mount_point(fd, last_path_component(t), flags);
 }
 
-int path_get_mnt_id_at_fallback(int dir_fd, const char *path, int *ret) {
-        int r;
-
-        assert(dir_fd >= 0 || dir_fd == AT_FDCWD);
-        assert(ret);
-
-        r = name_to_handle_at_loop(dir_fd, path, NULL, ret, isempty(path) ? AT_EMPTY_PATH : 0);
-        if (r == 0 || is_name_to_handle_at_fatal_error(r))
-                return r;
-
-        return fd_fdinfo_mnt_id(dir_fd, path, isempty(path) ? AT_EMPTY_PATH : 0, ret);
-}
-
 int path_get_mnt_id_at(int dir_fd, const char *path, int *ret) {
         STRUCT_NEW_STATX_DEFINE(buf);
+        int r;
 
         assert(dir_fd >= 0 || dir_fd == AT_FDCWD);
         assert(ret);
@@ -398,7 +386,11 @@ int path_get_mnt_id_at(int dir_fd, const char *path, int *ret) {
                 return 0;
         }
 
-        return path_get_mnt_id_at_fallback(dir_fd, path, ret);
+        r = name_to_handle_at_loop(dir_fd, path, NULL, ret, isempty(path) ? AT_EMPTY_PATH : 0);
+        if (r == 0 || is_name_to_handle_at_fatal_error(r))
+                return r;
+
+        return fd_fdinfo_mnt_id(dir_fd, path, isempty(path) ? AT_EMPTY_PATH : 0, ret);
 }
 
 #if 0 /// UNNEEDED by elogind
