5ac7cd3048746c2f33ac75f34ca715e715ab439a
Lennart Poettering <lennart@poettering.net>
2023-08-14 13:29:07 +0200

    efi-api: export UUID converter calls
    (while exporting, do some minor simplifications)

--- src/shared/efi-api.h
+++ src/shared/efi-api.h
@@ -75,3 +75,6 @@ static inline char *efi_tilt_backslashes(char *s) {
         return string_replace_char(s, '\\', '/');
 }
 #endif // 0
+
+sd_id128_t efi_guid_to_id128(const void *guid);
+void efi_id128_to_guid(sd_id128_t id, void *ret_guid);
