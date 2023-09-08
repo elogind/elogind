ca80df56f5b04865095a58d81e14d0d17904f6f7
Lennart Poettering <lennart@poettering.net>
2023-09-08 17:02:11 +0200

    serialize: add common deserialize_read_line() helper and use it everywhere
    This introduces a common helper replacing three very similar uses.
    
    Prompted by review of: #27890

--- src/shared/serialize.h
+++ src/shared/serialize.h
@@ -27,6 +27,8 @@ static inline int serialize_bool_elide(FILE *f, const char *key, bool b) {
         return b ? serialize_item(f, key, yes_no(b)) : 0;
 }
 
+int deserialize_read_line(FILE *f, char **ret);
+
 int deserialize_usec(const char *value, usec_t *timestamp);
 #if 0 /// UNNEEDED by elogind
 int deserialize_dual_timestamp(const char *value, dual_timestamp *t);
