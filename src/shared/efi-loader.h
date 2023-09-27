24dd694107d60d7073b552f75f62ec5655512194
Lennart Poettering <lennart@poettering.net>
2023-09-27 11:50:14 +0200

    efi-loader: rename efi_stub_measured() → efi_measured_uki()
    Let's say "uki" rather than "stub", since that is just too generic, and
    we shouldn't limit us to our own stub anyway, but generally define a
    concept of a "measured UKI", which is a UKI that measures its part to
    PCR 11.
    
    This is mostly preparation for exposing this check to the user via
    ConditionSecurity=.

--- src/shared/efi-loader.h
+++ src/shared/efi-loader.h
@@ -22,8 +22,8 @@ int efi_loader_get_features(uint64_t *ret);
 #if 0 /// UNNEEDED by elogind
 int efi_stub_get_features(uint64_t *ret);
 
-int efi_stub_measured(int log_level);
 #endif // 0
+int efi_measured_uki(int log_level);
 
 int efi_loader_get_config_timeout_one_shot(usec_t *ret);
 int efi_loader_update_entry_one_shot_cache(char **cache, struct stat *cache_stat);
@@ -54,7 +54,7 @@ static inline int efi_stub_get_features(uint64_t *ret) {
         return -EOPNOTSUPP;
 }
 
-static inline int efi_stub_measured(int log_level) {
+static inline int efi_measured_uki(int log_level) {
         return log_full_errno(log_level, SYNTHETIC_ERRNO(EOPNOTSUPP),
                               "Compiled without support for EFI");
 }
