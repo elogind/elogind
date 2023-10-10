24a3764df29bc2fd9f8b6646debb398356554c62
Luca Boccassi <bluca@debian.org>
2023-10-10 18:50:36 +0100

    core: fix checking for extension-releases for ExtensionImages/Directories
    The parsing is done after the image has been opened, not before, as it
    cannot be done on an block device. Also fix returning on any error for
    ExtensionDirectories, not just ENOENT.
    
    Follow-up for 55ea4ef096543d2bceea9315868d5aca945d7a57

--- src/shared/mount-util.c
+++ src/shared/mount-util.c
@@ -892,6 +892,7 @@ static int mount_in_namespace_legacy(
                                 /* required_host_os_release_id= */ NULL,
                                 /* required_host_os_release_version_id= */ NULL,
                                 /* required_host_os_release_sysext_level= */ NULL,
+                                /* required_host_os_release_confext_level= */ NULL,
                                 /* required_sysext_scope= */ NULL,
                                 /* ret_image= */ NULL);
         else
@@ -1110,6 +1111,7 @@ static int mount_in_namespace(
                                 /* required_host_os_release_id= */ NULL,
                                 /* required_host_os_release_version_id= */ NULL,
                                 /* required_host_os_release_sysext_level= */ NULL,
+                                /* required_host_os_release_confext_level= */ NULL,
                                 /* required_sysext_scope= */ NULL,
                                 &img);
                 if (r < 0)
