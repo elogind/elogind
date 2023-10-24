systemd/systemd

sd-id128: make sd_id128_get_boot() and friend return -ENOMEDIUM

src/libsystemd/sd-id128/sd-id128.c
@@ -148,6 +148,9 @@ _public_ int sd_id128_get_boot(sd_id128_t *ret) {
                 r = id128_read("/proc/sys/kernel/random/boot_id", ID128_UUID, &saved_boot_id);
                 if (r < 0)
                         return r;
+
+                if (sd_id128_is_null(saved_machine_id))
+                        return -ENOMEDIUM;
         }
 
         *ret = saved_boot_id;

elogind/elogind

src/libelogind/sd-id128/sd-id128.c
@@ -152,6 +152,9 @@ _public_ int sd_id128_get_boot(sd_id128_t *ret) {
                 r = id128_read("/proc/sys/kernel/random/boot_id", ID128_FORMAT_UUID, &saved_boot_id);
                 if (r < 0)
                         return r;
+
+                if (sd_id128_is_null(saved_boot_id))
+                        return -ENOMEDIUM;
         }
 
         *ret = saved_boot_id;