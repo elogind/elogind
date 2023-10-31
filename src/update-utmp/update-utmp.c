e11aec1c501ae2c958dd4267d4b3d00d81029fbd
Sven Eden <sven@eden-worx.com>
2023-10-31 14:21:23 +0100

    Comment out all unused includes in update-utmp/
    Signed-off-by: Sven Eden <sven@eden-worx.com>

--- src/update-utmp/update-utmp.c
+++ src/update-utmp/update-utmp.c
@@ -21,8 +21,8 @@
 //#include "process-util.h"
 //#include "special.h"
 #include "stdio-util.h"
-#include "strv.h"
-#include "unit-name.h"
+//#include "strv.h"
+//#include "unit-name.h"
 #include "utmp-wtmp.h"
 
 /// Additional includes needed by elogind
