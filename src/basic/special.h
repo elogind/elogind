systemd/systemd

hibernate-resume: rework to follow the logic of sleep.c and use main-func.h
Preparation for #27247

src/basic/special.h
@@ -93,6 +93,7 @@
 #define SPECIAL_GROWFS_ROOT_SERVICE "systemd-growfs-root.service"
 #define SPECIAL_PCRFS_SERVICE "systemd-pcrfs@.service"
 #define SPECIAL_PCRFS_ROOT_SERVICE "systemd-pcrfs-root.service"
+#define SPECIAL_HIBERNATE_RESUME_SERVICE "systemd-hibernate-resume.service"
 
 /* Services systemd relies on */
 #define SPECIAL_DBUS_SERVICE "dbus.service"

elogind/elogind

src/basic/special.h
@@ -94,6 +94,7 @@
 #define SPECIAL_GROWFS_ROOT_SERVICE "elogind-growfs-root.service"
 #define SPECIAL_PCRFS_SERVICE "elogind-pcrfs@.service"
 #define SPECIAL_PCRFS_ROOT_SERVICE "elogind-pcrfs-root.service"
+#define SPECIAL_HIBERNATE_RESUME_SERVICE "elogind-hibernate-resume.service"
 
 /* Services systemd relies on */
 #define SPECIAL_DBUS_SERVICE "dbus.service"