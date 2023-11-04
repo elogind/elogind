d42517d1789f9edb459820eb87b41d7969904986
Sven Eden <sven@eden-worx.com>
2023-11-04 11:21:48 +0100

    Revert generat-cap-list.sh back to upstream version, elogind extras are no longer needed.
    Signed-off-by: Sven Eden <sven@eden-worx.com>

--- src/basic/generate-cap-list.sh
+++ src/basic/generate-cap-list.sh
@@ -3,12 +3,6 @@
 set -eu
 set -o pipefail
 
-#if 0 /// elogind needs musl_missing.sh, which is in shared.
-# ${1:?} -dM -include linux/capability.h -include "${2:?}" -include "${3:?}" - </dev/null | \
-#        awk '/^#define[ \t]+CAP_[A-Z_]+[ \t]+/ { print $2; }' | \
-#        grep -v CAP_LAST_CAP
-#else // 0
-$1 -dM -include linux/capability.h -I"$2"/src/shared -include "$3" -include "$4" - </dev/null | \
-        awk '/^#define[ \t]+CAP_[A-Z_]+[ \t]+/ { print $2; }' | \
-        grep -v CAP_LAST_CAP
-#endif // 0
+${1:?} -dM -include linux/capability.h -include "${2:?}" -include "${3:?}" - </dev/null | \
+       awk '/^#define[ \t]+CAP_[A-Z_]+[ \t]+/ { print $2; }' | \
+       grep -v CAP_LAST_CAP
