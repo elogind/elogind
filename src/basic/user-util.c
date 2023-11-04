01e05d5c503cbec2050bbd952269d806b7bfcda1
Sven Eden <sven@eden-worx.com>
2023-11-04 15:09:43 +0100

    Adopt upstream dependency reordering on elogind specific libraries and executables.
    Signed-off-by: Sven Eden <sven@eden-worx.com>

--- src/basic/user-util.c
+++ src/basic/user-util.c
@@ -15,7 +15,7 @@
 #include "alloc-util.h"
 #include "chase.h"
 #include "errno-util.h"
-//#include "fd-util.h"
+#include "fd-util.h"
 #include "fileio.h"
 #include "format-util.h"
 #include "lock-util.h"
