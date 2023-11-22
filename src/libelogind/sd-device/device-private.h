4393368c5168d487486acebc9c2875f32bde77b0
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-11-22 13:05:26 +0900

    sd-device: introduce device_has_db() helper function

--- src/libelogind/sd-device/device-private.h
+++ src/libelogind/sd-device/device-private.h
@@ -77,6 +77,7 @@ int device_clone_with_db(sd_device *device, sd_device **ret);
 
 int device_tag_index(sd_device *dev, sd_device *dev_old, bool add);
 bool device_should_have_db(sd_device *device);
+int device_has_db(sd_device *device);
 int device_update_db(sd_device *device);
 int device_delete_db(sd_device *device);
 #endif // 0
