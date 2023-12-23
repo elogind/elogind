4538a966aa7741e14c517cc03b78bcdf460bb8f9
Yu Watanabe <watanabe.yu+github@gmail.com>
2023-12-24 01:49:57 +0900

    test: make assert_return() critical by default on fuzzer and unit tests
    Several test cases intentionally trigger assert_return(). So, to avoid
    the entire test fails, this introduces several macros that tentatively
    make assert_return() not critical.

--- src/shared/tests.c
+++ src/shared/tests.c
@@ -21,6 +21,7 @@
 #include "fd-util.h"
 //#include "fs-util.h"
 #include "log.h"
+#include "mountpoint-util.h"
 #include "namespace-util.h"
 //#include "path-util.h"
 //#include "process-util.h"
@@ -115,6 +116,7 @@ bool slow_tests_enabled(void) {
 }
 
 void test_setup_logging(int level) {
+        log_set_assert_return_is_critical(true);
         log_set_max_level(level);
         log_parse_environment();
         log_open();
@@ -271,7 +273,7 @@ static int enter_cgroup(char **ret_cgroup, bool enter_subroot) {
                 log_warning_errno(r, "Couldn't allocate a scope unit for this test, proceeding without.");
 
         r = cg_pid_get_path(NULL, 0, &cgroup_root);
-        if (r == -ENOMEDIUM)
+        if (IN_SET(r, -ENOMEDIUM, -ENOENT))
                 return log_warning_errno(r, "cg_pid_get_path(NULL, 0, ...) failed: %m");
         assert(r >= 0);
 
