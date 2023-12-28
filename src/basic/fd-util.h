be2a2c1dcbb11cc0bb1b33f22717f0f6de25f7e3
Mike Yuan <me@yhndnzj.com>
2023-12-28 18:17:52 +0800

    fd-util: modernization
    (cherry picked from commit 1276e633708c2d8b41a3d4c0a981a830ee537465)

--- src/basic/fd-util.h
+++ src/basic/fd-util.h
@@ -32,10 +32,10 @@ static inline int safe_close_above_stdio(int fd) {
         return safe_close(fd);
 }
 
-void close_many(const int fds[], size_t n_fd);
 #if 0 /// UNNEEDED by elogind
-void close_many_unset(int fds[], size_t n_fd);
 #endif // 0
+void close_many(const int fds[], size_t n_fds);
+void close_many_unset(int fds[], size_t n_fds);
 void close_many_and_free(int *fds, size_t n_fds);
 
 int fclose_nointr(FILE *f);
