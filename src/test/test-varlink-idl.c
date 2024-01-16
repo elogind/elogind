systemd/systemd

varlink: define interface io.systemd.service

src/test/test-varlink-idl.c
@@ -15,6 +15,7 @@
 #include "varlink-io.systemd.Resolve.h"
 #include "varlink-io.systemd.UserDatabase.h"
 #include "varlink-io.systemd.oom.h"
+#include "varlink-io.systemd.service.h"
 #include "varlink-io.systemd.sysext.h"
 #include "varlink-org.varlink.service.h"
 
@@ -137,6 +138,8 @@ TEST(parse_format) {
         test_parse_format_one(&vl_interface_io_systemd);
         print_separator();
         test_parse_format_one(&vl_interface_io_systemd_PCRExtend);
+        print_separator();
+        test_parse_format_one(&vl_interface_io_systemd_service);
         print_separator();
         test_parse_format_one(&vl_interface_io_systemd_sysext);
         print_separator();

elogind/elogind

src/test/test-varlink-idl.c
@@ -16,6 +16,7 @@
 #include "varlink-io.elogind.oom.h"
 #include "varlink-io.elogind.PCRExtend.h"
 #include "varlink-io.elogind.sysext.h"
+#include "varlink-io.elogind.service.h"
 #include "varlink-org.varlink.service.h"
 
 static VARLINK_DEFINE_ENUM_TYPE(
@@ -139,6 +140,8 @@ TEST(parse_format) {
         test_parse_format_one(&vl_interface_io_elogind_PCRExtend);
         print_separator();
         test_parse_format_one(&vl_interface_io_elogind_sysext);
+        test_parse_format_one(&vl_interface_io_elogind_service);
+        print_separator();
         print_separator();
         test_parse_format_one(&vl_interface_xyz_test);
 }