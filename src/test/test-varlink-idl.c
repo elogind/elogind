systemd/systemd

sysext: make some calls available via varlink

src/test/test-varlink-idl.c
@@ -15,6 +15,7 @@
 #include "varlink-io.systemd.Resolve.h"
 #include "varlink-io.systemd.UserDatabase.h"
 #include "varlink-io.systemd.oom.h"
+#include "varlink-io.systemd.sysext.h"
 #include "varlink-org.varlink.service.h"
 
 static VARLINK_DEFINE_ENUM_TYPE(
@@ -136,6 +137,8 @@ TEST(parse_format) {
         test_parse_format_one(&vl_interface_io_systemd);
         print_separator();
         test_parse_format_one(&vl_interface_io_systemd_PCRExtend);
+        print_separator();
+        test_parse_format_one(&vl_interface_io_systemd_sysext);
         print_separator();
         test_parse_format_one(&vl_interface_xyz_test);
 }

elogind/elogind

src/test/test-varlink-idl.c
@@ -15,6 +15,7 @@
 #include "varlink-io.elogind.UserDatabase.h"
 #include "varlink-io.elogind.oom.h"
 #include "varlink-io.elogind.PCRExtend.h"
+#include "varlink-io.elogind.sysext.h"
 #include "varlink-org.varlink.service.h"
 
 static VARLINK_DEFINE_ENUM_TYPE(
@@ -137,6 +138,8 @@ TEST(parse_format) {
         print_separator();
         test_parse_format_one(&vl_interface_io_elogind_PCRExtend);
         print_separator();
+        test_parse_format_one(&vl_interface_io_elogind_sysext);
+        print_separator();
         test_parse_format_one(&vl_interface_xyz_test);
 }
 
@@ -152,6 +155,12 @@ TEST(parse) {
 
         assert_se(varlink_idl_parse(text, NULL, NULL, &parsed) >= 0);
         test_parse_format_one(parsed);
+
+        assert_se(varlink_idl_parse("interface org.freedesktop.Foo\n"
+                                    "type Foo (b: bool, c: foo, c: int)", NULL, NULL, NULL) == -ENETUNREACH); /* unresolved type */
+        assert_se(varlink_idl_parse("interface org.freedesktop.Foo\n"
+                                    "type Foo ()", NULL, NULL, NULL) == -EBADMSG); /* empty struct/enum */
+
 }
 
 TEST(interface_name_is_valid) {