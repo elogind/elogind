systemd/systemd

pcrextend: make pcrextend tool acccessible via varlink
This is primarily supposed to be a 1st step with varlinkifying our
various command line tools, and excercise in how this might look like
across our codebase one day. However, at AllSystemsGo! 2023 it was
requested that we provide an API to do a PCR measurement along with a
matching event log record, and this provides that.

src/test/test-varlink-idl.c
@@ -10,6 +10,7 @@
 #include "varlink-io.systemd.h"
 #include "varlink-io.systemd.Journal.h"
 #include "varlink-io.systemd.ManagedOOM.h"
+#include "varlink-io.systemd.PCRExtend.h"
 #include "varlink-io.systemd.Resolve.Monitor.h"
 #include "varlink-io.systemd.Resolve.h"
 #include "varlink-io.systemd.UserDatabase.h"
@@ -133,6 +134,8 @@ TEST(parse_format) {
         test_parse_format_one(&vl_interface_io_systemd_oom);
         print_separator();
         test_parse_format_one(&vl_interface_io_systemd);
+        print_separator();
+        test_parse_format_one(&vl_interface_io_systemd_PCRExtend);
         print_separator();
         test_parse_format_one(&vl_interface_xyz_test);
 }

elogind/elogind

src/test/test-varlink-idl.c
@@ -14,6 +14,7 @@
 #include "varlink-io.elogind.Resolve.h"
 #include "varlink-io.elogind.UserDatabase.h"
 #include "varlink-io.elogind.oom.h"
+#include "varlink-io.elogind.PCRExtend.h"
 #include "varlink-org.varlink.service.h"
 
 static VARLINK_DEFINE_ENUM_TYPE(
@@ -133,6 +134,8 @@ TEST(parse_format) {
         test_parse_format_one(&vl_interface_io_elogind_oom);
         print_separator();
         test_parse_format_one(&vl_interface_io_elogind);
+        print_separator();
+        test_parse_format_one(&vl_interface_io_elogind_PCRExtend);
         print_separator();
         test_parse_format_one(&vl_interface_xyz_test);
 }