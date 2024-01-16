systemd/systemd

udev-util: add wrapper for sd_device_get_property_value()
... that allows to pass additional properties to fall back to.

src/shared/udev-util.c
@@ -382,3 +382,26 @@ bool udev_available(void) {
 
         return (cache = (path_is_read_only_fs("/sys/") <= 0));
 }
+
+int udev_get_property_value(sd_device *dev, const char *prop, Hashmap *extra_props, const char **ret) {
+        const char *value;
+        int r;
+
+        assert(dev);
+        assert(prop);
+        assert(ret);
+
+        r = sd_device_get_property_value(dev, prop, &value);
+        if (r < 0) {
+                if (r != -ENOENT)
+                        return r;
+
+                value = hashmap_get(extra_props, prop);
+                if (!value)
+                        return -ENOENT;
+        }
+
+        *ret = value;
+
+        return 1;
+}

elogind/elogind

src/shared/udev-util.c
@@ -419,3 +419,30 @@ int device_get_model_string(sd_device *device, const char **ret) {
 
         return -ENOENT;
 }
+
+int device_get_property_value_with_fallback(
+                sd_device *device,
+                const char *prop,
+                Hashmap *extra_props,
+                const char **ret) {
+        const char *value;
+        int r;
+
+        assert(device);
+        assert(prop);
+        assert(ret);
+
+        r = sd_device_get_property_value(device, prop, &value);
+        if (r < 0) {
+                if (r != -ENOENT)
+                        return r;
+
+                value = hashmap_get(extra_props, prop);
+                if (!value)
+                        return -ENOENT;
+        }
+
+        *ret = value;
+
+        return 1;
+}