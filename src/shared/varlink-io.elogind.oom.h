2faaffcb14dcba7f3f0c7043f14c7d275fa8426b
Sven Eden <sven@eden-worx.com>
2024-01-28 18:15:56 +0100

    Remove varlink sources elogind doesn't need
    Signed-off-by: Sven Eden <sven@eden-worx.com>

--- src/shared/varlink-io.elogind.oom.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: LGPL-2.1-or-later */
-#pragma once
-
-#include "varlink-idl.h"
-
-extern const VarlinkSymbol vl_type_ControlGroup;
-extern const VarlinkInterface vl_interface_io_elogind_oom;
