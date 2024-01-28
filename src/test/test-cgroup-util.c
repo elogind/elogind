5b1d5e2c48cae3b4abe2f54eac8668532d75db7d
Sven Eden <sven@eden-worx.com>
2024-01-28 18:51:35 +0100

    Fix test/test-cgroup-util.c
    Signed-off-by: Sven Eden <sven@eden-worx.com>

--- src/test/test-cgroup-util.c
+++ src/test/test-cgroup-util.c
@@ -218,15 +218,15 @@ TEST(proc) {
                 if (pidref_is_kernel_thread(&pid) != 0)
                         continue;
 
-#if 0 /// Not supported by elogind
-#endif // 0
                 cg_pid_get_path(SYSTEMD_CGROUP_CONTROLLER, pid.pid, &path);
                 cg_pid_get_path_shifted(pid.pid, NULL, &path_shifted);
                 cg_pid_get_owner_uid(pid.pid, &uid);
                 cg_pid_get_session(pid.pid, &session);
+#if 0 /// Not supported by elogind
                 cg_pid_get_unit(pid.pid, &unit);
                 cg_pid_get_user_unit(pid.pid, &user_unit);
                 cg_pid_get_machine_name(pid.pid, &machine);
+#endif // 0
                 cg_pid_get_slice(pid.pid, &slice);
 
                 printf(PID_FMT"\t%s\t%s\t"UID_FMT"\t%s\t%s\t%s\t%s\t%s\n",
