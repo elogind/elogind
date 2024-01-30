a85252cb93f5df4126551dfb3ae8711f4004a17f
Sam Leonard <sam.leonard@codethink.co.uk>
2024-01-30 13:24:22 +0000

    basic/namespace-util: add parse_userns_uid_range

--- src/basic/namespace-util.h
+++ src/basic/namespace-util.h
@@ -57,3 +57,5 @@ int userns_acquire(const char *uid_map, const char *gid_map);
 int netns_acquire(void);
 int in_same_namespace(pid_t pid1, pid_t pid2, NamespaceType type);
 #endif // 0
+
+int parse_userns_uid_range(const char *s, uid_t *ret_uid_shift, uid_t *ret_uid_range);
