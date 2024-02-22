548ea1ae6fb13a24cff4ce00c5a7d60f0b6c94cb
Mike Yuan <me@yhndnzj.com>
2024-02-22 13:03:09 +0800

    shared/mount-setup: minor modernization

--- src/shared/mount-setup.h
+++ src/shared/mount-setup.h
@@ -4,13 +4,13 @@
 #include <stdbool.h>
 
 #if 0 /// UNNEEDED by elogind
+bool mount_point_is_api(const char *path);
+bool mount_point_ignore(const char *path);
+
 int mount_setup_early(void);
 #endif // 0
 int mount_setup(bool loaded_policy, bool leave_propagation);
 
 #if 0 /// UNNEEDED by elogind
 int mount_cgroup_controllers(void);
-
-bool mount_point_is_api(const char *path);
-bool mount_point_ignore(const char *path);
 #endif // 0
