9aaf4b56f94d0f0c67617a5655d2ca5d61b64da8
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2024-03-19 21:08:52 +0100

    Replace strdup_or_null() by strdup_to()
    I didn't know that this helper existed… It is very similar to strdup_to_full(),
    but all callers can actually be replaced by strdup_to(), which has more fitting
    semantics.

--- src/basic/string-util.h
+++ src/basic/string-util.h
@@ -325,26 +325,6 @@ bool version_is_valid_versionspec(const char *s);
 ssize_t strlevenshtein(const char *x, const char *y);
 
 #if 0 /// UNNEEDED by elogind
-static inline int strdup_or_null(const char *s, char **ret) {
-        char *c;
-
-        assert(ret);
-
-        /* This is a lot like strdup(), but is happy with NULL strings, and does not treat that as error, but
-         * copies the NULL value. */
-
-        if (!s) {
-                *ret = NULL;
-                return 0;
-        }
-
-        c = strdup(s);
-        if (!c)
-                return -ENOMEM;
-
-        *ret = c;
-        return 1;
-}
 #endif // 0
 
 char *strrstr(const char *haystack, const char *needle);
