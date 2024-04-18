18db59ea03ca756405e641b4fd52d402632a6b0d
Matteo Croce <teknoraver@meta.com>
2024-04-18 14:31:39 +0200

    use FOREACH_ELEMENT
    Use FOREACH_ELEMENT where possible. Generated with this command,
    and checked manually:
    
        git grep -l 'FOREACH_ARRAY.*ELEMENTSOF' | \
        xargs sed -ri 's/FOREACH_ARRAY\((.*), (.*), (ELEMENTSOF.*)\)/FOREACH_ELEMENT(\1, \2)/'

--- src/test/test-stat-util.c
+++ src/test/test-stat-util.c
@@ -194,7 +194,7 @@ TEST(inode_type_from_string) {
                 S_IFSOCK,
         };
 
-        FOREACH_ARRAY(m, types, ELEMENTSOF(types))
+        FOREACH_ELEMENT(m, types)
                 assert_se(inode_type_from_string(inode_type_to_string(*m)) == *m);
 }
 
