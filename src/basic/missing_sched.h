82ed402925e0a5649feba03a745c50183fe062e1
Florian Schmaus <flo@geekplace.eu>
2024-06-26 14:37:52 +0200

    core/exec-invoke: use sched_setattr instead of sched_setscheduler
    The kernel's sched_setattr interface allows for more control over a processes
    scheduling attributes as the previously used sched_setscheduler interface.
    
    Using sched_setattr is also the prerequisite for support of utilization
    clamping (UCLAMP [1], see #26705) and allows to set sched_runtime. The latter,
    sched_runtime, will probably become a relevant scheduling parameter of the
    EEVDF scheduler [2, 3], and therefore will not only apply to processes
    scheduled via SCHED_DEADLINE, but also for processes scheduled via
    SCHED_OTHER/SCHED_BATCH (i.e., most processes).
    
    1: https://docs.kernel.org/next/scheduler/sched-util-clamp.html
    2: https://lwn.net/Articles/969062/
    3: https://lwn.net/ml/linux-kernel/20240405110010.934104715@infradead.org/
    (cherry picked from commit 016e9d8d08ce66f5e81b42e0a0db398afc17336a)
    (cherry picked from commit fb7ec285c98d9eeaa69d1efda3e450e6f7207e57)
    (cherry picked from commit 02e50f7a4b53e56b051889b982fa43118c577493)
    (cherry picked from commit fc96019babd5658b140ea2f45bfda5fd101434c7)
    (cherry picked from commit 50b84f2f2d487deecca81f5d66aa254b4fd31316)

--- src/basic/missing_sched.h
+++ src/basic/missing_sched.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
+#include <linux/types.h>
 #include <sched.h>
 
 #ifndef CLONE_NEWCGROUP
@@ -23,4 +24,21 @@
 /* Not exposed yet. Defined at include/linux/sched.h */
 #ifndef TASK_COMM_LEN
 #define TASK_COMM_LEN 16
+#endif
+
+#if !HAVE_STRUCT_SCHED_ATTR
+struct sched_attr {
+        __u32 size;             /* Size of this structure */
+        __u32 sched_policy;     /* Policy (SCHED_*) */
+        __u64 sched_flags;      /* Flags */
+        __s32  sched_nice;      /* Nice value (SCHED_OTHER,
+                                         SCHED_BATCH) */
+        __u32 sched_priority;   /* Static priority (SCHED_FIFO,
+                                       SCHED_RR) */
+        /* Remaining fields are for SCHED_DEADLINE
+           and potentially soon for SCHED_OTHER/SCHED_BATCH */
+        __u64 sched_runtime;
+        __u64 sched_deadline;
+        __u64 sched_period;
+};
 #endif
