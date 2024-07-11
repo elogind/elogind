4c3d8b5eda65dd579153dc4652bb55fc49d4604e
Lennart Poettering <lennart@poettering.net>
2024-07-11 10:31:02 +0200

    terminal-util: rename set_terminal_cursor_position() → terminal_set_cursor_position()
    Let's prefix these functions with the subsystem name, and clean them up
    a bit. Specifically, drop the error logging, it's entirely duplicative,
    since every single caller does it anyway.

--- src/basic/terminal-util.c
+++ src/basic/terminal-util.c
@@ -1542,19 +1542,13 @@ void get_log_colors(int priority, const char **on, const char **off, const char 
 }
 
 #if 0 /// UNNEEDED by elogind
-int set_terminal_cursor_position(int fd, unsigned int row, unsigned int column) {
-        int r;
-        char cursor_position[STRLEN("\x1B[") + DECIMAL_STR_MAX(int) * 2 + STRLEN(";H") + 1];
-
+int terminal_set_cursor_position(int fd, unsigned row, unsigned column) {
         assert(fd >= 0);
 
+        char cursor_position[STRLEN("\x1B[" ";" "H") + DECIMAL_STR_MAX(unsigned) * 2 + 1];
         xsprintf(cursor_position, "\x1B[%u;%uH", row, column);
 
-        r = loop_write(fd, cursor_position, SIZE_MAX);
-        if (r < 0)
-                return log_warning_errno(r, "Failed to set cursor position, ignoring: %m");
-
-        return 0;
+        return loop_write(fd, cursor_position, SIZE_MAX);
 }
 #endif // 0
 
--- src/basic/terminal-util.h
+++ src/basic/terminal-util.h
@@ -100,9 +100,10 @@ bool isatty_safe(int fd);
 #if 0 /// UNNEEDED by elogind
 int reset_terminal_fd(int fd, bool switch_to_text);
 int reset_terminal(const char *name);
-int set_terminal_cursor_position(int fd, unsigned int row, unsigned int column);
 #endif // 0
 int terminal_reset_ansi_seq(int fd);
+
+int terminal_set_cursor_position(int fd, unsigned row, unsigned column);
 
 int open_terminal(const char *name, int mode);
 
