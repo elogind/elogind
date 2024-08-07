a1243d107162daed112769db0d4168d8eeb6715a
Daan De Meyer <daan.j.demeyer@gmail.com>
2024-08-07 20:44:38 +0200

    crash-handler: Call vhangup on /dev/console before spawning crash shell
    When pid 1 crashes, the getty unit for the console will happily keep
    running which means we end up with two shells competing for the same
    tty. Let's call vhangup on /dev/console to kill every other process
    attached to the console before we spawn the crash shell. The getty
    units have Restart=always but lucky for us, pid 1 just crashed in fire
    and flames so it isn't actually able to restart the getty unit.

--- src/basic/terminal-util.h
+++ src/basic/terminal-util.h
@@ -72,6 +72,7 @@ const char* color_mode_to_string(ColorMode m) _const_;
 ColorMode color_mode_from_string(const char *s) _pure_;
 
 int terminal_vhangup_fd(int fd);
+int terminal_vhangup(const char *tty);
 
 int terminal_set_size_fd(int fd, const char *ident, unsigned rows, unsigned cols);
 #endif // 0
