0b69d9793966a85839c0919a429a1025fdf71c16
Lennart Poettering <lennart@poettering.net>
2024-08-28 14:10:01 +0200

    time-util: rework localtime_or_gmtime() into localtime_or_gmtime_usec()
    We typically want to deal in usec_t, hence let's change the prototype
    accordingly, and do proper range checks. Also, make sure are not
    confused by negative times.
    
    Do something similar for mktime_or_timegm().
    
    This is a more comprehensive alternative to #34065
    
    Replaces: #34065

--- src/basic/time-util.h
+++ src/basic/time-util.h
@@ -198,9 +198,9 @@ usec_t usec_shift_clock(usec_t, clockid_t from, clockid_t to);
 #if 0 /// UNNEEDED by elogind
 int get_timezone(char **ret);
 
-time_t mktime_or_timegm(struct tm *tm, bool utc);
 #endif // 0
-struct tm *localtime_or_gmtime_r(const time_t *t, struct tm *tm, bool utc);
+int mktime_or_timegm_usec(struct tm *tm, bool utc, usec_t *ret);
+int localtime_or_gmtime_usec(usec_t t, bool utc, struct tm *ret);
 
 #if 0 /// UNNEEDED by elogind
 uint32_t usec_to_jiffies(usec_t usec);
