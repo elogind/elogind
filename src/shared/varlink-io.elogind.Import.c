52350718fdc7523104663673db78d70502a384e9
Lennart Poettering <lennart@poettering.net>
2024-10-09 15:40:13 +0200

    polkit: introduce common macro for generating polkit allowInteractiveAuth varlink method call IDL field
    We define the same field at many places, let's add a macro with it, that
    also contains a suitable description comment.

--- src/shared/varlink-io.elogind.Import.c
+++ src/shared/varlink-io.elogind.Import.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
 #include "varlink-io.elogind.Import.h"
+#include "bus-polkit.h"
 
 static SD_VARLINK_DEFINE_ENUM_TYPE(
                 ImageClass,
@@ -90,8 +91,7 @@ static SD_VARLINK_DEFINE_METHOD_FULL(
                 SD_VARLINK_DEFINE_INPUT(readOnly, SD_VARLINK_BOOL, SD_VARLINK_NULLABLE),
                 SD_VARLINK_FIELD_COMMENT("Whether to keep a pristine copy of the download separate from the locally installed image. Defaults to false."),
                 SD_VARLINK_DEFINE_INPUT(keepDownload, SD_VARLINK_BOOL, SD_VARLINK_NULLABLE),
-                SD_VARLINK_FIELD_COMMENT("Whether to permit interactive authentication. Defaults to false."),
-                SD_VARLINK_DEFINE_INPUT(allowInteractiveAuthentication, SD_VARLINK_BOOL, SD_VARLINK_NULLABLE),
+                VARLINK_DEFINE_POLKIT_INPUT,
                 SD_VARLINK_FIELD_COMMENT("A progress update, as percent value"),
                 SD_VARLINK_DEFINE_OUTPUT(progress, SD_VARLINK_FLOAT, SD_VARLINK_NULLABLE),
                 SD_VARLINK_FIELD_COMMENT("A log message about the ongoing transfer"),
