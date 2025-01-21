systemd/systemd

dlopen: log debug message when a library is dlopened
Useful to track what is being used and when

src/shared/userdb.c
@@ -1455,6 +1455,8 @@ int userdb_block_nss_systemd(int b) {
                 return 0;
         }
 
+        log_debug("Loaded '%s' via dlopen()", LIBDIR "/libnss_systemd.so.2");
+
         call = dlsym(dl, "_nss_systemd_block");
         if (!call)
                 /* If the file is installed but lacks the symbol we expect, things are weird, let's complain */

elogind/elogind

src/shared/userdb.c
@@ -1491,6 +1491,8 @@ int userdb_block_nss_systemd(int b) {
         }
 
         call = dlsym(dl, "_nss_elogind_block");
+        log_debug("Loaded '%s' via dlopen()", LIBDIR "/libnss_elogind.so.2");
+
         if (!call)
                 /* If the file is installed but lacks the symbol we expect, things are weird, let's complain */
                 return log_debug_errno(SYNTHETIC_ERRNO(ELIBBAD),