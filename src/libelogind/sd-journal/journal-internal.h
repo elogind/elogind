systemd/systemd

journalctl: add --exclude-identifier option

src/libsystemd/sd-journal/journal-internal.h
@@ -87,6 +87,7 @@ struct sd_journal {
         uint64_t current_field;
 
         Match *level0, *level1, *level2;
+        Set *exclude;
 
         uint64_t origin_id;
 

elogind/elogind

src/libelogind/sd-journal/journal-internal.h
@@ -91,6 +91,7 @@ struct sd_journal {
         uint64_t current_field;
 
         Match *level0, *level1, *level2;
+        Set *exclude_syslog_identifiers;
 
         uint64_t origin_id;
 