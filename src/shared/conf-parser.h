systemd/systemd

shared/conf-parser: collapse pkgdir and conf_file args into one
This essentially reverts 5656cdfeeabc16b5489f5ec7a0a36025a2ec1f23. I find it
much easier to understand what is going on when the
path-relative-to-the-search-path is passed in full, instead of being constructed
from two parts, with one of the parts being implicit in some places.

Also, we call 'systemd-analyze cat-config <path>' with <path> with the same
meaning, so this makes the internal and external APIs more consistent.

src/shared/conf-parser.h
@@ -93,24 +93,13 @@ int config_parse(
                 void *userdata,
                 struct stat *ret_stat);     /* possibly NULL */
 
-int config_parse_config_file_full(
-                const char *conf_file,
-                const char *pkgdir,
+int config_parse_config_file(
+                const char *conf_file,      /* a path like "systemd/frobnicator.conf" */
                 const char *sections,       /* nulstr */
                 ConfigItemLookup lookup,
                 const void *table,
                 ConfigParseFlags flags,
                 void *userdata);
-
-static inline int config_parse_config_file(
-                const char *conf_file,
-                const char *sections,       /* nulstr */
-                ConfigItemLookup lookup,
-                const void *table,
-                ConfigParseFlags flags,
-                void *userdata) {
-        return config_parse_config_file_full(conf_file, "systemd", sections, lookup, table, flags, userdata);
-}
 
 int config_parse_many(
                 const char* const* conf_files,  /* possibly empty */

src/shared/sleep-config.c
@@ -145,7 +145,7 @@ int parse_sleep_config(SleepConfig **ret) {
                 {}
         };
 
-        (void) config_parse_config_file("sleep.conf", "Sleep\0",
+        (void) config_parse_config_file("systemd/sleep.conf", "Sleep\0",
                                         config_item_table_lookup, items,
                                         CONFIG_PARSE_WARN, NULL);
 

elogind/elogind

src/shared/conf-parser.h
@@ -93,9 +93,8 @@ int config_parse(
                 void *userdata,
                 struct stat *ret_stat);     /* possibly NULL */
 
-int config_parse_config_file_full(
-                const char *conf_file,
-                const char *pkgdir,
+int config_parse_config_file(
+                const char *conf_file,      /* a path like "elogind/frobnicator.conf" */
                 const char *sections,       /* nulstr */
                 ConfigItemLookup lookup,
                 const void *table,
@@ -103,16 +102,6 @@ int config_parse_config_file_full(
                 void *userdata);
 
 #if 0 /// UNNEEDED by elogind
-static inline int config_parse_config_file(
-                const char *conf_file,
-                const char *sections,       /* nulstr */
-                ConfigItemLookup lookup,
-                const void *table,
-                ConfigParseFlags flags,
-                void *userdata) {
-        return config_parse_config_file_full(conf_file, "elogind", sections, lookup, table, flags, userdata);
-}
-
 int config_parse_many(
                 const char* const* conf_files,  /* possibly empty */
                 const char* const* conf_file_dirs,
@@ -153,7 +142,11 @@ DEFINE_TRIVIAL_CLEANUP_FUNC(ConfigSection*, config_section_free);
 
 #if 0 /// UNNEEDED by elogind
 int config_section_new(const char *filename, unsigned line, ConfigSection **ret);
+
+void config_section_hash_func(const ConfigSection *c, struct siphash *state);
+int config_section_compare_func(const ConfigSection *x, const ConfigSection *y);
 extern const struct hash_ops config_section_hash_ops;
+
 int _hashmap_by_section_find_unused_line(
                 HashmapBase *entries_by_section,
                 const char *filename,

src/shared/sleep-config.c
@@ -189,7 +189,7 @@ int parse_sleep_config(SleepConfig **ret) {
                 {}
         };
 
-        (void) config_parse_config_file("sleep.conf", "Sleep\0",
+        (void) config_parse_config_file("elogind/sleep.conf", "Sleep\0",
                                         config_item_table_lookup, items,
                                         CONFIG_PARSE_WARN, NULL);
 