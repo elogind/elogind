28bdc2fb0b7bfb8120b17033da2981ea02b50058
Sven Eden <sven@eden-worx.com>
2025-01-26 18:00:35 +0100

    Add missing keyring-util.[hc]
    Signed-off-by: Sven Eden <sven@eden-worx.com>

---
+++ src/basic/keyring-util.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#include <sys/types.h>
+
+#include "missing_keyctl.h"
+
+/* Like TAKE_PTR() but for key_serial_t, resetting them to -1 */
+#define TAKE_KEY_SERIAL(key_serial) TAKE_GENERIC(key_serial, key_serial_t, -1)
+
+#if 0 /// UNNEEDED by elogind
+int keyring_read(key_serial_t serial, void **ret, size_t *ret_size);
+#endif // 0
+int keyring_describe(key_serial_t serial, char **ret);
