systemd/systemd

random-util: fix compilation error
Fixes the following error:
```
../src/basic/random-util.c: In function "fallback_random_bytes":
../src/basic/random-util.c:45:26: error: initializer-string for array of "char" is too long [-Werror=unterminated-string-initialization]
   45 |                 .label = "systemd fallback random bytes v1",
      |                          ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
cc1: all warnings being treated as errors
```

src/basic/random-util.c
@@ -42,7 +42,6 @@ static void fallback_random_bytes(void *p, size_t n) {
                 uint8_t auxval[16];
         } state = {
                 /* Arbitrary domain separation to prevent other usage of AT_RANDOM from clashing. */
-                .label = "systemd fallback random bytes v1",
                 .call_id = fallback_counter++,
                 .stamp_mono = now(CLOCK_MONOTONIC),
                 .stamp_real = now(CLOCK_REALTIME),
@@ -50,6 +49,7 @@ static void fallback_random_bytes(void *p, size_t n) {
                 .tid = gettid(),
         };
 
+        memcpy(state.label, "systemd fallback random bytes v1", sizeof(state.label));
         memcpy(state.auxval, ULONG_TO_PTR(getauxval(AT_RANDOM)), sizeof(state.auxval));
 
         while (n > 0) {

elogind/elogind

(cherry picked from commit e722fe74ca5e00d3c8a5f85342b75c40ace051f9)
(cherry picked from commit 8f2f04b7d4ce80d9908f93e9cb458c9e92b19108)
(cherry picked from commit 57d2446acdaa668e3a24489aa197d4672fce2197)
(cherry picked from commit 85eafb57df113cf85164120def1b6c42f894823c)
(cherry picked from commit 64f98eda2e9aae6bc47ddcc82d77e67821ebbba1)
(cherry picked from commit b1cdcb1d450ed49f45073933638b97e806ccd3ad)

src/basic/random-util.c
@@ -44,7 +44,6 @@ static void fallback_random_bytes(void *p, size_t n) {
                 uint8_t auxval[16];
         } state = {
                 /* Arbitrary domain separation to prevent other usage of AT_RANDOM from clashing. */
-                .label = "elogind fallback random bytes v1",
                 .call_id = fallback_counter++,
                 .stamp_mono = now(CLOCK_MONOTONIC),
                 .stamp_real = now(CLOCK_REALTIME),
@@ -52,6 +51,7 @@ static void fallback_random_bytes(void *p, size_t n) {
                 .tid = gettid(),
         };
 
+        memcpy(state.label, "elogind fallback random bytes v1", sizeof(state.label));
 #if HAVE_SYS_AUXV_H
         memcpy(state.auxval, ULONG_TO_PTR(getauxval(AT_RANDOM)), sizeof(state.auxval));
 #endif