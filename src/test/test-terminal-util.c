systemd/systemd

test: resolve "Unchecked return value" coverity warning
Follow-up for c8210d98a4b64af6fadb1cb765c0451758af1303.
Fixes CID#1548920.

src/test/test-terminal-util.c
@@ -279,7 +279,7 @@ TEST(get_color_mode) {
         test_get_color_mode_with_env("COLORTERM", "24bit",     terminal_is_dumb() ? COLOR_OFF : COLOR_24BIT);
         test_get_color_mode_with_env("COLORTERM", "invalid",   terminal_is_dumb() ? COLOR_OFF : COLOR_256);
         test_get_color_mode_with_env("COLORTERM", "42",        terminal_is_dumb() ? COLOR_OFF : COLOR_256);
-        unsetenv("COLORTERM");
+        ASSERT_OK(unsetenv("COLORTERM"));
         reset_terminal_feature_caches();
 }
 

elogind/elogind

src/test/test-terminal-util.c
@@ -295,7 +295,7 @@ TEST(get_color_mode) {
         test_get_color_mode_with_env("COLORTERM", "24bit",     terminal_is_dumb() ? COLOR_OFF : COLOR_24BIT);
         test_get_color_mode_with_env("COLORTERM", "invalid",   terminal_is_dumb() ? COLOR_OFF : COLOR_256);
         test_get_color_mode_with_env("COLORTERM", "42",        terminal_is_dumb() ? COLOR_OFF : COLOR_256);
-        unsetenv("COLORTERM");
+        ASSERT_OK_ERRNO(unsetenv("COLORTERM"));
         reset_terminal_feature_caches();
 }
 