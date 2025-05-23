systemd/systemd

logind: add support for hidraw devices
Add support for opening /dev/hidraw devices via logind's TakeDevice().
Same semantics as our support for evdev devices, but it requires the
HIDIOCREVOKE ioctl in the kernel.

src/login/logind-session-device.c
@@ -15,6 +15,7 @@
 #include "logind-session-device.h"
 #include "missing_drm.h"
 #include "missing_input.h"
+#include "missing_hidraw.h"
 #include "parse-util.h"
 #include "util.h"
 
@@ -103,6 +104,20 @@ static void sd_eviocrevoke(int fd) {
         }
 }
 
+static void sd_hidiocrevoke(int fd) {
+        static bool warned = false;
+
+        assert(fd >= 0);
+
+        if (ioctl(fd, HIDIOCREVOKE, NULL) < 0) {
+
+                if (errno == EINVAL && !warned) {
+                        log_warning_errno(errno, "Kernel does not support hidraw-revocation: %m");
+                        warned = true;
+                }
+        }
+}
+
 static int sd_drmsetmaster(int fd) {
         assert(fd >= 0);
         return RET_NERRNO(ioctl(fd, DRM_IOCTL_SET_MASTER, 0));
@@ -145,6 +160,11 @@ static int session_device_open(SessionDevice *sd, bool active) {
         case DEVICE_TYPE_EVDEV:
                 if (!active)
                         sd_eviocrevoke(fd);
+                break;
+
+        case DEVICE_TYPE_HIDRAW:
+                if (!active)
+                        sd_hidiocrevoke(fd);
                 break;
 
         case DEVICE_TYPE_UNKNOWN:
@@ -190,6 +210,17 @@ static int session_device_start(SessionDevice *sd) {
                 CLOSE_AND_REPLACE(sd->fd, r);
                 break;
 
+        case DEVICE_TYPE_HIDRAW:
+                /* Hidraw devices are revoked while inactive. Reopen it and we are fine. */
+                r = session_device_open(sd, true);
+                if (r < 0)
+                        return r;
+
+                /* For hidraw devices, the file descriptor might be left uninitialized. This might happen while resuming
+                 * into a session and logind has been restarted right before. */
+                CLOSE_AND_REPLACE(sd->fd, r);
+                break;
+
         case DEVICE_TYPE_UNKNOWN:
         default:
                 /* fallback for devices without synchronizations */
@@ -229,6 +260,14 @@ static void session_device_stop(SessionDevice *sd) {
                 sd_eviocrevoke(sd->fd);
                 break;
 
+        case DEVICE_TYPE_HIDRAW:
+                /* Revoke access on hidraw file-descriptors during deactivation.
+                 * This will basically prevent any operations on the fd and
+                 * cannot be undone. Good side is: it needs no CAP_SYS_ADMIN
+                 * protection this way. */
+                sd_hidiocrevoke(sd->fd);
+                break;
+
         case DEVICE_TYPE_UNKNOWN:
         default:
                 /* fallback for devices without synchronization */
@@ -252,6 +291,9 @@ static DeviceType detect_device_type(sd_device *dev) {
         } else if (streq(subsystem, "input")) {
                 if (startswith(sysname, "event"))
                         type = DEVICE_TYPE_EVDEV;
+        } else if (streq(subsystem, "hidraw")) {
+                if (startswith(sysname, "hidraw"))
+                        type = DEVICE_TYPE_HIDRAW;
         }
 
         return type;
@@ -285,7 +327,7 @@ static int session_device_verify(SessionDevice *sd) {
                 if (sd_device_get_syspath(dev, &sp) < 0)
                         return -ENODEV;
 
-        } else if (sd->type != DEVICE_TYPE_DRM)
+        } else if (sd->type != DEVICE_TYPE_DRM && sd->type != DEVICE_TYPE_HIDRAW)
                 /* Prevent opening unsupported devices. Especially devices of
                  * subsystem "input" must be opened via the evdev node as
                  * we require EVIOCREVOKE. */

elogind/elogind

src/login/logind-session-device.c
@@ -16,6 +16,7 @@
 #include "logind-session-dbus.h"
 #include "logind-session-device.h"
 #include "missing_drm.h"
+#include "missing_hidraw.h"
 #include "missing_input.h"
 #include "parse-util.h"
 
@@ -105,6 +106,20 @@ static void sd_eviocrevoke(int fd) {
         }
 }
 
+static void sd_hidiocrevoke(int fd) {
+        static bool warned = false;
+
+        assert(fd >= 0);
+
+        if (!warned && ioctl(fd, HIDIOCREVOKE, NULL) < 0) {
+                if (errno == EINVAL) {
+                        log_warning_errno(errno, "Kernel does not support hidraw-revocation, continuing without revoking device access: %m");
+                        warned = true;
+                } else if (errno != ENODEV)
+                        log_warning_errno(errno, "Failed to revoke hidraw device, continuing without revoking device access: %m");
+        }
+}
+
 static int sd_drmsetmaster(int fd) {
         assert(fd >= 0);
         return RET_NERRNO(ioctl(fd, DRM_IOCTL_SET_MASTER, 0));
@@ -148,6 +163,11 @@ static int session_device_open(SessionDevice *sd, bool active) {
                         sd_eviocrevoke(fd);
                 break;
 
+        case DEVICE_TYPE_HIDRAW:
+                if (!active)
+                        sd_hidiocrevoke(fd);
+                break;
+
         case DEVICE_TYPE_UNKNOWN:
         default:
                 /* fallback for devices without synchronizations */
@@ -181,12 +201,13 @@ static int session_device_start(SessionDevice *sd) {
                 break;
 
         case DEVICE_TYPE_EVDEV:
-                /* Evdev devices are revoked while inactive. Reopen it and we are fine. */
+        case DEVICE_TYPE_HIDRAW:
+                /* Evdev/hidraw devices are revoked while inactive. Reopen it and we are fine. */
                 r = session_device_open(sd, true);
                 if (r < 0)
                         return r;
 
-                /* For evdev devices, the file descriptor might be left uninitialized. This might happen while resuming
+                /* For evdev/hidraw devices, the file descriptor might be left uninitialized. This might happen while resuming
                  * into a session and logind has been restarted right before. */
                 close_and_replace(sd->fd, r);
                 break;
@@ -230,6 +251,14 @@ static void session_device_stop(SessionDevice *sd) {
                 sd_eviocrevoke(sd->fd);
                 break;
 
+        case DEVICE_TYPE_HIDRAW:
+                /* Revoke access on hidraw file-descriptors during deactivation.
+                 * This will basically prevent any operations on the fd and
+                 * cannot be undone. Good side is: it needs no CAP_SYS_ADMIN
+                 * protection this way. */
+                sd_hidiocrevoke(sd->fd);
+                break;
+
         case DEVICE_TYPE_UNKNOWN:
         default:
                 /* fallback for devices without synchronization */
@@ -252,6 +281,9 @@ static DeviceType detect_device_type(sd_device *dev) {
         } else if (device_in_subsystem(dev, "input")) {
                 if (startswith(sysname, "event"))
                         return DEVICE_TYPE_EVDEV;
+        } else if (device_in_subsystem(dev, "hidraw")) {
+                if (startswith(sysname, "hidraw"))
+                        return DEVICE_TYPE_HIDRAW;
         }
 
         return DEVICE_TYPE_UNKNOWN;
@@ -289,6 +321,7 @@ static int session_device_verify(SessionDevice *sd) {
                 break;
 
         case DEVICE_TYPE_DRM:
+        case DEVICE_TYPE_HIDRAW:
                 break;
 
         case  DEVICE_TYPE_UNKNOWN: