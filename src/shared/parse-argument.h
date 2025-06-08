ab1949c90e3977083e9b3d8f6680ccd6be8b842f
Sven Eden <sven@eden-worx.com>
2025-06-08 12:08:51 +0200

    Move parse_json_argument() prototype back out of the mask
    Signed-off-by: Sven Eden <sven@eden-worx.com>

--- src/shared/parse-argument.h
+++ src/shared/parse-argument.h
@@ -6,8 +6,8 @@
 #include "sd-json.h"
 
 int parse_boolean_argument(const char *optname, const char *s, bool *ret);
-#if 0 /// UNNEEDED by elogind
 int parse_json_argument(const char *s, sd_json_format_flags_t *ret);
+#if 0 /// UNNEEDED by elogind
 int parse_path_argument(const char *path, bool suppress_root, char **arg);
 #endif // 0
 int parse_signal_argument(const char *s, int *ret);
