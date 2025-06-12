systemd/systemd

exec-util: allow to invoke polkit/ask-password agent even if STDIN is not a tty
Closes #35018.

src/shared/exec-util.c
@@ -546,10 +546,6 @@ int fexecve_or_execve(int executable_fd, const char *executable, char *const arg
 int can_fork_agent(void) {
         int r;
 
-        /* We check STDIN here, not STDOUT, since this is about input, not output */
-        if (!isatty_safe(STDIN_FILENO))
-                return false;
-
         /* Check if we have a controlling terminal. If not (ENXIO here), we aren't actually invoked
          * interactively on a terminal, hence fail. */
         r = get_ctty_devnr(0, NULL);
@@ -565,7 +561,6 @@ int can_fork_agent(void) {
 }
 
 int _fork_agent(const char *name, const int except[], size_t n_except, pid_t *ret_pid, const char *path, ...) {
-        bool stdout_is_tty, stderr_is_tty;
         size_t n, i;
         va_list ap;
         char **l;
@@ -588,19 +583,25 @@ int _fork_agent(const char *name, const int except[], size_t n_except, pid_t *re
 
         /* In the child: */
 
-        stdout_is_tty = isatty_safe(STDOUT_FILENO);
-        stderr_is_tty = isatty_safe(STDERR_FILENO);
+        bool stdin_is_tty = isatty_safe(STDIN_FILENO),
+                stdout_is_tty = isatty_safe(STDOUT_FILENO),
+                stderr_is_tty = isatty_safe(STDERR_FILENO);
 
-        if (!stdout_is_tty || !stderr_is_tty) {
+        if (!stdin_is_tty || !stdout_is_tty || !stderr_is_tty) {
                 int fd;
 
-                /* Detach from stdout/stderr and reopen /dev/tty for them. This is important to ensure that
-                 * when systemctl is started via popen() or a similar call that expects to read EOF we
+                /* Detach from stdin/stdout/stderr and reopen /dev/tty for them. This is important to ensure
+                 * that when systemctl is started via popen() or a similar call that expects to read EOF we
                  * actually do generate EOF and not delay this indefinitely by keeping an unused copy of
                  * stdin around. */
-                fd = open_terminal("/dev/tty", O_WRONLY);
+                fd = open_terminal("/dev/tty", stdin_is_tty ? O_WRONLY : (stdout_is_tty && stderr_is_tty) ? O_RDONLY : O_RDWR);
                 if (fd < 0) {
                         log_error_errno(fd, "Failed to open /dev/tty: %m");
+                        _exit(EXIT_FAILURE);
+                }
+
+                if (!stdin_is_tty && dup2(fd, STDIN_FILENO) < 0) {
+                        log_error_errno(errno, "Failed to dup2 /dev/tty to STDIN: %m");
                         _exit(EXIT_FAILURE);
                 }
 

elogind/elogind

(cherry picked from commit 0f81c8406f0f47175c699715e84de8291057033c)

src/shared/exec-util.c
@@ -550,7 +550,6 @@ int fexecve_or_execve(int executable_fd, const char *executable, char *const arg
 #endif // 0
 
 int _fork_agent(const char *name, const int except[], size_t n_except, pid_t *ret_pid, const char *path, ...) {
-        bool stdout_is_tty, stderr_is_tty;
         size_t n, i;
         va_list ap;
         char **l;
@@ -573,17 +572,18 @@ int _fork_agent(const char *name, const int except[], size_t n_except, pid_t *re
 
         /* In the child: */
 
-        stdout_is_tty = isatty_safe(STDOUT_FILENO);
-        stderr_is_tty = isatty_safe(STDERR_FILENO);
+        bool stdin_is_tty = isatty_safe(STDIN_FILENO),
+                stdout_is_tty = isatty_safe(STDOUT_FILENO),
+                stderr_is_tty = isatty_safe(STDERR_FILENO);
 
-        if (!stdout_is_tty || !stderr_is_tty) {
+        if (!stdin_is_tty || !stdout_is_tty || !stderr_is_tty) {
                 int fd;
 
-                /* Detach from stdout/stderr and reopen /dev/tty for them. This is important to ensure that
-                 * when systemctl is started via popen() or a similar call that expects to read EOF we
+                /* Detach from stdin/stdout/stderr and reopen /dev/tty for them. This is important to ensure
+                 * that when systemctl is started via popen() or a similar call that expects to read EOF we
                  * actually do generate EOF and not delay this indefinitely by keeping an unused copy of
                  * stdin around. */
-                fd = open("/dev/tty", O_WRONLY);
+                fd = open("/dev/tty", stdin_is_tty ? O_WRONLY : (stdout_is_tty && stderr_is_tty) ? O_RDONLY : O_RDWR);
                 if (fd < 0) {
                         if (errno != ENXIO) {
                                 log_error_errno(errno, "Failed to open /dev/tty: %m");
@@ -594,13 +594,18 @@ int _fork_agent(const char *name, const int except[], size_t n_except, pid_t *re
                          * connected to a TTY. That's a weird setup, but let's handle it gracefully: let's
                          * skip the forking of the agents, given the TTY setup is not in order. */
                 } else {
+                        if (!stdin_is_tty && dup2(fd, STDIN_FILENO) < 0) {
+                                log_error_errno(errno, "Failed to dup2 /dev/tty to STDIN: %m");
+                                _exit(EXIT_FAILURE);
+                        }
+
                         if (!stdout_is_tty && dup2(fd, STDOUT_FILENO) < 0) {
-                                log_error_errno(errno, "Failed to dup2 /dev/tty: %m");
+                                log_error_errno(errno, "Failed to dup2 /dev/tty to STDOUT: %m");
                                 _exit(EXIT_FAILURE);
                         }
 
                         if (!stderr_is_tty && dup2(fd, STDERR_FILENO) < 0) {
-                                log_error_errno(errno, "Failed to dup2 /dev/tty: %m");
+                                log_error_errno(errno, "Failed to dup2 /dev/tty to STDERR: %m");
                                 _exit(EXIT_FAILURE);
                         }
 