systemd/systemd

docs: add man pages for sd_device_enumerator_[new,ref,unref,unrefp] (#37586)
For #20929.

man/sd_device_enumerator_new-example.c
@@ -1,0 +1,22 @@
+/* SPDX-License-Identifier: MIT-0 */
+
+#include <stdio.h>
+#include <systemd/sd-device.h>
+
+int main(void) {
+    sd_device_enumerator *enumerator;
+    int r;
+
+    r = sd_device_enumerator_new(&enumerator);
+    if (r < 0) {
+        fprintf(stderr, "Failed to create enumerator: %s\n", strerror(-r));
+        return 1;
+    }
+
+    sd_device_enumerator_ref(enumerator);
+    sd_device_enumerator_unref(enumerator);
+
+    sd_device_enumerator_unref(enumerator);
+
+    return 0;
+}

elogind/elogind

(cherry picked from commit ac8ed83947237956b867de9cfbf5fcd26de8ea96)

man/sd_device_enumerator_new-example.c
@@ -1,0 +1,22 @@
+/* SPDX-License-Identifier: MIT-0 */
+
+#include <stdio.h>
+#include <elogind/sd-device.h>
+
+int main(void) {
+    sd_device_enumerator *enumerator;
+    int r;
+
+    r = sd_device_enumerator_new(&enumerator);
+    if (r < 0) {
+        fprintf(stderr, "Failed to create enumerator: %s\n", strerror(-r));
+        return 1;
+    }
+
+    sd_device_enumerator_ref(enumerator);
+    sd_device_enumerator_unref(enumerator);
+
+    sd_device_enumerator_unref(enumerator);
+
+    return 0;
+}