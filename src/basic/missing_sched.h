systemd/systemd

missing_sched: add CLONE_PIDFD
CLONE_PIDFD was introduced in v5.2 so without this, building with older
Linux kernel headers fails with:

src/basic/raw-clone.h:41:108: error: 'CLONE_PIDFD' undeclared (first use in this function); did you mean 'CLONE_FILES'?

src/basic/missing_sched.h
@@ -13,6 +13,13 @@
 assert_cc(CLONE_NEWCGROUP == 0x02000000);
 #endif
 
+/* b3e5838252665ee4cfa76b82bdf1198dca81e5be (5.2) */
+#ifndef CLONE_PIDFD
+#  define CLONE_PIDFD 0x00000080
+#else
+assert_cc(CLONE_PIDFD == 0x00001000);
+#endif
+
 /* 769071ac9f20b6a447410c7eaa55d1a5233ef40c (5.8) */
 #ifndef CLONE_NEWTIME
 #  define CLONE_NEWTIME 0x00000080

elogind/elogind

CLONE_PIDFD was introduced in v5.2 and in sched.h in glibc-2.31 so
without this, building with older version fails with:
(cherry picked from commit e91c5cf06ab7ca9e5576c6feac5f743927f2b063)

src/basic/missing_sched.h
@@ -13,6 +13,13 @@
 assert_cc(CLONE_NEWCGROUP == 0x02000000);
 #endif
 
+/* b3e5838252665ee4cfa76b82bdf1198dca81e5be (5.2) */
+#ifndef CLONE_PIDFD
+#  define CLONE_PIDFD 0x00001000
+#else
+assert_cc(CLONE_PIDFD == 0x00001000);
+#endif
+
 /* 769071ac9f20b6a447410c7eaa55d1a5233ef40c (5.8) */
 #ifndef CLONE_NEWTIME
 #  define CLONE_NEWTIME 0x00000080