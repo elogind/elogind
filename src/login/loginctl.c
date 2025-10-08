2b8c18f0390b3d346ba323334936f36c3e9c807f
Sven Eden <sven@eden-worx.com>
2025-10-08 07:40:55 +0200

    Fix masking issues in logind-button.c
    - Corrected handling of button_set_mask call.
    - Removed redundant blank lines in sd_bus_set_address.xml and loginctl.c.
    
    These changes address specific masking issues and improve code readability by removing unnecessary blank lines.
    
    Signed-off-by: Sven Eden <sven@eden-worx.com>

--- src/login/loginctl.c
+++ src/login/loginctl.c
@@ -1499,6 +1499,7 @@ static int parse_argv(int argc, char *argv[]) {
                                 arg_legend = false;
 
                         break;
+
 #if 1 /// elogind supports --no-wall and --dry-run
                 case ARG_NO_WALL:
                         arg_no_wall = true;
@@ -1540,6 +1541,7 @@ static int parse_argv(int argc, char *argv[]) {
                         arg_transport = BUS_TRANSPORT_MACHINE;
                         arg_host = optarg;
                         break;
+
 #if 1 /// elogind can cancel shutdowns, allows to ignore inhibitors and can controll the reboot process
                 case 'c':
                         arg_action = ACTION_CANCEL_SHUTDOWN;
