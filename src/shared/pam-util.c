systemd/systemd

pam-util: fix pam_syslog_errno() ignoring the level parameter
The function accepts a level parameter but was always logging at
LOG_ERR. Fix by passing the level parameter to sym_pam_vsyslog()
instead of hardcoding LOG_ERR.

This caused debug and warning messages to incorrectly appear in error
logs.

Signed-off-by: val4oss <val4oss@pm.me>

src/shared/pam-util.c
@@ -75,7 +75,7 @@ int pam_syslog_errno(pam_handle_t *handle, int level, int error, const char *for
         LOCAL_ERRNO(error);
 
         va_start(ap, format);
-        sym_pam_vsyslog(handle, LOG_ERR, format, ap);
+        sym_pam_vsyslog(handle, level, format, ap);
         va_end(ap);
 
         return error == -ENOMEM ? PAM_BUF_ERR : PAM_SERVICE_ERR;

elogind/elogind

(cherry picked from commit bc9dc63f4f24eb989e92d4a96e46da279528a767)
(cherry picked from commit 9b62782d1e3395598982daeabbaeac0c1d0d2a9c)

src/shared/pam-util.c
@@ -28,7 +28,7 @@ int pam_syslog_errno(pam_handle_t *handle, int level, int error, const char *for
         LOCAL_ERRNO(error);
 
         va_start(ap, format);
-        pam_vsyslog(handle, LOG_ERR, format, ap);
+        pam_vsyslog(handle, level, format, ap);
         va_end(ap);
 
         return error == -ENOMEM ? PAM_BUF_ERR : PAM_SERVICE_ERR;