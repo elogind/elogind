6dfa91094fd29b8e9d2ca54b8c8c8ca736c465d9
Sven Eden <sven@eden-worx.com>
2026-05-01 09:25:13 +0200

    userwork.c: Use gid_is_valid instead of uid_is_valid for p.gid check.
    Signed-off-by: Sven Eden <sven@eden-worx.com>

--- src/userdb/userwork.c
+++ src/userdb/userwork.c
@@ -346,7 +346,7 @@ static int vl_method_get_group_record(sd_varlink *link, sd_json_variant *paramet
                 return sd_varlink_error(link, "io.systemd.UserDatabase.ServiceNotAvailable", NULL);
         }
 
-        if ((uid_is_valid(p.gid) && g->gid != p.gid) ||
+        if ((gid_is_valid(p.gid) && g->gid != p.gid) ||
             (p.group_name && !streq(g->group_name, p.group_name)))
                 return sd_varlink_error(link, "io.systemd.UserDatabase.ConflictingRecordFound", NULL);
 
