4bfd5041ef7633fe41e0dd6c937447aaa36bf52d
Sven Eden <sven@eden-worx.com>
2026-06-01 11:16:44 +0200

    missing_prctl.h: add musl-friendly conditional prctl includes (#350)
    - Remove direct <linux/prctl.h> include
    + Add conditional include block that prefers <sys/prctl.h>
      and falls back to <linux/prctl.h> when needed
    
    Bug: #350
    Closes: #350
    Signed-off-by: Sven Eden <sven@eden-worx.com>

--- src/basic/missing_prctl.h
+++ src/basic/missing_prctl.h
@@ -1,7 +1,14 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #pragma once
 
+#if 0 /// elogind should also be buildable on musl, and needs a bit more care here
 #include <linux/prctl.h>
+#else // 0
+#include <sys/prctl.h>
+#ifndef PR_SET_MM_MAP
+#  include <linux/prctl.h>
+#endif
+#endif // 0
 
 /* 58319057b7847667f0c9585b9de0e8932b0fdb08 (4.3) */
 #ifndef PR_CAP_AMBIENT
